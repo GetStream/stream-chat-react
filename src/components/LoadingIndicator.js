9cc7ee31b397574bc6f6da93877e6560184e51d2
Jaap <jaap@getstream.io>
2019-04-02 17:39:52 +0200

    better loadingindicator and do s

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -13,20 +13,44 @@ export class LoadingIndicator extends React.PureComponent {
   static propTypes = {
     /** The size of the loading icon */
     size: PropTypes.number,
+    /** Set the color of the LoadingIndicator */
+    color: PropTypes.string,
   };
   static defaultProps = {
     size: 15,
+    color: '#006CFF',
   };
   render() {
+    const { size, color } = this.props;
     return (
       <div
         className="str-chat__loading-indicator"
-        style={{ width: this.props.size, height: this.props.size }}
+        style={{ width: size, height: size }}
       >
-        <img
+        {/* <img
           src={svg}
-          style={{ width: this.props.size, height: this.props.size }}
-        />
+          style={{ width: size, height: size }}
+        /> */}
+
+        <svg
+          width={size}
+          height={size}
+          viewBox={`0 0 28 30`}
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="a">
+              <stop stopColor="#FFF" stopOpacity="0" offset="0%" />
+              <stop stopColor={color} offset="100%" />
+            </linearGradient>
+          </defs>
+          <path
+            d="M2.518 23.321l1.664-1.11A12.988 12.988 0 0 0 15 28c7.18 0 13-5.82 13-13S22.18 2 15 2V0c8.284 0 15 6.716 15 15 0 8.284-6.716 15-15 15-5.206 0-9.792-2.652-12.482-6.679z"
+            transform="translate(-2)"
+            fill="url(#a)"
+            fillRule="evenodd"
+          />
+        </svg>
       </div>
     );
   }
