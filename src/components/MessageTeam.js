d6745e43a270505b6efb243b6d24c5e53d3e9f18
Jaap <jaap@getstream.io>
2019-04-04 14:55:36 +0200

    removed own seen state from message team

--- src/components/MessageTeam.js
+++ src/components/MessageTeam.js
@@ -108,7 +108,10 @@ export class MessageTeam extends PureComponent {
   // https://stackoverflow.com/a/29234240/7625485
   formatArray = (arr) => {
     let outStr = '';
-    const slicedArr = arr.map((item) => item.name || item.id).slice(0, 5);
+    const slicedArr = arr
+      .filter((item) => item.id !== this.props.client.user.id)
+      .map((item) => item.name || item.id)
+      .slice(0, 5);
     const restLength = arr.length - slicedArr.length;
     const lastStr = restLength > 0 ? ' and ' + restLength + ' more' : '';
 
@@ -155,13 +158,21 @@ export class MessageTeam extends PureComponent {
         <span className="str-chat__message-team-status">
           <Tooltip>{this.formatArray(message.readBy)}</Tooltip>
           <Avatar
-            name={this.props.readBy[0].id}
-            image={this.props.readBy[0].image}
+            name={
+              this.props.readBy.filter(
+                (item) => item.id !== this.props.client.user.id,
+              )[0].id
+            }
+            image={
+              this.props.readBy.filter(
+                (item) => item.id !== this.props.client.user.id,
+              )[0].image
+            }
             size={15}
           />
-          {message.readBy.length > 1 && (
+          {message.readBy.length - 1 > 1 && (
             <span className="str-chat__message-team-status-number">
-              {message.readBy.length}
+              {message.readBy.length - 1}
             </span>
           )}
         </span>
