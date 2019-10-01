cb26eb7a8325f61de09f2fee82f257d827d84ba2
Vishal Narkhede <vishal@getstream.io>
2019-10-01 14:17:02 +0200

    CHAT-704: Making empty typing object immutable

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -283,7 +283,7 @@ class ChannelInner extends PureComponent {
       members: channel.state.members,
       watcher_count: channel.state.watcher_count,
       loading: false,
-      typing: {},
+      typing: Immutable({}),
     });
 
     channel.markRead();
