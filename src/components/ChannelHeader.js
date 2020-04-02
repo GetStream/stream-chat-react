3db736ca5d1fc53fc35d1597692d8132bf27027b
Jaap <jaap@getstream.io>
2020-04-02 17:59:05 +0200

    removed info button

--- src/components/ChannelHeader.js
+++ src/components/ChannelHeader.js
@@ -71,23 +71,6 @@ class ChannelHeader extends PureComponent {
             {t('{{ watcherCount }} online', { watcherCount: watcher_count })}
           </p>
         </div>
-        <div className="str-chat__header-livestream-right">
-          <div className="str-chat__header-livestream-right-button-wrapper">
-            <a
-              href="https://getstream.io/chat/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="str-chat__square-button str-chat__header-livestream-right-button--info"
-            >
-              <svg width="4" height="14" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M3 13h1v.5H0V13h1V5.5H0V5h3v8zM1.994 3.516A1.507 1.507 0 1 1 1.995.502a1.507 1.507 0 0 1-.001 3.014z"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </a>
-          </div>
-        </div>
       </div>
     );
   }
