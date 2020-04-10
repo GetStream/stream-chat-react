8c23ad64c4b9f8c48d55f5938c635f1dda94ebb4
Amin Mahboubi <aminmahboobi@gmail.com>
2020-04-10 16:29:41 +0200

    break down context file

--- src/context/ChannelContext.js
+++ src/context/ChannelContext.js
@@ -1,29 +1,6 @@
 import React from 'react';
 import Dayjs from 'dayjs';
 
-export const ChatContext = React.createContext({ client: null });
-
-export function withChatContext(OriginalComponent) {
-  const ContextAwareComponent = function ContextComponent(props) {
-    return (
-      <ChatContext.Consumer>
-        {(context) => {
-          const mergedProps = { ...context, ...props };
-          return <OriginalComponent {...mergedProps} />;
-        }}
-      </ChatContext.Consumer>
-    );
-  };
-  ContextAwareComponent.displayName =
-    OriginalComponent.displayName || OriginalComponent.name || 'Component';
-  ContextAwareComponent.displayName = ContextAwareComponent.displayName.replace(
-    'Base',
-    '',
-  );
-
-  return ContextAwareComponent;
-}
-
 export const ChannelContext = React.createContext({});
 
 export function withChannelContext(OriginalComponent) {
---
+++ src/context/index.js
@@ -0,0 +1,2 @@
+export * from './ChatContext';
+export * from './ChannelContext';
