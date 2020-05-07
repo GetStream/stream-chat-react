33d200b9b4de46ff3b76c7a96c00113563fd071a
Amin Mahboubi <aminmahboobi@gmail.com>
2020-05-07 15:38:29 +0200

    ChatDown uses hooks

--- src/components/ChatDown/ChatDown.js
+++ src/components/ChatDown/ChatDown.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 import { LoadingChannels } from '../Loading';
-import { withTranslationContext } from '../../context';
+import { TranslationContext } from '../../context';
 
 import placeholder from '../../assets/str-chat__connection-error.svg';
 
@@ -10,20 +10,24 @@ import placeholder from '../../assets/str-chat__connection-error.svg';
  * ChatDown - Indicator that chat is down or your network isn't working
  *
  * @example ../../docs/ChatDown.md
- * @extends PureComponent
  */
-const ChatDown = ({ image, type, text, t }) => (
-  <div className="str-chat__down">
-    <LoadingChannels />
-    <div className="str-chat__down-main">
-      <img data-testid="chatdown-img" src={image || placeholder} />
-      <h1>{type}</h1>
-      <h3>
-        {text || t('Error connecting to chat, refresh the page to try again.')}
-      </h3>
+const ChatDown = ({ image, type, text }) => {
+  const { t } = useContext(TranslationContext);
+
+  return (
+    <div className="str-chat__down">
+      <LoadingChannels />
+      <div className="str-chat__down-main">
+        <img data-testid="chatdown-img" src={image || placeholder} />
+        <h1>{type}</h1>
+        <h3>
+          {text ||
+            t('Error connecting to chat, refresh the page to try again.')}
+        </h3>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 ChatDown.defaultProps = {
   type: 'Error',
@@ -38,4 +42,4 @@ ChatDown.propTypes = {
   text: PropTypes.string,
 };
 
-export default withTranslationContext(React.memo(ChatDown));
+export default React.memo(ChatDown);
