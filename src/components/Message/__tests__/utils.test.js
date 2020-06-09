0f35a47754555ca887c36ea6a42bb70a3ff6a62c
Vini Andrade <vini.andrade.dev@gmail.com>
2020-06-09 16:20:07 +0200

    Extract isUserMuted function
    Signed-off-by: Vini Andrade <vini.andrade.dev@gmail.com>

--- src/components/Message/__tests__/utils.test.js
+++ src/components/Message/__tests__/utils.test.js
@@ -1,21 +1,59 @@
-import { validateAndGetMessage } from '../utils';
+import { generateMessage, generateUser } from 'mock-builders';
+import { isUserMuted, validateAndGetMessage } from '../utils';
 
 describe('Message utils', () => {
-  it('should return null if called without a function as the first parameter', () => {
-    const result = validateAndGetMessage({}, 'Message');
-    expect(result).toBeNull();
+  describe('validateAndGetMessage function', () => {
+    it('should return null if called without a function as the first parameter', () => {
+      const result = validateAndGetMessage({}, 'Message');
+      expect(result).toBeNull();
+    });
+
+    it('should return null if result of function call is not a string', () => {
+      const fn = () => null;
+      const result = validateAndGetMessage(fn, 'something');
+      expect(result).toBeNull();
+    });
+
+    it('should return the result of the function call when it is a string', () => {
+      const fn = (msg) => msg;
+      const message = 'message';
+      const result = validateAndGetMessage(fn, [message]);
+      expect(result).toBe(message);
+    });
   });
 
-  it('should return null if result of function call is not a string', () => {
-    const fn = () => null;
-    const result = validateAndGetMessage(fn, 'something');
-    expect(result).toBeNull();
-  });
+  describe('isUserMuted function', () => {
+    const alice = generateUser({ name: 'alice' });
+    const bob = generateUser({ name: 'bob' });
+    it('should return false if message is not defined', () => {
+      const mutes = [
+        {
+          user: alice,
+          target: bob,
+          created_at: new Date('2019-03-30T13:24:10'),
+        },
+      ];
+      const result = isUserMuted(undefined, mutes);
+      expect(result).toBe(false);
+    });
 
-  it('should return the result of the function call when it is a string', () => {
-    const fn = (msg) => msg;
-    const message = 'message';
-    const result = validateAndGetMessage(fn, [message]);
-    expect(result).toBe(message);
+    it('should return false if mutes is not defined', () => {
+      const message = generateMessage();
+      const result = isUserMuted(message, undefined);
+      expect(result).toBe(false);
+    });
+
+    it('should return true if user was muted', () => {
+      const mutes = [
+        {
+          user: alice,
+          target: bob,
+          created_at: new Date('2019-03-30T13:24:10'),
+        },
+      ];
+      const message = generateMessage({ user: bob });
+      const result = isUserMuted(message, mutes);
+      expect(result).toBe(true);
+    });
   });
 });
--- src/components/Message/utils.js
+++ src/components/Message/utils.js
@@ -3,8 +3,6 @@
  * Following function validates a function which returns notification message.
  * It validates if the first parameter is function and also if return value of function is string or no.
  *
- * @param func {Function}
- * @param args {any[]} Arguments to be provided to func while executing.
  * @type {(func: Function, args: any) => null | string}
  */
 export const validateAndGetMessage = (func, args) => {
@@ -15,4 +13,21 @@ export const validateAndGetMessage = (func, args) => {
   if (typeof returnValue !== 'string') return null;
 
   return returnValue;
+};
+
+/**
+ * Tell if the owner of the current message is muted
+ *
+ * @type {(message?: import('stream-chat').MessageResponse, mutes?: import('stream-chat').Mute[]) => boolean}
+ */
+export const isUserMuted = (message, mutes) => {
+  if (!mutes || !message) {
+    return false;
+  }
+
+  const userMuted = mutes.filter(
+    /** @type {(el: import('stream-chat').Mute) => boolean} Typescript syntax */
+    (el) => el.target.id === message.user?.id,
+  );
+  return !!userMuted.length;
 };
