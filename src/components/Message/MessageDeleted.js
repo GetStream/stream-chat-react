26fbdba861f4a67f8ed8fc992e7855d449864315
Vini Andrade <vini.andrade.dev@gmail.com>
2020-06-15 11:55:54 +0200

    Move MessageDeleted to Messages namespace
    Signed-off-by: Vini Andrade <vini.andrade.dev@gmail.com>

--- src/components/Message/MessageDeleted.js
+++ src/components/Message/MessageDeleted.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withTranslationContext } from '../context';
+import { withTranslationContext } from '../../context';
 
 class MessageDeleted extends React.PureComponent {
   static propTypes = {
