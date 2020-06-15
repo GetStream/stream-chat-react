6d6d5cd210f672d356865f47995efb506e47b8b7
Jaap <jaap@getstream.io>
2020-06-15 16:24:08 +0200

    UserItem: tests

---
+++ src/components/UserItem/__tests__/UserItem.test.js
@@ -0,0 +1,68 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { cleanup, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import UserItem from '../UserItem';
+
+afterEach(cleanup); // eslint-disable-line
+
+describe('UserItem', () => {
+  it('should render component with default props', () => {
+    const tree = renderer.create(<UserItem entity={{}} />).toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+      <div
+        className="str-chat__user-item"
+      >
+        <div
+          className="str-chat__avatar str-chat__avatar--circle"
+          data-testid="avatar"
+          onClick={[Function]}
+          onMouseOver={[Function]}
+          style={
+            Object {
+              "flexBasis": 20,
+              "fontSize": 10,
+              "height": 20,
+              "lineHeight": "20px",
+              "width": 20,
+            }
+          }
+        >
+          <div
+            className="str-chat__avatar-fallback"
+            data-testid="avatar-fallback"
+          >
+            
+          </div>
+        </div>
+        <div>
+          <strong />
+           
+        </div>
+      </div>
+    `);
+  });
+
+  it('should render username if provided', () => {
+    const { getByText } = render(
+      <UserItem entity={{ name: 'Frits Sissing' }} />,
+    );
+    expect(getByText('Frits Sissing')).toBeInTheDocument();
+  });
+
+  it('should render id if no name is provided', () => {
+    const { getByText } = render(<UserItem entity={{ id: '123' }} />);
+    expect(getByText('123')).toBeInTheDocument();
+  });
+
+  it('should render profile picture if provided', () => {
+    const { getByTestId } = render(
+      <UserItem
+        entity={{ id: '123', image: 'frits.jpg', name: 'Frits Sissing' }}
+      />,
+    );
+    expect(getByTestId('avatar-img')).toHaveAttribute('src', 'frits.jpg');
+  });
+});
