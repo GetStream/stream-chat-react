3c169a062bb6830eb390d97f0dbdbcdee07bdf35
Tom Hutman <tom@getstream.io>
2020-06-22 17:04:31 +0200

    check if open in escape handler, internationalize Close

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,11 +1,13 @@
 // @ts-check
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
+import { TranslationContext } from '../../context';
 
 /** @type {React.FC<import("types").ModalProps>} */
 const Modal = ({ children, onClose, open }) => {
   /** @type {React.RefObject<HTMLDivElement>} */
   const innerRef = useRef(null);
+  const { t } = useContext(TranslationContext);
 
   /** @param {React.MouseEvent} e */
   const handleClick = (e) => {
@@ -15,6 +17,7 @@ const Modal = ({ children, onClose, open }) => {
   };
 
   useEffect(() => {
+    if (!open) return () => {};
     /** @type {EventListener} */
     const handleEscKey = (e) => {
       if (e instanceof KeyboardEvent && e.keyCode === 27) {
@@ -23,7 +26,7 @@ const Modal = ({ children, onClose, open }) => {
     };
     document.addEventListener('keyPress', handleEscKey, false);
     return () => document.removeEventListener('keyPress', handleEscKey, false);
-  }, [onClose]);
+  }, [onClose, open]);
 
   const openClasses = open
     ? 'str-chat__modal--open'
@@ -32,7 +35,7 @@ const Modal = ({ children, onClose, open }) => {
   return (
     <div className={`str-chat__modal ${openClasses}`} onClick={handleClick}>
       <div className="str-chat__modal__close-button">
-        Close
+        {t('Close')}
         <svg width="10" height="10" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M9.916 1.027L8.973.084 5 4.058 1.027.084l-.943.943L4.058 5 .084 8.973l.943.943L5 5.942l3.973 3.974.943-.943L5.942 5z"
