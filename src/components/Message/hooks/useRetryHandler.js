19197703e0c23e15a0a6b77b16d8845ee36beb9f
Vini Andrade <vini.andrade.dev@gmail.com>
2020-07-03 12:29:18 +0200

    Allow custom retry handler from its custom hook
    Signed-off-by: Vini Andrade <vini.andrade.dev@gmail.com>

--- src/components/Message/hooks/useRetryHandler.js
+++ src/components/Message/hooks/useRetryHandler.js
@@ -3,13 +3,18 @@ import { useContext } from 'react';
 import { ChannelContext } from '../../../context';
 
 /**
- * @type {() => (message: import('stream-chat').Message | undefined) => Promise<void>}
+ * @type {(
+ *  customRetrySendMessage?: (message: import('stream-chat').Message) => Promise<void>
+ * ) => (message: import('stream-chat').Message | undefined) => Promise<void>}
  */
-export const useRetryHandler = () => {
+export const useRetryHandler = (customRetrySendMessage) => {
   /**
    *@type {import('types').ChannelContextValue}
    */
-  const { retrySendMessage } = useContext(ChannelContext);
+  const { retrySendMessage: contextRetrySendMessage } = useContext(
+    ChannelContext,
+  );
+  const retrySendMessage = customRetrySendMessage || contextRetrySendMessage;
   return async (message) => {
     if (retrySendMessage && message) {
       await retrySendMessage(message);
