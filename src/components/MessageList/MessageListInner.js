540dc80a58554fe0ce2f1b587056631f37d984d3
Amin Mahboubi <aminmahboobi@gmail.com>
2020-07-09 15:24:12 +0200

    simplify message list getGroupStyles

--- src/components/MessageList/MessageListInner.js
+++ src/components/MessageList/MessageListInner.js
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/cognitive-complexity */
 /* eslint-disable no-continue */
 /* eslint-disable sonarjs/no-duplicate-string */
 import React, { useMemo } from 'react';
@@ -115,88 +116,54 @@ const insertIntro = (messages, headerPosition) => {
   return newMessages;
 };
 
-const getGroupStyles = (messages, noGroupByUser) => {
-  const messageGroupStyles = {};
+const getGroupStyles = (
+  message,
+  previousMessage,
+  nextMessage,
+  noGroupByUser,
+) => {
+  if (message.type === 'message.date') return '';
+  if (message.type === 'channel.event') return '';
+  if (message.type === 'channel.intro') return '';
 
-  for (let i = 0, l = messages.length; i < l; i += 1) {
-    const previousMessage = messages[i - 1];
-    const message = messages[i];
-    const nextMessage = messages[i + 1];
-    const groupStyles = [];
+  if (noGroupByUser || message.attachments.length !== 0) return 'single';
 
-    if (message.type === 'message.date') {
-      continue;
-    }
+  const isTopMessage =
+    !previousMessage ||
+    previousMessage.type === 'channel.intro' ||
+    previousMessage.type === 'message.date' ||
+    previousMessage.type === 'system' ||
+    previousMessage.type === 'channel.event' ||
+    previousMessage.attachments.length !== 0 ||
+    message.user.id !== previousMessage.user.id ||
+    previousMessage.type === 'error' ||
+    previousMessage.deleted_at;
 
-    if (message.type === 'channel.event') {
-      continue;
-    }
+  const isBottomMessage =
+    !nextMessage ||
+    nextMessage.type === 'message.date' ||
+    nextMessage.type === 'system' ||
+    nextMessage.type === 'channel.event' ||
+    nextMessage.type === 'channel.intro' ||
+    nextMessage.attachments.length !== 0 ||
+    message.user.id !== nextMessage.user.id ||
+    nextMessage.type === 'error' ||
+    nextMessage.deleted_at;
 
-    if (message.type === 'channel.intro') {
-      continue;
-    }
-
-    const userId = message.user.id;
-
-    const isTopMessage =
-      !previousMessage ||
-      previousMessage.type === 'channel.intro' ||
-      previousMessage.type === 'message.date' ||
-      previousMessage.type === 'system' ||
-      previousMessage.type === 'channel.event' ||
-      previousMessage.attachments.length !== 0 ||
-      userId !== previousMessage.user.id ||
-      previousMessage.type === 'error' ||
-      previousMessage.deleted_at;
-
-    const isBottomMessage =
-      !nextMessage ||
-      nextMessage.type === 'message.date' ||
-      nextMessage.type === 'system' ||
-      nextMessage.type === 'channel.event' ||
-      nextMessage.type === 'channel.intro' ||
-      nextMessage.attachments.length !== 0 ||
-      userId !== nextMessage.user.id ||
-      nextMessage.type === 'error' ||
-      nextMessage.deleted_at;
-
-    if (isTopMessage) {
-      groupStyles.push('top');
-    }
-
-    if (isBottomMessage) {
-      if (isTopMessage || message.deleted_at || message.type === 'error') {
-        groupStyles.splice(0, groupStyles.length);
-        groupStyles.push('single');
-      } else {
-        groupStyles.push('bottom');
-      }
-    }
-
-    if (!isTopMessage && !isBottomMessage) {
-      if (message.deleted_at || message.type === 'error') {
-        groupStyles.splice(0, groupStyles.length);
-        groupStyles.push('single');
-      } else {
-        groupStyles.splice(0, groupStyles.length);
-        groupStyles.push('middle');
-      }
-    }
-
-    if (message.attachments.length !== 0) {
-      groupStyles.splice(0, groupStyles.length);
-      groupStyles.push('single');
-    }
-
-    if (noGroupByUser) {
-      groupStyles.splice(0, groupStyles.length);
-      groupStyles.push('single');
-    }
-
-    messageGroupStyles[message.id] = groupStyles;
+  if (!isTopMessage && !isBottomMessage) {
+    if (message.deleted_at || message.type === 'error') return 'single';
+    return 'middle';
   }
 
-  return messageGroupStyles;
+  if (isBottomMessage) {
+    if (isTopMessage || message.deleted_at || message.type === 'error')
+      return 'single';
+    return 'bottom';
+  }
+
+  if (isTopMessage) return 'top';
+
+  return '';
 };
 
 const MessageListInner = (props) => {
@@ -226,7 +193,17 @@ const MessageListInner = (props) => {
   }, [HeaderComponent, eventHistory, headerPosition, messages]);
 
   const messageGroupStyles = useMemo(
-    () => getGroupStyles(enrichedMessages, noGroupByUser),
+    () =>
+      enrichedMessages.reduce((acc, message, i) => {
+        const style = getGroupStyles(
+          message,
+          enrichedMessages[i - 1],
+          enrichedMessages[i + 1],
+          noGroupByUser,
+        );
+        if (style) acc[message.id] = style;
+        return acc;
+      }, {}),
     [enrichedMessages, noGroupByUser],
   );
 
@@ -274,8 +251,7 @@ const MessageListInner = (props) => {
       }
 
       if (message.type !== 'message.read') {
-        const groupStyles = messageGroupStyles[message.id] || [];
-        const readBy = readData[message.id] || [];
+        const groupStyles = messageGroupStyles[message.id] || '';
 
         return (
           <li
@@ -286,8 +262,8 @@ const MessageListInner = (props) => {
             <Message
               client={client}
               message={message}
-              groupStyles={groupStyles}
-              readBy={readBy}
+              groupStyles={[groupStyles]} /* TODO: convert to simple string */
+              readBy={readData[message.id] || []}
               lastReceivedId={lastReceivedId}
               threadList={threadList}
               {...internalMessageProps}
