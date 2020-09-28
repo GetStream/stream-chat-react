f815e4215b5b7ba25bb3e2e692694a314510a1db
Amin Mahboubi <aminmahboobi@gmail.com>
2020-09-28 16:09:12 +0200

    DateSeparator supports unread prop

--- src/components/DateSeparator/__tests__/DateSeparator.test.js
+++ src/components/DateSeparator/__tests__/DateSeparator.test.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Dayjs from 'dayjs';
+import calendar from 'dayjs/plugin/calendar';
 import { cleanup, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import DateSeparator from '../DateSeparator';
+import { TranslationContext } from '../../../context';
+
+Dayjs.extend(calendar);
 
 afterEach(cleanup); // eslint-disable-line
 
 // this changes every time tests are run,
 // but by mocking the actual renderers tests are still deterministic
 const now = new Date();
+
+const withContext = (props) => {
+  const t = jest.fn((key) => key);
+  const tDateTimeParser = jest.fn((input) => Dayjs(input));
+  const Component = (
+    <TranslationContext.Provider value={{ t, tDateTimeParser }}>
+      <DateSeparator {...props} />
+    </TranslationContext.Provider>
+  );
+
+  return { Component, t, tDateTimeParser };
+};
 
 describe('DateSeparator', () => {
   it('should use formatDate if it is provided', () => {
@@ -20,9 +37,42 @@ describe('DateSeparator', () => {
     expect(queryByText('the date')).toBeInTheDocument();
   });
 
-  it.todo(
-    "should use tDateTimeParser's calendar method to format dates if formatDate prop is not specified",
-  );
+  it('should render New text if unread prop is true', () => {
+    const { Component, t } = withContext({ date: now, unread: true });
+    const { queryByText } = render(Component);
+
+    expect(queryByText('New')).toBeInTheDocument();
+    expect(t).toHaveBeenCalledWith('New');
+  });
+
+  it('should render properly for unread', () => {
+    const { Component } = withContext({ date: now, unread: true });
+    const tree = renderer.create(Component).toJSON();
+    expect(tree).toMatchInlineSnapshot(`
+      <div
+        className="str-chat__date-separator"
+      >
+        <hr
+          className="str-chat__date-separator-line"
+        />
+        <div
+          className="str-chat__date-separator-date"
+        >
+          New
+        </div>
+      </div>
+    `);
+  });
+
+  it("should use tDateTimeParser's calendar method by default", () => {
+    const { Component, tDateTimeParser } = withContext({ date: now });
+    const { queryByText } = render(Component);
+
+    expect(tDateTimeParser).toHaveBeenCalledWith(now.toISOString());
+    expect(
+      queryByText(Dayjs(now.toISOString()).calendar()),
+    ).toBeInTheDocument();
+  });
 
   describe('Position prop', () => {
     const renderWithPosition = (position) => (
