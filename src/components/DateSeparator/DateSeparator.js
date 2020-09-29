2c2180ed10d0b8afaa90d17779e09e1de5bca010
Amin Mahboubi <amin@getstream.io>
2020-09-29 09:03:24 +0200

    Feature/new messages indicator (#548)
    * DateSeparator supports unread prop
    
    * show New indicator in MessageList for unread msgs
    
    * check for undefined lastRead
    
    Co-authored-by: Vinícius Andrade <vini.andrade.dev@gmail.com>
    
    Co-authored-by: Vinícius Andrade <vini.andrade.dev@gmail.com>

--- src/components/DateSeparator/DateSeparator.js
+++ src/components/DateSeparator/DateSeparator.js
@@ -10,9 +10,13 @@ import { TranslationContext } from '../../context';
  * @example ../../docs/DateSeparator.md
  * @type {React.FC<import('types').DateSeparatorProps>}
  */
-const DateSeparator = ({ position = 'right', formatDate, date }) => {
-  const { tDateTimeParser } = useContext(TranslationContext);
+const DateSeparator = ({ position = 'right', formatDate, date, unread }) => {
+  const { t, tDateTimeParser } = useContext(TranslationContext);
   if (typeof date === 'string') return null;
+
+  const formattedDate = formatDate
+    ? formatDate(date)
+    : tDateTimeParser(date.toISOString()).calendar();
 
   return (
     <div className="str-chat__date-separator">
@@ -20,9 +24,7 @@ const DateSeparator = ({ position = 'right', formatDate, date }) => {
         <hr className="str-chat__date-separator-line" />
       )}
       <div className="str-chat__date-separator-date">
-        {formatDate
-          ? formatDate(date)
-          : tDateTimeParser(date.toISOString()).calendar()}
+        {unread ? t('New') : formattedDate}
       </div>
       {(position === 'left' || position === 'center') && (
         <hr className="str-chat__date-separator-line" />
@@ -34,6 +36,8 @@ const DateSeparator = ({ position = 'right', formatDate, date }) => {
 DateSeparator.propTypes = {
   /** The date to format */
   date: PropTypes.instanceOf(Date).isRequired,
+  /** If following messages are not new */
+  unread: PropTypes.bool,
   /** Set the position of the date in the separator */
   position: PropTypes.oneOf(['left', 'center', 'right']),
   /** Override the default formatting of the date. This is a function that has access to the original date object. Returns a string or Node  */
