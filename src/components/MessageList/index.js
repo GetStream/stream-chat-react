70b6f8eeb65315457f89111b6c486ecf201de281
vishtree <vishalnarkhede.iitd@gmail.com>
2020-10-09 14:45:34 +0200

    Adding livestreammessagelist

--- src/components/MessageList/index.js
+++ src/components/MessageList/index.js
@@ -1,4 +1,5 @@
 export { default as MessageList } from './MessageList';
+export { default as LivestreamMessageList } from './LivestreamMessageList';
 export { default as VirtualizedMessageList } from './VirtualizedMessageList';
 export { default as MessageNotification } from './MessageNotification';
 export { default as ConnectionStatus } from './ConnectionStatus'; // TODO: export this under its own folder
