f4cb24537c5a59f49b21b3e69e4836bce943b83a
Jaap <jaap@getstream.io>
2020-10-14 13:53:41 +0200

    update useMessageNew handler

--- src/components/ChannelList/hooks/useMessageNewListener.js
+++ src/components/ChannelList/hooks/useMessageNewListener.js
@@ -1,8 +1,8 @@
 // @ts-check
-
 import { useEffect, useContext } from 'react';
+import uniqBy from 'lodash.uniqby';
 import { ChatContext } from '../../../context';
-import { moveChannelUp } from '../utils';
+import { moveChannelUp, getChannel } from '../utils';
 
 /**
  * @typedef {React.Dispatch<React.SetStateAction<import('stream-chat').Channel[]>>} SetChannels
@@ -17,7 +17,19 @@ export const useMessageNewListener = (
   useEffect(() => {
     /** @param {import('stream-chat').Event} e */
     const handleEvent = (e) => {
-      setChannels((channels) => {
+      setChannels(async (channels) => {
+        const channelInList =
+          channels.filter((c) => c.cid === e.cid).length > 0;
+        if (!channelInList) {
+          const channel = await getChannel(
+            client,
+            e.channel_type || '',
+            e.channel_id || '',
+          );
+          console.log(channel);
+          return uniqBy([channel, ...channels], 'cid');
+        }
+
         if (!lockChannelOrder) return moveChannelUp(e.cid, channels);
         return channels;
       });
