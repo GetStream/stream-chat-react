6de4b415bed8f87804b26274a82302b8368edc15
Tom Hutman <tom@getstream.io>
2020-10-14 10:59:28 +0200

    CRS-292 Prevent state updates on unmounted Channel component (#566)
    * Add ref to keep track of whether component is mounted. Do not trigger state update if component has already been unmounted.
    
    * Separate logic into hook and add test
    
    Co-authored-by: jaapbakker88 <jaap@getstream.io>

---
+++ src/components/Channel/hooks/useIsMounted.js
@@ -0,0 +1,11 @@
+import { useRef, useEffect } from 'react';
+
+export default () => {
+  const isMounted = useRef(true);
+  useEffect(() => {
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
+  return isMounted;
+};
