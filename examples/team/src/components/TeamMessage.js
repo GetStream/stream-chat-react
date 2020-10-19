090b24433cd8d0a3fea0ae038fb5b7b88ddfb979
Dan Carbonell <dan@getstream.io>
2020-10-19 15:49:01 -0600

    more styling on message component

--- examples/team/src/components/TeamMessage.js
+++ examples/team/src/components/TeamMessage.js
@@ -1,55 +1,23 @@
 import React from 'react';
 import {
-  // Avatar,
   MessageTeam,
+  // TranslationContext
 } from 'stream-chat-react';
 
 import './TeamMessage.css';
 
 export const TeamMessage = (props) => {
   // const { message } = props;
-
-  // const formatTime = (time) => {
-  //   let hours = time.getHours();
-  //   let minutes = time.getMinutes();
-  //   const period = hours > 12 ? 'PM' : 'AM';
-
-  //   if (hours > 12) {
-  //     hours -= 12;
-  //   }
-
-  //   if (String(minutes).length === 1) {
-  //     minutes = `0${minutes}`;
-  //   }
-
-  //   return `${hours}:${minutes} ${period}`;
-  // };
+  // const { tDateTimeParser } = useContext(TranslationContext);
 
   return (
     <div>
-      {/* <p className="team-message__user__time">
-        {formatTime(message.created_at)}
-      </p> */}
-      <MessageTeam {...props} />
+      {/* {message.type === 'regular' && (
+        <p className="team-message__user__time">
+          {tDateTimeParser(message.created_at).format('h:mm A')}
+        </p>
+      )} */}
+      <MessageTeam {...props} formatDate="h:mm A" />
     </div>
   );
 };
-
-/**
- * return (
-    <div className="team-message__container">
-      <Avatar image={message.user.image} size={40} />
-      <div className="team-message__details">
-        <div className="team-message__user">
-          <p className="team-message__user__name">{message.user.name}</p>
-          <p className="team-message__user__time">
-            {formatTime(message.created_at)}
-          </p>
-        </div>
-        <div className="team-message__message__wrapper">
-          <p className="team-message__message__text">{message.text}</p>
-        </div>
-      </div>
-    </div>
-  );
- */
