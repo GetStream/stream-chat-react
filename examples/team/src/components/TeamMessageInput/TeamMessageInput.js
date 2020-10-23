214c9c23c2203f2fa7a4bda4d589951d86996c9e
Dan Carbonell <dan@getstream.io>
2020-10-23 10:07:39 -0600

    add dropzone component

--- examples/team/src/components/TeamMessageInput/TeamMessageInput.js
+++ examples/team/src/components/TeamMessageInput/TeamMessageInput.js
@@ -1,4 +1,5 @@
 import React, { useCallback, useContext, useState } from 'react';
+import { ImageDropzone } from 'react-file-utils';
 import { logChatPromiseExecution } from 'stream-chat';
 import {
   ChannelContext,
@@ -9,6 +10,7 @@ import {
 
 import './TeamMessageInput.css';
 
+import { UploadsPreview } from './UploadsPreview';
 import { TeamTypingIndicator } from '../TeamTypingIndicator/TeamTypingIndicator';
 
 import { BoldIcon } from '../../assets/BoldIcon';
@@ -21,11 +23,23 @@ import { SmileyFace } from '../../assets/SmileyFace';
 import { StrikeThroughIcon } from '../../assets/StrikeThroughIcon';
 
 export const TeamMessageInput = (props) => {
-  const { channel, sendMessage } = useContext(ChannelContext);
+  const {
+    acceptedFiles,
+    channel,
+    maxNumberOfFiles,
+    multipleUploads,
+    sendMessage,
+  } = useContext(ChannelContext);
+
   const [giphyState, setGiphyState] = useState(false);
 
   const overrideSubmitHandler = (message) => {
     let updatedMessage;
+
+    if (message.attachments.length && message.text.startsWith('/giphy')) {
+      const updatedText = message.text.replace('/giphy', '');
+      updatedMessage = { ...message, text: updatedText };
+    }
 
     if (giphyState) {
       const updatedText = `/giphy ${message.text}`;
@@ -57,7 +71,11 @@ export const TeamMessageInput = (props) => {
         setGiphyState(false);
       }
 
-      if (messageInput.text.startsWith('/giphy') && !giphyState) {
+      if (
+        !giphyState &&
+        messageInput.text.startsWith('/giphy') &&
+        !messageInput.numberOfUploads
+      ) {
         e.target.value = e.target.value.replace('/giphy', '');
         setGiphyState(true);
       }
@@ -76,50 +94,61 @@ export const TeamMessageInput = (props) => {
 
   return (
     <div className="team-message-input__wrapper">
-      <div className="team-message-input__input">
-        <div className="team-message-input__top">
-          {giphyState && <GiphyIcon />}
-          <ChatAutoComplete
-            commands={messageInput.getCommands()}
-            innerRef={messageInput.textareaRef}
-            handleSubmit={messageInput.handleSubmit}
-            onSelectItem={messageInput.onSelectItem}
-            onChange={onChange}
-            value={messageInput.text}
-            rows={1}
-            maxRows={props.maxRows}
-            placeholder={`Message ${getPlaceholder()}`}
-            onPaste={messageInput.onPaste}
-            triggers={props.autocompleteTriggers}
-            grow={props.grow}
-            disabled={props.disabled}
-            additionalTextareaProps={{
-              ...props.additionalTextareaProps,
-            }}
-          />
-          <div
-            className="team-message-input__button"
-            role="button"
-            aria-roledescription="button"
-            onClick={messageInput.handleSubmit}
-          >
-            <SendButton />
+      <ImageDropzone
+        accept={acceptedFiles}
+        handleFiles={messageInput.uploadNewFiles}
+        multiple={multipleUploads}
+        disabled={
+          (maxNumberOfFiles !== undefined &&
+            messageInput.numberOfUploads >= maxNumberOfFiles) ||
+          giphyState
+        }
+      >
+        <div className="team-message-input__input">
+          <div className="team-message-input__top">
+            {giphyState && !messageInput.numberOfUploads && <GiphyIcon />}
+            <UploadsPreview {...messageInput} />
+            <ChatAutoComplete
+              innerRef={messageInput.textareaRef}
+              handleSubmit={messageInput.handleSubmit}
+              onSelectItem={messageInput.onSelectItem}
+              onChange={onChange}
+              value={messageInput.text}
+              rows={1}
+              maxRows={props.maxRows}
+              placeholder={`Message ${getPlaceholder()}`}
+              onPaste={messageInput.onPaste}
+              triggers={props.autocompleteTriggers}
+              grow={props.grow}
+              disabled={props.disabled}
+              additionalTextareaProps={{
+                ...props.additionalTextareaProps,
+              }}
+            />
+            <div
+              className="team-message-input__button"
+              role="button"
+              aria-roledescription="button"
+              onClick={messageInput.handleSubmit}
+            >
+              <SendButton />
+            </div>
+          </div>
+          <div className="team-message-input__bottom">
+            <div className="team-message-input__icons">
+              <SmileyFace openEmojiPicker={messageInput.openEmojiPicker} />
+              <LightningBolt />
+              <div className="icon-divider"></div>
+              <BoldIcon />
+              <ItalicsIcon />
+              <StrikeThroughIcon />
+              <CodeSnippet />
+            </div>
           </div>
         </div>
-        <div className="team-message-input__bottom">
-          <div className="team-message-input__icons">
-            <SmileyFace openEmojiPicker={messageInput.openEmojiPicker} />
-            <LightningBolt />
-            <div className="icon-divider"></div>
-            <BoldIcon />
-            <ItalicsIcon />
-            <StrikeThroughIcon />
-            <CodeSnippet />
-          </div>
-        </div>
-      </div>
-      <EmojiPicker {...messageInput} />
-      <TeamTypingIndicator type="input" />
+        <EmojiPicker {...messageInput} />
+        <TeamTypingIndicator type="input" />
+      </ImageDropzone>
     </div>
   );
 };
