903354f9b08545db695491f76f5d3e80560b94ec
Dan Carbonell <dan@getstream.io>
2020-10-26 16:51:45 -0600

    convert App component and add custom channel header

--- examples/customer support/src/App.js
+++ examples/customer support/src/App.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import {
   Chat,
@@ -8,103 +7,80 @@ import {
   MessageInput,
   MessageInputFlat,
   MessageCommerce,
-  ChannelHeader,
   Window,
 } from 'stream-chat-react';
 import 'stream-chat-react/dist/css/index.css';
+
 import './App.css';
 
+import { SupportChannelHeader } from './components/ChannelHeader/SupportChannelHeader';
+
+import { ToggleButton } from './assets/ToggleButton';
+
 const urlParams = new URLSearchParams(window.location.search);
-// const user =
-//   urlParams.get('user') || process.env.REACT_APP_CHAT_API_DEFAULT_USER;
+const apiKey = urlParams.get('apikey') || 'qk4nn7rpcn75';
+const user = urlParams.get('user') || 'example-user';
 const theme = urlParams.get('theme') || 'light';
-const channelName = urlParams.get('channel') || 'demo';
-// const userToken =
-//   urlParams.get('user_token') ||
-//   process.env.REACT_APP_CHAT_API_DEFAULT_USER_TOKEN;
+const channelName = urlParams.get('channel') || 'support-demo';
+const userToken =
+  urlParams.get('user_token') ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZXhhbXBsZS11c2VyIn0.HlC0dMKL43y3K_XbfvQS_Yc3V314HU4Z7LrBLil777g';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.chatClient = new StreamChat('qk4nn7rpcn75');
-    // this.chatClient.setBaseURL(process.env.REACT_APP_CHAT_SERVER_ENDPOINT);
-    this.chatClient.setUser(
-      {
-        id: 'example-user',
-      },
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZXhhbXBsZS11c2VyIn0.HlC0dMKL43y3K_XbfvQS_Yc3V314HU4Z7LrBLil777g',
-    );
-    this.channel = this.chatClient.channel('commerce', channelName, {
-      image: 'https://i.stack.imgur.com/e7G42m.jpg',
-      name: 'Hello 👋',
-      subtitle: 'Chat with us about NASA stuff!',
-      example: 1,
-    });
+const App = () => {
+  const [channel, setChannel] = useState();
+  const [open, setOpen] = useState(false);
 
-    this.channel.watch();
+  const chatClient = new StreamChat(apiKey);
+  chatClient.setUser({ id: user }, userToken);
 
-    this.state = {
-      open: true,
+  useEffect(() => {
+    const getChannel = async () => {
+      const [existingChannel] = await chatClient.queryChannels({
+        id: channelName,
+      });
+
+      if (existingChannel) await existingChannel.delete();
+
+      const newChannel = chatClient.channel('commerce', channelName, {
+        image: 'https://i.stack.imgur.com/e7G42m.jpg',
+        name: 'Hello',
+        subtitle: 'We are here to help.',
+        example: 1,
+      });
+
+      await newChannel.watch();
+
+      setChannel(newChannel);
+      setOpen(true);
     };
-  }
 
-  toggleDemo = () => {
-    if (this.state.open) {
-      this.setState({ open: false });
-    } else {
-      this.setState({ open: true });
+    if (chatClient) {
+      getChannel();
     }
-  };
+  }, []); // eslint-disable-line
 
-  render() {
-    return (
-      <>
-        <div className={`wrapper ${this.state.open ? 'wrapper--open' : ''}`}>
-          <Chat client={this.chatClient} theme={`commerce ${theme}`}>
-            <Channel
-              channel={this.channel}
-              onMentionsHover={(e, user) => console.log(e, user)}
-              onMentionsClick={(e, user) => console.log(e, user)}
-            >
-              <Window>
-                <ChannelHeader />
-                {this.state.open && <MessageList Message={MessageCommerce} />}
-                <MessageInput
-                  onFocus={!this.state.open ? this.toggleDemo : null}
-                  Input={MessageInputFlat}
-                  focus
-                />
-              </Window>
-            </Channel>
-          </Chat>
-          <Button onClick={this.toggleDemo} open={this.state.open} />
-        </div>
-      </>
-    );
-  }
-}
+  const toggleDemo = () => setOpen(!open);
+
+  return (
+    <div className={`wrapper ${open ? 'wrapper--open' : ''}`}>
+      <Chat client={chatClient} theme={`commerce ${theme}`}>
+        {channel && (
+          <Channel channel={channel}>
+            <Window>
+              <SupportChannelHeader />
+              {open && <MessageList Message={MessageCommerce} />}
+              <MessageInput
+                onFocus={!open ? toggleDemo : null}
+                Input={MessageInputFlat}
+                focus
+              />
+            </Window>
+          </Channel>
+        )}
+      </Chat>
+      <ToggleButton onClick={toggleDemo} open={open} />
+    </div>
+  );
+};
 
 export default App;
-
-const Button = ({ open, onClick }) => (
-  <div
-    onClick={onClick}
-    className={`button ${open ? 'button--open' : 'button--closed'}`}
-  >
-    {open ? (
-      <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M19.333 2.547l-1.88-1.88L10 8.12 2.547.667l-1.88 1.88L8.12 10 .667 17.453l1.88 1.88L10 11.88l7.453 7.453 1.88-1.88L11.88 10z"
-          fillRule="evenodd"
-        />
-      </svg>
-    ) : (
-      <svg width="24" height="20" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M.011 20L24 10 .011 0 0 7.778 17.143 10 0 12.222z"
-          fillRule="evenodd"
-        />
-      </svg>
-    )}
-  </div>
-);
---
+++ examples/customer support/src/components/ChannelHeader/SupportChannelHeader.js
@@ -0,0 +1,52 @@
+/* eslint-disable global-require */
+import React, { useContext } from 'react';
+import { ChannelContext } from 'stream-chat-react';
+
+import './SupportChannelHeader.css';
+
+import { ClockIcon } from '../../assets/ClockIcon';
+
+export const SupportChannelHeader = () => {
+  const { channel } = useContext(ChannelContext);
+
+  return (
+    <div className="channel-header__container">
+      <div className="channel-header__heading">
+        <div style={{ width: '115px' }}>
+          <img
+            className="channel-header__image-3"
+            src={require('../../assets/user3.png')}
+            alt="user-3"
+            height="64"
+            width="64"
+          />
+          <img
+            className="channel-header__image-2"
+            src={require('../../assets/user2.png')}
+            alt="user-3"
+            height="64"
+            width="64"
+          />
+          <img
+            className="channel-header__image-1"
+            src={require('../../assets/user1.png')}
+            alt="user-3"
+            height="64"
+            width="64"
+          />
+          <div className="channel-header__active" />
+        </div>
+        <div className="channel-header__text">
+          <p className="channel-header__name">{channel.data.name}</p>
+          <p className="channel-header__subtitle">{channel.data.subtitle}</p>
+        </div>
+      </div>
+      <div className="channel-header__wait__wrapper">
+        <ClockIcon />
+        <p className="channel-header__wait__text">
+          Average wait time: <b>2 minutes</b>
+        </p>
+      </div>
+    </div>
+  );
+};
