0c8dd4feb376a9012dff417b2c89ef47be5df263
Dan Carbonell <dan@getstream.io>
2020-10-26 11:24:48 -0600

    remove messaging example code

--- examples/team/messaging/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as MessagingInput } from './MessagingInput';
-export { default as MessagingChannelHeader } from './MessagingChannelHeader';
-export { default as MessagingChannelList } from './MessagingChannelList';
-export { default as MessagingCreateChannel } from './MessagingCreateChannel';
