2385b7376a54d73ac09d45332b3544880bfcf827
Grant Steinke <grantjsteinke@gmail.com>
2021-01-21 16:01:36 -0700

    adds prop type

--- src/components/ChannelList/ChannelList.js
+++ src/components/ChannelList/ChannelList.js
@@ -357,6 +357,10 @@ ChannelList.propTypes = {
    * */
   onChannelDeleted: PropTypes.func,
   /**
+   * Optional function for custom filtering channels.
+   * */
+  filterFunction: PropTypes.func,
+  /**
    * Object containing query filters
    * @see See [Channel query documentation](https://getstream.io/chat/docs/query_channels/?language=js) for a list of available fields for filter.
    * */
