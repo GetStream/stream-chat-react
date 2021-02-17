5402debf7eced3edca28f0f6d915dd50c8417716
Grant Steinke <grantjsteinke@gmail.com>
2021-02-17 11:23:47 -0700

    converts Chat to TS

---
+++ src/components/Chat/Chat.tsx
@@ -0,0 +1,134 @@
+import React, { PropsWithChildren } from 'react';
+
+import { useChat } from './hooks/useChat';
+
+import { ChatProvider } from '../../context/ChatContext';
+import { TranslationProvider } from '../../context/TranslationContext';
+
+import type { StreamChat } from 'stream-chat';
+import type { Streami18n } from '../../i18n/Streami18n';
+import type {
+  DefaultAttachmentType,
+  DefaultChannelType,
+  DefaultCommandType,
+  DefaultEventType,
+  DefaultMessageType,
+  DefaultReactionType,
+  DefaultUserType,
+  UnknownType,
+} from '../../../types/types';
+
+export type ChatProps<
+  At extends UnknownType = DefaultAttachmentType,
+  Ch extends UnknownType = DefaultChannelType,
+  Co extends string = DefaultCommandType,
+  Ev extends UnknownType = DefaultEventType,
+  Me extends UnknownType = DefaultMessageType,
+  Re extends UnknownType = DefaultReactionType,
+  Us extends UnknownType = DefaultUserType
+> = {
+  /** The StreamChat client object */
+  client: StreamChat<At, Ch, Co, Ev, Me, Re, Us>;
+  /** Instance of Stream i18n */
+  i18nInstance: Streami18n;
+  /** navOpen initial status */
+  initialNavOpen?: boolean;
+  /**
+   *
+   * Theme could be used for custom styling of the components.
+   *
+   * You can override the classes used in our components under parent theme class.
+   *
+   * e.g. If you want to build a theme where background of message is black
+   *
+   * ```
+   *  <Chat client={client} theme={demo}>
+   *    <Channel>
+   *      <MessageList />
+   *    </Channel>
+   *  </Chat>
+   * ```
+   *
+   * ```scss
+   *  .demo.str-chat {
+   *    .str-chat__message-simple {
+   *      &-text-inner {
+   *        background-color: black;
+   *      }
+   *    }
+   *  }
+   * ```
+   *
+   * Built in available themes:
+   *
+   *  - `messaging light`
+   *  - `messaging dark`
+   *  - `team light`
+   *  - `team dark`
+   *  - `commerce light`
+   *  - `commerce dark`
+   *  - `livestream light`
+   *  - `livestream dark`
+   */
+  theme?: string;
+};
+
+/**
+ *
+ * Chat - Wrapper component for Chat. The needs to be placed around any other chat components.
+ * This Chat component provides the ChatContext to all other components.
+ *
+ * It also exposes the withChatContext HOC which you can use to consume the ChatContext
+ *
+ * @example ../../docs/Chat.md
+ */
+const Chat = <
+  At extends UnknownType = DefaultAttachmentType,
+  Ch extends UnknownType = DefaultChannelType,
+  Co extends string = DefaultCommandType,
+  Ev extends UnknownType = DefaultEventType,
+  Me extends UnknownType = DefaultMessageType,
+  Re extends UnknownType = DefaultReactionType,
+  Us extends UnknownType = DefaultUserType
+>(
+  props: PropsWithChildren<ChatProps<At, Ch, Co, Ev, Me, Re, Us>>,
+) => {
+  const {
+    children,
+    client,
+    i18nInstance,
+    initialNavOpen = true,
+    theme = 'messaging light',
+  } = props;
+
+  const {
+    channel,
+    closeMobileNav,
+    mutes,
+    navOpen,
+    openMobileNav,
+    setActiveChannel,
+    translators,
+  } = useChat({ client, i18nInstance, initialNavOpen });
+
+  if (!translators.t) return null;
+
+  return (
+    <ChatProvider<At, Ch, Co, Ev, Me, Re, Us>
+      value={{
+        channel,
+        client,
+        closeMobileNav,
+        mutes,
+        navOpen,
+        openMobileNav,
+        setActiveChannel,
+        theme,
+      }}
+    >
+      <TranslationProvider value={translators}>{children}</TranslationProvider>
+    </ChatProvider>
+  );
+};
+
+export default Chat;
