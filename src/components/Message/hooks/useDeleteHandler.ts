0843b8199498e43b9df0a7c69cc5d6db127618dd
Dan Carbonell <dan1525@gmail.com>
2021-02-25 13:47:22 -0700

    type useDeleteHandler

--- src/components/Message/hooks/useDeleteHandler.ts
+++ src/components/Message/hooks/useDeleteHandler.ts
@@ -1,17 +1,50 @@
-// @ts-check
-import { MouseEvent, useContext } from 'react';
-import { ChannelContext } from '../../../context';
+import { useChannelContext } from '../../../context/ChannelContext';
+
+import type { MouseEvent } from 'react';
 import type { MessageResponse } from 'stream-chat';
 
-export const useDeleteHandler = (
-  message: MessageResponse | undefined,
-): ((event: MouseEvent<HTMLElement>) => Promise<void>) => {
-  const { client, updateMessage } = useContext(ChannelContext);
+import type {
+  DefaultAttachmentType,
+  DefaultChannelType,
+  DefaultCommandType,
+  DefaultEventType,
+  DefaultMessageType,
+  DefaultReactionType,
+  DefaultUserType,
+  UnknownType,
+} from '../../../../types/types';
+
+export type DeleteHandlerReturnType = (
+  event: MouseEvent<HTMLElement>,
+) => Promise<void>;
+
+export const useDeleteHandler = <
+  At extends UnknownType = DefaultAttachmentType,
+  Ch extends UnknownType = DefaultChannelType,
+  Co extends string = DefaultCommandType,
+  Ev extends UnknownType = DefaultEventType,
+  Me extends UnknownType = DefaultMessageType,
+  Re extends UnknownType = DefaultReactionType,
+  Us extends UnknownType = DefaultUserType
+>(
+  message?: MessageResponse<At, Ch, Co, Me, Re, Us>,
+): DeleteHandlerReturnType => {
+  const { client, updateMessage } = useChannelContext<
+    At,
+    Ch,
+    Co,
+    Ev,
+    Me,
+    Re,
+    Us
+  >();
+
   return async (event) => {
     event.preventDefault();
     if (!message?.id || !client || !updateMessage) {
       return;
     }
+
     const data = await client.deleteMessage(message.id);
     updateMessage(data.message);
   };
