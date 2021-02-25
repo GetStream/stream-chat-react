b25e3d639ed09d67f8ac198113534b9a9e444388
Dan Carbonell <dan1525@gmail.com>
2021-02-25 13:33:56 -0700

    type useActionHandler

--- src/components/Message/hooks/useActionHandler.ts
+++ src/components/Message/hooks/useActionHandler.ts
@@ -1,9 +1,23 @@
-import { BaseSyntheticEvent, useContext } from 'react';
-import { ChannelContext } from '../../../context';
+import { useChannelContext } from '../../../context/ChannelContext';
+
+import type { BaseSyntheticEvent } from 'react';
 import type { MessageResponse } from 'stream-chat';
 
+import type {
+  DefaultAttachmentType,
+  DefaultChannelType,
+  DefaultCommandType,
+  DefaultEventType,
+  DefaultMessageType,
+  DefaultReactionType,
+  DefaultUserType,
+  UnknownType,
+} from '../../../../types/types';
+
+export type FormData = Record<string, string>;
+
 export type ActionHandlerReturnType = (
-  dataOrName: string | Record<string, string>,
+  dataOrName: string | FormData,
   value?: string,
   event?: BaseSyntheticEvent,
 ) => Promise<void>;
@@ -11,24 +25,42 @@ export type ActionHandlerReturnType = (
 export const handleActionWarning = `Action handler was called, but it is missing one of its required arguments.
       Make sure the ChannelContext was properly set and that this hook was initialized with a valid message.`;
 
-export function useActionHandler(
-  message: MessageResponse | undefined,
-): ActionHandlerReturnType {
-  const { channel, removeMessage, updateMessage } = useContext(ChannelContext);
+export function useActionHandler<
+  At extends UnknownType = DefaultAttachmentType,
+  Ch extends UnknownType = DefaultChannelType,
+  Co extends string = DefaultCommandType,
+  Ev extends UnknownType = DefaultEventType,
+  Me extends UnknownType = DefaultMessageType,
+  Re extends UnknownType = DefaultReactionType,
+  Us extends UnknownType = DefaultUserType
+>(message?: MessageResponse<At, Ch, Co, Me, Re, Us>): ActionHandlerReturnType {
+  const { channel, removeMessage, updateMessage } = useChannelContext<
+    At,
+    Ch,
+    Co,
+    Ev,
+    Me,
+    Re,
+    Us
+  >();
+
   return async (dataOrName, value, event) => {
     if (event) event.preventDefault();
+
     if (!message || !updateMessage || !removeMessage || !channel) {
       console.warn(handleActionWarning);
       return;
     }
-    const messageID = message.id;
 
-    let formData: Record<string, string> = {};
+    const messageID = message.id;
+    let formData: FormData = {};
 
     // deprecated: value&name should be removed in favor of data obj
     if (typeof dataOrName === 'string') {
       formData[dataOrName] = value as string;
-    } else formData = { ...dataOrName };
+    } else {
+      formData = { ...dataOrName };
+    }
 
     if (messageID) {
       const data = await channel.sendAction(messageID, formData);
