6d9c850b75854fe490f36e3c5a809abf74e40f8f
Grant Steinke <grantjsteinke@gmail.com>
2021-03-03 16:01:28 -0700

    adds utils file with type guard

---
+++ src/components/Reactions/utils/utils.ts
@@ -0,0 +1,8 @@
+export const isMutableRef = <T>(
+  ref: React.ForwardedRef<T> | null,
+): ref is React.MutableRefObject<T> => {
+  if (ref) {
+    return (ref as React.MutableRefObject<T>).current !== undefined;
+  }
+  return false;
+};
