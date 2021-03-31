d8c7366e0d9b74400f4f0f1ba26cd592dee700ff
Dan Carbonell <dan1525@gmail.com>
2021-03-31 11:46:59 -0600

    create ChannelStateContext

---
+++ src/context/ChannelStateContext.tsx
@@ -0,0 +1,201 @@
+import React, { PropsWithChildren, useContext } from 'react';
+
+import type {
+  Data as EmojiMartData,
+  NimbleEmojiIndex,
+  NimbleEmojiProps,
+  NimblePickerProps,
+} from 'emoji-mart';
+import type {
+  Channel,
+  MessageResponse,
+  Mute,
+  ChannelState as StreamChannelState,
+} from 'stream-chat';
+
+import type {
+  DefaultAttachmentType,
+  DefaultChannelType,
+  DefaultCommandType,
+  DefaultEventType,
+  DefaultMessageType,
+  DefaultReactionType,
+  DefaultUserType,
+  UnknownType,
+} from '../../types/types';
+
+export type ChannelNotifications = Array<{
+  id: string;
+  text: string;
+  type: 'success' | 'error';
+}>;
+
+export type CommonEmoji = {
+  custom: boolean;
+  emoticons: [];
+  short_names: [];
+};
+
+export type EmojiSetDef = {
+  sheetColumns: number;
+  sheetRows: number;
+  sheetSize: number;
+  size: number;
+  spriteUrl: string;
+};
+
+export type MinimalEmoji = CommonEmoji &
+  EmojiSetDef & {
+    colons: string;
+    id: string;
+    name: string;
+    sheet_x: number;
+    sheet_y: number;
+  };
+
+export type EmojiConfig = {
+  commonEmoji: CommonEmoji;
+  defaultMinimalEmojis: MinimalEmoji[];
+  Emoji: React.ComponentType<NimbleEmojiProps>;
+  emojiData: EmojiMartData;
+  EmojiIndex: NimbleEmojiIndex;
+  EmojiPicker: React.ComponentType<NimblePickerProps>;
+  emojiSetDef: EmojiSetDef;
+};
+
+export type StreamMessage<
+  At extends DefaultAttachmentType = DefaultAttachmentType,
+  Ch extends DefaultChannelType = DefaultChannelType,
+  Co extends DefaultCommandType = DefaultCommandType,
+  Ev extends DefaultEventType = DefaultEventType,
+  Me extends DefaultMessageType = DefaultMessageType,
+  Re extends DefaultReactionType = DefaultReactionType,
+  Us extends DefaultUserType<Us> = DefaultUserType
+> =
+  | ReturnType<StreamChannelState<At, Ch, Co, Ev, Me, Re, Us>['formatMessage']>
+  | MessageResponse<At, Ch, Co, Me, Re, Us>;
+
+export type ChannelState<
+  At extends DefaultAttachmentType = DefaultAttachmentType,
+  Ch extends DefaultChannelType = DefaultChannelType,
+  Co extends DefaultCommandType = DefaultCommandType,
+  Ev extends DefaultEventType = DefaultEventType,
+  Me extends DefaultMessageType = DefaultMessageType,
+  Re extends DefaultReactionType = DefaultReactionType,
+  Us extends DefaultUserType<Us> = DefaultUserType
+> = {
+  error?: Error | null;
+  hasMore?: boolean;
+  loading?: boolean;
+  loadingMore?: boolean;
+  members?: StreamChannelState<At, Ch, Co, Ev, Me, Re, Us>['members'];
+  messages?: StreamMessage<At, Ch, Co, Ev, Me, Re, Us>[];
+  pinnedMessages?: StreamMessage<At, Ch, Co, Ev, Me, Re, Us>[];
+  read?: StreamChannelState<At, Ch, Co, Ev, Me, Re, Us>['read'];
+  thread?: StreamMessage<At, Ch, Co, Ev, Me, Re, Us> | null;
+  threadHasMore?: boolean;
+  threadLoadingMore?: boolean;
+  threadMessages?: StreamMessage<At, Ch, Co, Ev, Me, Re, Us>[];
+  typing?: StreamChannelState<At, Ch, Co, Ev, Me, Re, Us>['typing'];
+  watcherCount?: number;
+  watchers?: StreamChannelState<At, Ch, Co, Ev, Me, Re, Us>['watchers'];
+};
+
+export type ChannelStateContextValue<
+  At extends DefaultAttachmentType = DefaultAttachmentType,
+  Ch extends DefaultChannelType = DefaultChannelType,
+  Co extends DefaultCommandType = DefaultCommandType,
+  Ev extends DefaultEventType = DefaultEventType,
+  Me extends DefaultMessageType = DefaultMessageType,
+  Re extends DefaultReactionType = DefaultReactionType,
+  Us extends DefaultUserType<Us> = DefaultUserType
+> = ChannelState<At, Ch, Co, Ev, Me, Re, Us> & {
+  channel: Channel<At, Ch, Co, Ev, Me, Re, Us>;
+  notifications: ChannelNotifications;
+  acceptedFiles?: string[];
+  emojiConfig?: EmojiConfig;
+  maxNumberOfFiles?: number;
+  multipleUploads?: boolean;
+  mutes?: Mute<Us>[];
+  watcher_count?: number;
+};
+
+export const ChannelStateContext = React.createContext<ChannelStateContextValue>(
+  {} as ChannelStateContextValue,
+);
+
+export const ChannelStateProvider = <
+  At extends DefaultAttachmentType = DefaultAttachmentType,
+  Ch extends DefaultChannelType = DefaultChannelType,
+  Co extends DefaultCommandType = DefaultCommandType,
+  Ev extends DefaultEventType = DefaultEventType,
+  Me extends DefaultMessageType = DefaultMessageType,
+  Re extends DefaultReactionType = DefaultReactionType,
+  Us extends DefaultUserType<Us> = DefaultUserType
+>({
+  children,
+  value,
+}: PropsWithChildren<{
+  value: ChannelStateContextValue<At, Ch, Co, Ev, Me, Re, Us>;
+}>) => (
+  <ChannelStateContext.Provider
+    value={(value as unknown) as ChannelStateContextValue}
+  >
+    {children}
+  </ChannelStateContext.Provider>
+);
+
+export const useChannelStateContext = <
+  At extends DefaultAttachmentType = DefaultAttachmentType,
+  Ch extends DefaultChannelType = DefaultChannelType,
+  Co extends DefaultCommandType = DefaultCommandType,
+  Ev extends DefaultEventType = DefaultEventType,
+  Me extends DefaultMessageType = DefaultMessageType,
+  Re extends DefaultReactionType = DefaultReactionType,
+  Us extends DefaultUserType<Us> = DefaultUserType
+>() =>
+  (useContext(ChannelStateContext) as unknown) as ChannelStateContextValue<
+    At,
+    Ch,
+    Co,
+    Ev,
+    Me,
+    Re,
+    Us
+  >;
+
+/**
+ * Typescript currently does not support partial inference, so if ChannelStateContext
+ * typing is desired while using the HOC withChannelStateContext, the Props for the
+ * wrapped component must be provided as the first generic.
+ */
+export const withChannelStateContext = <
+  P extends UnknownType,
+  At extends DefaultAttachmentType = DefaultAttachmentType,
+  Ch extends DefaultChannelType = DefaultChannelType,
+  Co extends DefaultCommandType = DefaultCommandType,
+  Ev extends DefaultEventType = DefaultEventType,
+  Me extends DefaultMessageType = DefaultMessageType,
+  Re extends DefaultReactionType = DefaultReactionType,
+  Us extends DefaultUserType<Us> = DefaultUserType
+>(
+  Component: React.ComponentType<P>,
+): React.FC<
+  Omit<P, keyof ChannelStateContextValue<At, Ch, Co, Ev, Me, Re, Us>>
+> => {
+  const WithChannelStateContextComponent = (
+    props: Omit<P, keyof ChannelStateContextValue<At, Ch, Co, Ev, Me, Re, Us>>,
+  ) => {
+    const channelContext = useChannelStateContext<At, Ch, Co, Ev, Me, Re, Us>();
+
+    return <Component {...(props as P)} {...channelContext} />;
+  };
+
+  WithChannelStateContextComponent.displayName = (
+    Component.displayName ||
+    Component.name ||
+    'Component'
+  ).replace('Base', '');
+
+  return WithChannelStateContextComponent;
+};
