1f77beda969960aa381383a7df1fcd3fb244a64c
Dan Carbonell <dan1525@gmail.com>
2021-04-21 11:29:31 -0600

    adjust pin permissions and tests

--- src/components/Message/utils.tsx
+++ src/components/Message/utils.tsx
@@ -87,7 +87,7 @@ export const defaultPinPermissions: PinPermissions = {
     guest: false,
     member: false,
     moderator: true,
-    owner: false,
+    owner: true,
     user: false,
   },
   gaming: {
