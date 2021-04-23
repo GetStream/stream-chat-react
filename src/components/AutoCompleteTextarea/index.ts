229f3794b92eaff572498e12b184ca665eca80f3
Tom Hutman <tom@getstream.io>
2021-04-23 17:25:24 +0200

    convert to ts because we're importing/exporting a type as well

--- src/components/AutoCompleteTextarea/index.ts
+++ src/components/AutoCompleteTextarea/index.ts
@@ -1,6 +1,7 @@
 export { default as DefaultSuggestionListItem } from './Item';
 export { default as DefaultuggestionList } from './List';
-export { DefaultSuggestionListHeader, SuggestionListHeaderProps } from './Header';
+export { DefaultSuggestionListHeader } from './Header';
+export type { SuggestionListHeaderProps } from './Header';
 export { default as listener, KEY_CODES } from './listener';
 export { default as AutoCompleteTextarea } from './Textarea';
 export { defaultScrollToItem } from './utils';
