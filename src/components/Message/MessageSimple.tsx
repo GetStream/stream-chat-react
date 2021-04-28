0d3c5f18523530b46d4b4b5f1e811278bbb4d7a7
vishtree <vishalnarkhede.iitd@gmail.com>
2021-04-28 18:41:21 +0200

    user.updated and user.deleted handlers

--- src/components/Message/MessageSimple.tsx
+++ src/components/Message/MessageSimple.tsx
@@ -111,7 +111,7 @@ const MessageSimpleWithContext = <
     return null;
   }
 
-  if (message.deleted_at) {
+  if (message.deleted_at || message.type === 'deleted') {
     return <MessageDeleted message={message} />;
   }
 
