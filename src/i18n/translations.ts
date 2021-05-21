1a4fe99f53362ccfc59e363817f2ea3d9e8495f3
Dan Carbonell <dan1525@gmail.com>
2021-05-21 09:35:23 -0600

    add german translations

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -1,21 +1,23 @@
+import deTranslations from './de.json';
 import enTranslations from './en.json';
 import esTranslations from './es.json';
-import nlTranslations from './nl.json';
-import ruTranslations from './ru.json';
-import trTranslations from './tr.json';
-import ptTranslations from './pt.json';
 import frTranslations from './fr.json';
 import hiTranslations from './hi.json';
 import itTranslations from './it.json';
+import nlTranslations from './nl.json';
+import ptTranslations from './pt.json';
+import ruTranslations from './ru.json';
+import trTranslations from './tr.json';
 
 export {
+  deTranslations,
   enTranslations,
   esTranslations,
-  nlTranslations,
-  ruTranslations,
-  trTranslations,
-  ptTranslations,
   frTranslations,
   hiTranslations,
   itTranslations,
+  nlTranslations,
+  ptTranslations,
+  ruTranslations,
+  trTranslations,
 };
