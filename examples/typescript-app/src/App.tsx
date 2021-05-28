209d882c780ce4a7848f5aae8ba15e8abcbe4cd9
Dan Carbonell <dan1525@gmail.com>
2021-05-28 09:55:09 -0600

    remove App change

--- examples/typescript-app/src/App.tsx
+++ examples/typescript-app/src/App.tsx
@@ -48,7 +48,7 @@ const App = () => (
       <Window>
         <ChannelHeader />
         <MessageList />
-        <MessageInput disableMentions Input={MessageInputFlat} focus />
+        <MessageInput Input={MessageInputFlat} focus />
       </Window>
       <Thread />
     </Channel>
