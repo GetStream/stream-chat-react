25e0cf94769759c6911244988c6bd2651a399f6e
Anna Baldwin <anna@getstream.io>
2021-06-11 12:18:33 -0600

    remove example app files from PR

--- examples/typescript-app/src/App.tsx
+++ examples/typescript-app/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-// import { LoadingIndicatorProps } from 'react-file-utils/dist/types/components/LoadingIndicator';
 import { ChannelSort, StreamChat } from 'stream-chat';
 import {
   Chat,
   Channel,
   ChannelHeader,
   ChannelList,
-  // ChannelListMessenger,
-  // ChannelPreviewMessenger,
+  ChannelListMessenger,
+  ChannelPreviewMessenger,
   MessageList,
   MessageInput,
   MessageInputFlat,
   Thread,
   Window,
-  ChatDownProps,
-  // ChatDownProps,
 } from 'stream-chat-react';
 import 'stream-chat-react/dist/css/index.css';
 import './App.css';
-import { CustomList } from './components/CustomList';
 
 const apiKey = process.env.REACT_APP_STREAM_KEY as string;
 const userId = process.env.REACT_APP_USER_ID as string;
@@ -37,39 +33,25 @@ if (process.env.REACT_APP_CHAT_SERVER_ENDPOINT) {
 
 chatClient.connectUser({ id: userId }, userToken);
 
-const App = () => {
-  const CustomErrorIndicator = (props: ChatDownProps) => {
-    const {
-      text
-    } = props
-  
-    console.log('props HERE:', props);
-  
-    return <div>{text}</div>
-  };
-
-  return (
-    <Chat client={chatClient} theme={`messaging ${theme}`}>
-      <ChannelList
-        List={CustomList}
-        filters={filters}
-        sort={sort}
-        options={options}
-        showChannelSearch
-        LoadingErrorIndicator={() => (
-          <CustomErrorIndicator text={'Loading Error - check your connection.'} type={'connection'} />
-        )}
-      />
-      <Channel>
-        <Window>
-          <ChannelHeader />
-          <MessageList />
-          <MessageInput Input={MessageInputFlat} focus />
-        </Window>
-        <Thread />
-      </Channel>
-    </Chat>
-  )
-};
+const App = () => (
+  <Chat client={chatClient} theme={`messaging ${theme}`}>
+    <ChannelList
+      List={ChannelListMessenger}
+      Preview={ChannelPreviewMessenger}
+      filters={filters}
+      sort={sort}
+      options={options}
+      showChannelSearch
+    />
+    <Channel>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput Input={MessageInputFlat} focus />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
