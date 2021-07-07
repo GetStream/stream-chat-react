e74ccac1520965a73b01ef0092594513a1351d42
Grant Steinke <grantjsteinke@gmail.com>
2021-07-07 14:00:47 -0600

    css updates

--- examples/typescript-app/src/components/CustomPinMessage.tsx
+++ examples/typescript-app/src/components/CustomPinMessage.tsx
@@ -4,27 +4,36 @@ import {
   MessageRepliesCountButton,
   MessageStatus,
   MessageText,
+  ReactionSelector,
   useMessageContext,
 } from 'stream-chat-react';
 
 import './CustomPinMessage.css';
 
 export const CustomPinMessage = () => {
-  const { message } = useMessageContext();
+  const {
+    message,
+    showDetailedReactions,
+    isReactionEnabled,
+    reactionSelectorRef,
+  } = useMessageContext();
 
   const messageWrapperRef = useRef<HTMLDivElement>(null);
 
   return (
     <div className='pin-message-wrapper'>
       <div className='pin-message-wrapper-content'>
-        <MessageOptions displayLeft={true} messageWrapperRef={messageWrapperRef} />
         <div className='pin-message-header'>
-          <div className='pin-message-header-name'>{message.user?.name}</div>
+          <div className='pin-message-header-name'>{message.user?.name || message.user?.id}</div>
         </div>
+        {showDetailedReactions && isReactionEnabled && (
+          <ReactionSelector ref={reactionSelectorRef} />
+        )}
         <MessageText />
         <MessageStatus />
         <MessageRepliesCountButton reply_count={message.reply_count} />
       </div>
+      <MessageOptions displayLeft={false} messageWrapperRef={messageWrapperRef} />
     </div>
   );
 };
