7f1d5732fed764d153b67c7371641601f37cfa12
Dan Carbonell <dan1525@gmail.com>
2021-07-07 16:59:49 -0600

    add theme change hook

--- examples/website-demos/virtual-event/src/App.tsx
+++ examples/website-demos/virtual-event/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-
 import './styles/app.scss';
 
 import { ChatContainer } from './components/Chat/ChatContainer';
 import { Navigation } from './components/Navigation/Navigation';
 import { VideoContainer } from './components/Video/VideoContainer';
+import { useTheme } from './hooks/useTheme';
 
 export const App = () => {
+  const { setMode, setTheme } = useTheme(); // eslint-disable-line
+
   return (
     <div className='app-container'>
       <Navigation />
--- examples/website-demos/virtual-event/src/components/Chat/ChatContainer.tsx
+++ examples/website-demos/virtual-event/src/components/Chat/ChatContainer.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ChatContainer.scss';
 
-export const ChatContainer = () => {
-  return <div className='chat-container'>Chat</div>;
+import { CloseChatButton } from '../../assets';
+
+type Props = {};
+
+export const ChatContainer: React.FC<Props> = (props) => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  return (
+    <div className={`chat-container ${isFullScreen ? 'full-screen' : ''}`}>
+      <CloseChatButton isFullScreen={isFullScreen} setIsFullScreen={setIsFullScreen} />
+    </div>
+  );
 };
