1ab46ee4a0ebd5b2c7c07abc30bcf9133f7b1b75
Dan Carbonell <dan1525@gmail.com>
2021-07-09 15:11:40 -0600

    initial chat components and finish themes

--- examples/website-demos/virtual-event/src/components/Chat/ChatContainer.tsx
+++ examples/website-demos/virtual-event/src/components/Chat/ChatContainer.tsx
@@ -1,17 +1,64 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from 'stream-chat-react';
+import 'stream-chat-react/dist/css/index.css';
 
 import './ChatContainer.scss';
 
 import { CloseChatButton } from '../../assets';
 
+const apiKey = process.env.REACT_APP_STREAM_KEY as string;
+const userId = process.env.REACT_APP_USER_ID as string;
+const userToken = process.env.REACT_APP_USER_TOKEN as string;
+
 type Props = {};
 
 export const ChatContainer: React.FC<Props> = (props) => {
+  const [chatClient, setChatClient] = useState<StreamChat | null>(null);
   const [isFullScreen, setIsFullScreen] = useState(false);
+
+  useEffect(() => {
+    const initChat = async () => {
+      const client = StreamChat.getInstance(apiKey);
+
+      if (process.env.REACT_APP_CHAT_SERVER_ENDPOINT) {
+        client.setBaseURL(process.env.REACT_APP_CHAT_SERVER_ENDPOINT);
+      }
+
+      await client.connectUser({ id: userId }, userToken);
+      setChatClient(client);
+    };
+
+    if (!chatClient) initChat();
+
+    return () => {
+      chatClient?.disconnectUser();
+    };
+  }, []); // eslint-disable-line
+
+  if (!chatClient) return null;
 
   return (
     <div className={`chat-container ${isFullScreen ? 'full-screen' : ''}`}>
       <CloseChatButton isFullScreen={isFullScreen} setIsFullScreen={setIsFullScreen} />
+      <Chat client={chatClient}>
+        <Channel>
+          <Window>
+            <ChannelHeader />
+            <MessageList />
+            <MessageInput focus />
+          </Window>
+          <Thread />
+        </Channel>
+      </Chat>
     </div>
   );
 };
