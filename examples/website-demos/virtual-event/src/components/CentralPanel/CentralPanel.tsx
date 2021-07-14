4f2ec5b058d4947593032c44782a2114f9d67c17
Grant Steinke <grantjsteinke@gmail.com>
2021-07-14 14:07:31 -0600

    layout and re-scaffold

---
+++ examples/website-demos/virtual-event/src/components/CentralPanel/CentralPanel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { HomePage } from './HomePage';
+
+import './CentralPanel.scss';
+
+export const CentralPanel = () => {
+  return (
+    <div className='central-panel-container'>
+      <HomePage />
+    </div>
+  );
+};
