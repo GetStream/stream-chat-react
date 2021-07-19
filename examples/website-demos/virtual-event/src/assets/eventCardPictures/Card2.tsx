2e67308191fad1a55fec13cda0e05e3284ec43b1
Grant Steinke <grantjsteinke@gmail.com>
2021-07-19 14:08:33 -0600

    adds assets

---
+++ examples/website-demos/virtual-event/src/assets/eventCardPictures/Card2.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+
+export const Card2 = () => {
+  return (
+    <svg
+      width='250'
+      height='157'
+      viewBox='0 0 250 157'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect width='250' height='156' transform='translate(0 0.341309)' fill='#160F1D' />
+      <path d='M125.371 78.2041L145.494 66.6262V90.0563L125.371 101.36V78.2041Z' fill='#73779C' />
+      <path d='M105.247 66.6262L125.371 78.2053V101.36L105.247 90.0597V66.6262Z' fill='#8DD6F7' />
+      <path
+        d='M105.247 66.6262L125.371 55.0483L145.494 66.6262L125.371 78.2042L105.247 66.6262Z'
+        fill='#EE5D2D'
+      />
+      <g filter='url(#filter0_d)'>
+        <path
+          d='M125.371 78.0671L165.618 54.9113V101.771L125.371 124.379V78.0671Z'
+          fill='#73779C'
+          fill-opacity='0.75'
+        />
+      </g>
+      <path
+        d='M85.1235 54.9114L125.371 78.0695V124.379L85.1235 101.778V54.9114Z'
+        fill='#8DD6F7'
+        fill-opacity='0.75'
+      />
+      <path
+        opacity='0.75'
+        d='M85.1235 54.9112L125.371 31.7554L165.618 54.9112L125.371 78.067L85.1235 54.9112Z'
+        fill='#EE5D2D'
+      />
+      <path
+        opacity='0.5'
+        d='M125.371 77.9299L185.741 43.1962V113.486L125.371 147.397V77.9299Z'
+        fill='#73779C'
+      />
+      <path
+        d='M65 43.1963L125.371 77.9335V147.397L65 113.497V43.1963Z'
+        fill='#8DD6F7'
+        fill-opacity='0.5'
+      />
+      <g filter='url(#filter1_b)'>
+        <path
+          d='M65 43.1971L125.371 8.46338L185.741 43.1971L125.371 77.9308L65 43.1971Z'
+          fill='#EE5D2D'
+          fill-opacity='0.4'
+        />
+      </g>
+      <defs>
+        <filter
+          id='filter0_d'
+          x='121.371'
+          y='54.9114'
+          width='48.247'
+          height='77.4674'
+          filterUnits='userSpaceOnUse'
+          color-interpolation-filters='sRGB'
+        >
+          <feFlood flood-opacity='0' result='BackgroundImageFix' />
+          <feColorMatrix
+            in='SourceAlpha'
+            type='matrix'
+            values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+          />
+          <feOffset dy='4' />
+          <feGaussianBlur stdDeviation='2' />
+          <feColorMatrix type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0' />
+          <feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow' />
+          <feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow' result='shape' />
+        </filter>
+        <filter
+          id='filter1_b'
+          x='61'
+          y='4.46338'
+          width='128.741'
+          height='77.4674'
+          filterUnits='userSpaceOnUse'
+          color-interpolation-filters='sRGB'
+        >
+          <feFlood flood-opacity='0' result='BackgroundImageFix' />
+          <feGaussianBlur in='BackgroundImage' stdDeviation='2' />
+          <feComposite in2='SourceAlpha' operator='in' result='effect1_backgroundBlur' />
+          <feBlend mode='normal' in='SourceGraphic' in2='effect1_backgroundBlur' result='shape' />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
---
+++ examples/website-demos/virtual-event/src/assets/eventCardPictures/Card4.tsx
@@ -0,0 +1,290 @@
+export const Card4 = () => {
+  return (
+    <svg
+      width='250'
+      height='140'
+      viewBox='0 0 250 140'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g clip-path='url(#clip0)'>
+        <rect width='250' height='156' fill='#1353FF' />
+        <path d='M158 86L315.738 206H0L158 86Z' fill='#2D2D2D' />
+        <path d='M158 63L315.738 183H0L158 63Z' fill='url(#paint0_linear)' fill-opacity='0.8' />
+        <path d='M158 42L315.738 162H0L158 42Z' fill='url(#paint1_linear)' fill-opacity='0.6' />
+        <path d='M158 21L315.738 141H0L158 21Z' fill='url(#paint2_linear)' fill-opacity='0.4' />
+        <path d='M158 86L315.738 206H0L158 86Z' fill='#2D2D2D' />
+        <path d='M158 63L315.738 183H0L158 63Z' fill='url(#paint3_linear)' fill-opacity='0.8' />
+        <path d='M158 42L315.738 162H0L158 42Z' fill='url(#paint4_linear)' fill-opacity='0.6' />
+        <path d='M158 21L315.738 141H0L158 21Z' fill='url(#paint5_linear)' fill-opacity='0.4' />
+        <path d='M316 76L158.262 -44L474 -44L316 76Z' fill='#2D2D2D' />
+        <path
+          d='M316 99L158.262 -21L474 -21L316 99Z'
+          fill='url(#paint6_linear)'
+          fill-opacity='0.8'
+        />
+        <path
+          d='M316 120L158.262 -2.75799e-05L474 0L316 120Z'
+          fill='url(#paint7_linear)'
+          fill-opacity='0.6'
+        />
+        <path
+          d='M316 141L158.262 21L474 21L316 141Z'
+          fill='url(#paint8_linear)'
+          fill-opacity='0.4'
+        />
+        <path d='M316 76L158.262 -44L474 -44L316 76Z' fill='#2D2D2D' />
+        <path
+          d='M316 99L158.262 -21L474 -21L316 99Z'
+          fill='url(#paint9_linear)'
+          fill-opacity='0.8'
+        />
+        <path
+          d='M316 120L158.262 -2.75799e-05L474 0L316 120Z'
+          fill='url(#paint10_linear)'
+          fill-opacity='0.6'
+        />
+        <path
+          d='M316 141L158.262 21L474 21L316 141Z'
+          fill='url(#paint11_linear)'
+          fill-opacity='0.4'
+        />
+        <path d='M0.000411987 76L-157.738 -44L158 -44L0.000411987 76Z' fill='#2D2D2D' />
+        <path
+          d='M0.000411987 99L-157.738 -21L158 -21L0.000411987 99Z'
+          fill='url(#paint12_linear)'
+          fill-opacity='0.8'
+        />
+        <path
+          d='M0.000411987 120L-157.738 -2.76027e-05L158 0L0.000411987 120Z'
+          fill='url(#paint13_linear)'
+          fill-opacity='0.6'
+        />
+        <path
+          d='M0.000411987 141L-157.738 21L158 21L0.000411987 141Z'
+          fill='url(#paint14_linear)'
+          fill-opacity='0.4'
+        />
+        <path d='M0.000411987 76L-157.738 -44L158 -44L0.000411987 76Z' fill='#2D2D2D' />
+        <path
+          d='M0.000411987 99L-157.738 -21L158 -21L0.000411987 99Z'
+          fill='url(#paint15_linear)'
+          fill-opacity='0.8'
+        />
+        <path
+          d='M0.000411987 120L-157.738 -2.76027e-05L158 0L0.000411987 120Z'
+          fill='url(#paint16_linear)'
+          fill-opacity='0.6'
+        />
+        <path
+          d='M0.000411987 141L-157.738 21L158 21L0.000411987 141Z'
+          fill='url(#paint17_linear)'
+          fill-opacity='0.4'
+        />
+      </g>
+      <defs>
+        <linearGradient
+          id='paint0_linear'
+          x1='157.869'
+          y1='63'
+          x2='157.869'
+          y2='183'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#3F1B1B' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint1_linear'
+          x1='157.869'
+          y1='42'
+          x2='157.869'
+          y2='162'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#481212' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint2_linear'
+          x1='157.869'
+          y1='21'
+          x2='157.869'
+          y2='141'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#510909' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint3_linear'
+          x1='157.869'
+          y1='63'
+          x2='157.869'
+          y2='183'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#3F1B1B' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint4_linear'
+          x1='157.869'
+          y1='42'
+          x2='157.869'
+          y2='162'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#481212' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint5_linear'
+          x1='157.869'
+          y1='21'
+          x2='157.869'
+          y2='141'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#510909' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint6_linear'
+          x1='316.131'
+          y1='99'
+          x2='316.131'
+          y2='-21'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#3F1B1B' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint7_linear'
+          x1='316.131'
+          y1='120'
+          x2='316.131'
+          y2='-1.37899e-05'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#481212' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint8_linear'
+          x1='316.131'
+          y1='141'
+          x2='316.131'
+          y2='21'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#510909' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint9_linear'
+          x1='316.131'
+          y1='99'
+          x2='316.131'
+          y2='-21'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#3F1B1B' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint10_linear'
+          x1='316.131'
+          y1='120'
+          x2='316.131'
+          y2='-1.37899e-05'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#481212' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint11_linear'
+          x1='316.131'
+          y1='141'
+          x2='316.131'
+          y2='21'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#510909' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint12_linear'
+          x1='0.130859'
+          y1='99'
+          x2='0.13087'
+          y2='-21'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#3F1B1B' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint13_linear'
+          x1='0.130859'
+          y1='120'
+          x2='0.13087'
+          y2='-1.38014e-05'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#481212' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint14_linear'
+          x1='0.130859'
+          y1='141'
+          x2='0.13087'
+          y2='21'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#510909' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint15_linear'
+          x1='0.130859'
+          y1='99'
+          x2='0.13087'
+          y2='-21'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#3F1B1B' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint16_linear'
+          x1='0.130859'
+          y1='120'
+          x2='0.13087'
+          y2='-1.38014e-05'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#481212' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <linearGradient
+          id='paint17_linear'
+          x1='0.130859'
+          y1='141'
+          x2='0.13087'
+          y2='21'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stop-color='#510909' />
+          <stop offset='1' stop-color='#2D2D2D' stop-opacity='0' />
+        </linearGradient>
+        <clipPath id='clip0'>
+          <rect width='250' height='156' fill='white' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
