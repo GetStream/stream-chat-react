99db2968be98f9f5851795768c275d54b2d12825
Grant Steinke <grantjsteinke@gmail.com>
2021-07-20 14:25:02 -0600

    adds send button

--- examples/website-demos/virtual-event/src/components/Chat/MessageInput.tsx
+++ examples/website-demos/virtual-event/src/components/Chat/MessageInput.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SendArrow } from '../../assets/SendArrow';
 
 import './MessageInput.scss';
 
@@ -6,7 +7,10 @@ export const MessageInput = () => {
   return (
     <div className='message-input-container'>
       <div className='message-input-input'></div>
-      <div className='message-input-send'></div>
+      <div className='message-input-send'>
+        <SendArrow />
+        <div>269</div>
+      </div>
     </div>
   );
 };
