b1dd02758d3176cdbd90f7e2bb3e4f33b8f27901
Anna Baldwin <anna@getstream.io>
2021-07-22 13:34:50 -0700

    avatar group in preview

--- examples/website-demos/social-messaging/src/components/ChannelPreview/SocialChannelPreview.tsx
+++ examples/website-demos/social-messaging/src/components/ChannelPreview/SocialChannelPreview.tsx
@@ -1,82 +1,59 @@
 import React, { useRef } from 'react';
 
-import type { Channel } from 'stream-chat';
-import { Avatar as DefaultAvatar, ChannelPreviewUIComponentProps } from 'stream-chat-react';
+import { ChannelPreviewUIComponentProps, useChatContext } from 'stream-chat-react';
+
+import { AvatarGroup, getTimeStamp } from './utils';
 
 import './SocialChannelPreview.scss';
 
 export const SocialChannelPreview: React.FC<ChannelPreviewUIComponentProps> = (props) => {
     const {
-        active,
-        Avatar = DefaultAvatar,
-        channel,
-        displayImage,
-        displayTitle,
-        latestMessage,
-        setActiveChannel,
-        unread,
-        watchers,
-      } = props;
-    
-      const channelPreviewButton = useRef<HTMLButtonElement | null>(null);
-    
-      const activeClass = active ? 'active' : '';
-      const unreadCount = unread && unread > 0 ? true : false;
-    
-      const onSelectChannel = () => {
-        if (setActiveChannel) {
-          setActiveChannel(channel, watchers);
-        }
-        if (channelPreviewButton?.current) {
-          channelPreviewButton.current.blur();
-        }
-      };
+      active,
+      channel,
+      displayTitle,
+      latestMessage,
+      setActiveChannel,
+      unread,
+    } = props;
 
-      const getTimeStamp = (channel: Channel) => {
-        let lastHours = channel.state.last_message_at?.getHours();
-        let lastMinutes: string | number | undefined = channel.state.last_message_at?.getMinutes();
-        let half = 'AM';
-      
-        if (lastHours === undefined || lastMinutes === undefined) {
-          return '';
-        }
-      
-        if (lastHours > 12) {
-          lastHours = lastHours - 12;
-          half = 'PM';
-        }
-      
-        if (lastHours === 0) lastHours = 12;
-        if (lastHours === 12) half = 'PM';
-      
-        if (lastMinutes.toString().length === 1) {
-          lastMinutes = `0${lastMinutes}`;
-        }
-      
-        return `${lastHours}:${lastMinutes} ${half}`;
-      };
-    
-      return (
-        <button
-          className={`channel-preview ${activeClass}`}
-          onClick={onSelectChannel}
-          ref={channelPreviewButton}
-        >
-          <div className='channel-preview-avatar'>
-            <Avatar image={displayImage} name={displayTitle} size={56} />
+    const { client } = useChatContext();
+
+    console.log('channel IS:', channel);
+    // checkmark - delivered;
+    // double checkmark - read;
+  
+    const channelPreviewButton = useRef<HTMLButtonElement | null>(null);
+  
+    const activeClass = active ? 'active' : '';
+    const unreadCount = unread && unread > 0 ? true : false;
+
+    const members = Object.values(channel.state.members).filter(
+      ({ user }) => user?.id !== client.userID,
+    );
+  
+    return (
+      <button
+        className={`channel-preview ${activeClass}`}
+        onClick={() => {
+          setActiveChannel?.(channel);
+        }}
+        ref={channelPreviewButton}
+      >
+        <div className='channel-preview-avatar'>
+          <AvatarGroup members={members} />
+        </div>
+        <div className='channel-preview-contents'>
+          <div className='channel-preview-contents-name'>
+            <span>{displayTitle}</span>
           </div>
-          <div className='channel-preview-contents'>
-            <div className='channel-preview-contents-name'>
-              <span>{displayTitle}</span>
-            </div>
-            <div className='channel-preview-contents-last-message'>{latestMessage}</div>
+          <div className='channel-preview-contents-last-message'>{latestMessage}</div>
+        </div>
+        <div className='channel-preview-end'>
+          <div className={`channel-preview-end-unread ${unreadCount ? '' : 'unreadCount'}`}>
+            <span className='channel-preview-end-unread-text'>{unread}</span>
           </div>
-          <div className='channel-preview-end'>
-            <div className={`channel-preview-end-unread ${unreadCount ? '' : 'unreadCount'}`}>
-              <span className='channel-preview-end-unread-text'>{unread}</span>
-            </div>
-            <p className='channel-preview-end-timestamp'>{getTimeStamp(channel)}</p>
-          </div>
-        </button>
-      )
+          <p className='channel-preview-end-timestamp'>{getTimeStamp(channel)}</p>
+        </div>
+      </button>
+    )
 };
---
+++ examples/website-demos/social-messaging/src/components/ChannelPreview/utils.tsx
@@ -0,0 +1,77 @@
+import type { Channel, ChannelMemberResponse } from 'stream-chat';
+
+import { Avatar } from 'stream-chat-react';
+
+export const AvatarGroup = ({ members }: { members: ChannelMemberResponse[] }) => {
+  if (members.length === 1) {
+    return <Avatar image={members[0].user?.image as string || ''} size={56} />;
+  }
+
+  if (members.length === 2) {
+    return (
+      <div className='channel-preview-avatar-avatars two'>
+        <span>
+          <Avatar image={members[0].user?.image as string || ''} shape='square' size={25} />
+        </span>
+        <span>
+          <Avatar image={members[1].user?.image as string || ''} shape='square' size={25} />
+        </span>
+      </div>
+    );
+  }
+
+  if (members.length === 3) {
+    return (
+      <div className='channel-preview-avatar-avatars three'>
+        <span>
+          <Avatar image={members[0].user?.image as string || ''} shape='square' size={25} />
+        </span>
+        <span>
+          <Avatar image={members[1].user?.image as string || ''} shape='square' size={25} />
+          <Avatar image={members[2].user?.image as string || ''} shape='square' size={25} />
+        </span>
+      </div>
+    );
+  }
+
+  if (members.length >= 4) {
+    return (
+      <div className='channel-preview-avatar-avatars'>
+        <span>
+          <Avatar image={members[members.length - 1].user?.image as string || ''} shape='square' size={30} />
+          <Avatar image={members[members.length - 2].user?.image as string || ''} shape='square' size={30} />
+        </span>
+        <span>
+          <Avatar image={members[members.length - 3].user?.image as string || ''} shape='square' size={30} />
+          <Avatar image={members[members.length - 4].user?.image as string || ''} shape='square' size={30} />
+        </span>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export const getTimeStamp = (channel: Channel) => {
+  let lastHours = channel.state.last_message_at?.getHours();
+  let lastMinutes: string | number | undefined = channel.state.last_message_at?.getMinutes();
+  let half = 'AM';
+
+  if (lastHours === undefined || lastMinutes === undefined) {
+    return '';
+  }
+
+  if (lastHours > 12) {
+    lastHours = lastHours - 12;
+    half = 'PM';
+  }
+
+  if (lastHours === 0) lastHours = 12;
+  if (lastHours === 12) half = 'PM';
+
+  if (lastMinutes.toString().length === 1) {
+    lastMinutes = `0${lastMinutes}`;
+  }
+
+  return `${lastHours}:${lastMinutes} ${half}`;
+};
