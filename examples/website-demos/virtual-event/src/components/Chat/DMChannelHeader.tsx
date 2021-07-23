76581aca585d338c5bd03d4e014b33c9b50aa906
Grant Steinke <grantjsteinke@gmail.com>
2021-07-23 14:25:01 -0600

    adds dropdown

--- examples/website-demos/virtual-event/src/components/Chat/DMChannelHeader.tsx
+++ examples/website-demos/virtual-event/src/components/Chat/DMChannelHeader.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 import { CloseX } from '../../assets/CloseX';
 import { Ellipse } from '../../assets/Ellipse';
 import { useEventContext } from '../../contexts/EventContext';
 
 import './DMChannelHeader.scss';
 
+const DropDown = () => {
+  return (
+    <div className='dropdown'>
+      <div className='dropdown-option'>Mute User</div>
+      <div className='dropdown-option'>Block User</div>
+      <div className='dropdown-option'>Report User</div>
+    </div>
+  );
+};
+
 export const DMChannelHeader = () => {
   const { dmChannel, setDmChannel } = useEventContext();
+  const [openMenu, setOpenMenu] = useState(false);
 
   console.log({ dmChannel });
 
@@ -19,17 +31,20 @@ export const DMChannelHeader = () => {
   };
 
   return (
-    <div className='dm-header-container'>
-      <div className='dm-header-close' onClick={handleCloseDm}>
-        <CloseX />
+    <>
+      <div className='dm-header-container'>
+        <div className='dm-header-close' onClick={handleCloseDm}>
+          <CloseX />
+        </div>
+        <div className='dm-header-title'>
+          <div>{channelTitle}</div>
+          <div className='dm-header-title-sub-title'>Direct Message</div>
+        </div>
+        <div className='dm-header-actions' onClick={() => setOpenMenu(!openMenu)}>
+          <Ellipse />
+        </div>
       </div>
-      <div className='dm-header-title'>
-        <div>{channelTitle}</div>
-        <div className='dm-header-title-sub-title'>Direct Message</div>
-      </div>
-      <div className='dm-header-actions'>
-        <Ellipse />
-      </div>
-    </div>
+      {openMenu && <DropDown />}
+    </>
   );
 };
