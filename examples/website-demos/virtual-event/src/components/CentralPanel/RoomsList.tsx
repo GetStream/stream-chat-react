99695553db832c927b1e1f72614d2fc093f3dbc8
Grant Steinke <grantjsteinke@gmail.com>
2021-08-02 12:05:47 -0600

    adds header

--- examples/website-demos/virtual-event/src/components/CentralPanel/RoomsList.tsx
+++ examples/website-demos/virtual-event/src/components/CentralPanel/RoomsList.tsx
@@ -5,6 +5,7 @@ import { Card1, Card2 } from '../../assets';
 import { useEventContext } from '../../contexts/EventContext';
 
 import './RoomsList.scss';
+import { CalendarButton } from '../../assets/CalendarButton';
 
 export const RoomsList = () => {
   const { chatType } = useEventContext();
@@ -17,77 +18,88 @@ export const RoomsList = () => {
         <h1>ROOM VIDEO</h1>
       </div>
       <div className={`rooms-list-container${chatType === 'room' ? '-hidden' : ''}`}>
-        <div className='rooms-list-card'>
-          <EventCard
-            chatType='room'
-            content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
-            eventName='esg'
-            Image={Card1}
-            label='Private'
-            presenters={6}
-            title='ESG Data - How to create it'
-            viewers={150}
-          />
+        <div className='rooms-list-header'>
+          <div className='rooms-list-header-title'>
+            <div className='rooms-list-header-title-main'>World Hacker Summit 2021</div>
+            <div className='rooms-list-header-title-sub'>Stream.IO</div>
+          </div>
+          <div className='rooms-list-header-calendar'>
+            <CalendarButton />
+          </div>
         </div>
-        <div className='rooms-list-card'>
-          <EventCard
-            chatType='room'
-            content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
-            eventName='qa'
-            Image={Card1}
-            label='Moderated'
-            presenters={6}
-            title='Q&A session: Data strategy and executive communication'
-            viewers={150}
-          />
-        </div>
-        <div className='rooms-list-card'>
-          <EventCard
-            chatType='room'
-            content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
-            eventName='qa'
-            Image={Card1}
-            label='Moderated'
-            presenters={6}
-            title='Q&A session: Data strategy and executive communication'
-            viewers={150}
-          />
-        </div>
-        <div className='rooms-list-card'>
-          <EventCard
-            chatType='room'
-            content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
-            eventName='entertainment'
-            Image={Card2}
-            label='Open'
-            presenters={6}
-            title='Entertainment data literacy: learning to love the data'
-            viewers={150}
-          />
-        </div>
-        <div className='rooms-list-card'>
-          <EventCard
-            chatType='room'
-            content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
-            eventName='tools'
-            Image={Card1}
-            label='Closed'
-            presenters={6}
-            title='Tools, Teams and Processes: how to successfully implement a data…'
-            viewers={150}
-          />
-        </div>
-        <div className='rooms-list-card'>
-          <EventCard
-            chatType='room'
-            content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
-            eventName='roi'
-            Image={Card1}
-            label='Open'
-            presenters={6}
-            title='Defining ROI in the Modern Data World'
-            viewers={150}
-          />
+        <div className='rooms-list-container-cards'>
+          <div className='rooms-list-card'>
+            <EventCard
+              chatType='room'
+              content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
+              eventName='esg'
+              Image={Card1}
+              label='Private'
+              presenters={6}
+              title='ESG Data - How to create it'
+              viewers={150}
+            />
+          </div>
+          <div className='rooms-list-card'>
+            <EventCard
+              chatType='room'
+              content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
+              eventName='qa'
+              Image={Card1}
+              label='Moderated'
+              presenters={6}
+              title='Q&A session: Data strategy and executive communication'
+              viewers={150}
+            />
+          </div>
+          <div className='rooms-list-card'>
+            <EventCard
+              chatType='room'
+              content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
+              eventName='qa'
+              Image={Card1}
+              label='Moderated'
+              presenters={6}
+              title='Q&A session: Data strategy and executive communication'
+              viewers={150}
+            />
+          </div>
+          <div className='rooms-list-card'>
+            <EventCard
+              chatType='room'
+              content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
+              eventName='entertainment'
+              Image={Card2}
+              label='Open'
+              presenters={6}
+              title='Entertainment data literacy: learning to love the data'
+              viewers={150}
+            />
+          </div>
+          <div className='rooms-list-card'>
+            <EventCard
+              chatType='room'
+              content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
+              eventName='tools'
+              Image={Card1}
+              label='Closed'
+              presenters={6}
+              title='Tools, Teams and Processes: how to successfully implement a data…'
+              viewers={150}
+            />
+          </div>
+          <div className='rooms-list-card'>
+            <EventCard
+              chatType='room'
+              content='How to set a business plans to use information to a competitive advantage and support enterprise goals.'
+              eventName='roi'
+              Image={Card1}
+              label='Open'
+              presenters={6}
+              title='Defining ROI in the Modern Data World'
+              viewers={150}
+            />
+          </div>
         </div>
       </div>
     </>
