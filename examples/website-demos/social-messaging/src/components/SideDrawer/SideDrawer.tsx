5633f8aba6049a2822dabe162eda2acfe81fce53
Anna Baldwin <anna@getstream.io>
2021-08-04 11:34:46 -0600

    add theme toggle and adjust css colors

--- examples/website-demos/social-messaging/src/components/SideDrawer/SideDrawer.tsx
+++ examples/website-demos/social-messaging/src/components/SideDrawer/SideDrawer.tsx
@@ -1,3 +1,5 @@
+import { Avatar, useChatContext } from 'stream-chat-react';
+
 import { NewDirectMessage } from '../../assets/NewDirectMessage';
 import { NewGroupMessage } from '../../assets/NewGroupMessage';
 import { ThemeIcon } from '../../assets/ThemeIcon';
@@ -7,21 +9,27 @@ import { useViewContext } from '../../contexts/ViewContext';
 import './SideDrawer.scss';
 
 export const SideDrawer: React.FC = () => {
-  const { isSideDrawerOpen, setSideDrawerOpen } = useViewContext();
+  const { isSideDrawerOpen, setNewChat, setSideDrawerOpen } = useViewContext();
+  const { client } = useChatContext();
+
+  const user = client.user;
+
+  const onClickFunction = () => {
+    setSideDrawerOpen(false);
+    setNewChat(true);
+  };
 
   return (
-    <div
-      className={`side-drawer ${isSideDrawerOpen ? 'isSideDrawerOpen' : ''}`}
-      onClick={() => setSideDrawerOpen(!isSideDrawerOpen)}
-    >
+    <div className={`side-drawer ${isSideDrawerOpen ? 'isSideDrawerOpen' : ''}`}>
       <div className='side-drawer-content-line'>
+        <Avatar image={user?.image || ''} name={user?.name || 'User'} size={45} />
         <span>User</span>
       </div>
-      <div className='side-drawer-content-line'>
+      <div className='side-drawer-content-line' onClick={() => onClickFunction()}>
         <NewDirectMessage />
         <span>New Direct Message</span>
       </div>
-      <div className='side-drawer-content-line'>
+      <div className='side-drawer-content-line' onClick={() => onClickFunction()}>
         <NewGroupMessage />
         <span>New Group Message</span>
       </div>
