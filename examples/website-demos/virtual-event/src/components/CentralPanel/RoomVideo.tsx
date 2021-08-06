b64aa09f0ecabdc8dcc83f7488557d8b0b254e52
Grant Steinke <grantjsteinke@gmail.com>
2021-08-06 14:58:00 -0600

    initial layout and functionality

---
+++ examples/website-demos/virtual-event/src/components/CentralPanel/RoomVideo.tsx
@@ -0,0 +1,124 @@
+import { BaseSyntheticEvent, useState } from 'react';
+
+import { ConnectionStatus, Listening, Muted, Speaking } from '../../assets';
+
+import part1 from '../../assets/participant01.jpg';
+import part2 from '../../assets/participant02.jpg';
+import part3 from '../../assets/participant07.jpg';
+import part4 from '../../assets/participant04.jpg';
+import part5 from '../../assets/participant05.jpg';
+import part6 from '../../assets/participant06.jpg';
+
+export const RoomVideo = () => {
+  const [isPinned, setIsPinned] = useState(false);
+  const [pinnedID, setPinnedID] = useState(null);
+
+  const handleClick = (event: BaseSyntheticEvent) => {
+    if (!isPinned) {
+      setIsPinned(true);
+      setPinnedID(event.target.id);
+    }
+    if (isPinned && event.target.id === pinnedID) {
+      setIsPinned(false);
+      setPinnedID(null);
+    }
+
+    if (isPinned && pinnedID !== event.target.id) {
+      setPinnedID(event.target.id);
+    }
+  };
+
+  console.log(pinnedID === '1');
+
+  return (
+    <div className='room-video-container'>
+      <div className='room-video-header'>Data strategy and executive communication</div>
+      <div className={`room-video-grid ${isPinned ? 'isPinned' : ''}`}>
+        <div
+          className={`room-video-grid-participant ${pinnedID === '1' ? 'pinned' : ''}`}
+          id='1'
+          style={{ backgroundImage: `url(${part1})`, backgroundSize: 'cover' }}
+          onClick={handleClick}
+        >
+          <div className='room-video-grid-participant-info'>
+            <div className='room-video-grid-participant-info-name'>Lita Sherman</div>
+            <Speaking />
+          </div>
+          <div className='room-video-grid-participant-connection'>
+            <ConnectionStatus />
+          </div>
+        </div>
+        <div
+          className={`room-video-grid-participant ${pinnedID === '2' ? 'pinned' : ''}`}
+          id='2'
+          onClick={handleClick}
+          style={{ backgroundImage: `url(${part2})`, backgroundSize: 'cover' }}
+        >
+          <div className='room-video-grid-participant-info'>
+            <div className='room-video-grid-participant-info-name'>Kirk Purdie</div>
+            <Listening />
+          </div>
+          <div className='room-video-grid-participant-connection'>
+            <ConnectionStatus />
+          </div>
+        </div>
+        <div
+          className={`room-video-grid-participant ${pinnedID === '3' ? 'pinned' : ''}`}
+          id='3'
+          onClick={handleClick}
+          style={{ backgroundImage: `url(${part5})`, backgroundSize: 'cover' }}
+        >
+          <div className='room-video-grid-participant-info'>
+            <div className='room-video-grid-participant-info-name'>Khalid Ignác</div>
+            <Muted />
+          </div>
+          <div className='room-video-grid-participant-connection'>
+            <ConnectionStatus />
+          </div>
+        </div>
+        <div
+          className={`room-video-grid-participant ${pinnedID === '4' ? 'pinned' : ''}`}
+          id='4'
+          onClick={handleClick}
+          style={{ backgroundImage: `url(${part6})`, backgroundSize: 'cover' }}
+        >
+          <div className='room-video-grid-participant-info'>
+            <div className='room-video-grid-participant-info-name'>Jaana Kirstie</div>
+            <Muted />
+          </div>
+          <div className='room-video-grid-participant-connection'>
+            <ConnectionStatus />
+          </div>
+        </div>
+        <div
+          className={`room-video-grid-participant ${pinnedID === '5' ? 'pinned' : ''}`}
+          id='5'
+          onClick={handleClick}
+          style={{ backgroundImage: `url(${part3})`, backgroundSize: 'cover' }}
+        >
+          <div className='room-video-grid-participant-info'>
+            <div className='room-video-grid-participant-info-name'>Neal Sameera</div>
+            <Muted />
+          </div>
+          <div className='room-video-grid-participant-connection'>
+            <ConnectionStatus />
+          </div>
+        </div>
+        <div
+          className={`room-video-grid-participant ${pinnedID === '6' ? 'pinned' : ''}`}
+          id='6'
+          onClick={handleClick}
+          style={{ backgroundImage: `url(${part4})`, backgroundSize: 'cover' }}
+        >
+          <div className='room-video-grid-participant-info'>
+            <div className='room-video-grid-participant-info-name'>Halide Nursultan</div>
+            <Muted />
+          </div>
+          <div className='room-video-grid-participant-connection'>
+            <ConnectionStatus />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
