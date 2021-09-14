179f9f080fd70c34f4dc742a8f627b3f533bc13a
Anna Baldwin <anna@getstream.io>
2021-09-14 15:08:12 -0600

    fix mentions list to not include muted users

--- src/components/MessageInput/hooks/useUserTrigger.ts
+++ src/components/MessageInput/hooks/useUserTrigger.ts
@@ -126,7 +126,7 @@ export const useUserTrigger = <
 
       const filterMutes = (data: UserResponse<Us>[]) => {
         if (!mutes.length) return data;
-        return data.filter((suggestion) => mutes.some((mute) => mute.target.id === suggestion.id));
+        return data.filter((suggestion) => mutes.some((mute) => mute.target.id !== suggestion.id));
       };
 
       if (mentionAllAppUsers) {
