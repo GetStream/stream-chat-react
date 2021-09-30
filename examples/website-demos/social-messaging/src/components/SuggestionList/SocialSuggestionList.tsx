f16b90c0c9b8ce79995e72ec7f3dbe11b0fb07eb
Anna Baldwin <anna@getstream.io>
2021-09-30 11:02:43 -0600

    update to emoji item

--- examples/website-demos/social-messaging/src/components/SuggestionList/SocialSuggestionList.tsx
+++ examples/website-demos/social-messaging/src/components/SuggestionList/SocialSuggestionList.tsx
@@ -72,7 +72,7 @@ export const SocialSuggestionList = React.forwardRef(
     const { description, Icon } = getCommandIcon(item.name);
 
     const itemName = isEmojiOrMention(item) ? item.name || item.id : item.name;
-    const displayText = isEmoji(item) ? `${item.native}- ${itemName}` : itemName;
+    const displayText = isEmoji(item) ? `${item.native} ${itemName}` : itemName;
 
     return (
       <div
