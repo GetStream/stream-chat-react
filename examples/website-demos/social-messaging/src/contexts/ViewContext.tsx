ce88024062059b98b995feaa1bc066e273c7f26b
Zachery Converse <zacheryconverse@gmail.com>
2021-10-11 14:24:12 -0600

    create MessageActionsBox, flag, and mute users. add modal state to view context

--- examples/website-demos/social-messaging/src/contexts/ViewContext.tsx
+++ examples/website-demos/social-messaging/src/contexts/ViewContext.tsx
@@ -1,18 +1,24 @@
 import React, { useContext, useState } from 'react';
 
+export type UserActions = 'reply' | 'flag' | 'mute' | 'unmute';
+
 type ViewContextValue = {
   chatsUnreadCount: number;
   isListMentions: boolean;
   isSideDrawerOpen: boolean;
   isNewChat: boolean;
   mentionsUnreadCount: number;
+  messageActionsOpen: boolean;
   reactionsOpenId: string;
   setChatsUnreadCount: React.Dispatch<React.SetStateAction<number>>;
   setListMentions: React.Dispatch<React.SetStateAction<boolean>>;
   setMentionsUnreadCount: React.Dispatch<React.SetStateAction<number>>;
+  setMessageActionsOpen: React.Dispatch<React.SetStateAction<boolean>>;
   setNewChat: React.Dispatch<React.SetStateAction<boolean>>;
   setReactionsOpenId: React.Dispatch<React.SetStateAction<string>>;
   setSideDrawerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setUserActionType: React.Dispatch<React.SetStateAction<UserActions | undefined>>;
+  userActionType?: UserActions;
 };
 
 const ViewContext = React.createContext({} as ViewContextValue);
@@ -23,7 +29,9 @@ export const ViewProvider: React.FC = ({ children }) => {
   const [isSideDrawerOpen, setSideDrawerOpen] = useState(false);
   const [isNewChat, setNewChat] = useState(false);
   const [mentionsUnreadCount, setMentionsUnreadCount] = useState(0);
+  const [messageActionsOpen, setMessageActionsOpen] = useState(false);
   const [reactionsOpenId, setReactionsOpenId] = useState('');
+  const [userActionType, setUserActionType] = useState<UserActions>();
 
   const contextValue: ViewContextValue = {
     chatsUnreadCount,
@@ -31,13 +39,17 @@ export const ViewProvider: React.FC = ({ children }) => {
     isSideDrawerOpen,
     isNewChat,
     mentionsUnreadCount,
+    messageActionsOpen,
     reactionsOpenId,
     setChatsUnreadCount,
     setListMentions,
     setMentionsUnreadCount,
+    setMessageActionsOpen,
     setNewChat,
     setReactionsOpenId,
     setSideDrawerOpen,
+    setUserActionType,
+    userActionType,
   };
 
   return <ViewContext.Provider value={contextValue}>{children}</ViewContext.Provider>;
