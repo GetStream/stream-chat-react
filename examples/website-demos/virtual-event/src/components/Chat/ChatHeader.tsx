03f667fd80b44ec3caf5725a935ed0d5596de96b
Dan Carbonell <dan1525@gmail.com>
2021-10-26 10:48:45 -0600

    add animations to tab selectors

--- examples/website-demos/virtual-event/src/components/Chat/ChatHeader.tsx
+++ examples/website-demos/virtual-event/src/components/Chat/ChatHeader.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AnimatePresence, motion, Variants } from 'framer-motion';
 
 import { CloseChatButton, OnlineUsersIcon } from '../../assets';
 import { useEventContext } from '../../contexts/EventContext';
@@ -36,6 +37,11 @@ export const ChatHeader: React.FC<Props> = (props) => {
     setShowChannelList(false);
   };
 
+  const variants: Variants = {
+    open: { opacity: 1, y: 0, zIndex: 1 },
+    closed: { opacity: 0, y: -4, zIndex: -1 },
+  };
+
   return (
     <div className='chat-components-header'>
       <div className='chat-components-header-top'>
@@ -44,15 +50,33 @@ export const ChatHeader: React.FC<Props> = (props) => {
       </div>
       <div className='chat-components-header-tabs'>
         <div className='chat-components-header-tabs-item' onClick={handleGlobalClick}>
-          <div className={`${chatType === 'global' ? 'selected' : ''}`} />
+          <AnimatePresence>
+            {chatType === 'global' && (
+              <motion.div
+                className='selected'
+                variants={variants}
+                initial='closed'
+                animate='open'
+                exit='closed'
+              />
+            )}
+          </AnimatePresence>
           <div>Global</div>
           <div className={`${globalUnread && chatType !== 'global' ? 'unread' : ''}`} />
         </div>
         {selected !== 'overview' && eventName && (
           <div className='chat-components-header-tabs-item' onClick={handleEventClick}>
-            <div
-              className={`${chatType === 'main-event' || chatType === 'room' ? 'selected' : ''}`}
-            />
+            <AnimatePresence>
+              {(chatType === 'main-event' || chatType === 'room') && (
+                <motion.div
+                  className='selected'
+                  variants={variants}
+                  initial='closed'
+                  animate='open'
+                  exit='closed'
+                />
+              )}
+            </AnimatePresence>
             <div>{selected === 'main-event' ? 'Event' : 'Room'}</div>
             <div
               className={`${
@@ -62,12 +86,32 @@ export const ChatHeader: React.FC<Props> = (props) => {
           </div>
         )}
         <div className='chat-components-header-tabs-item' onClick={handleDirectClick}>
-          <div className={`${chatType === 'direct' ? 'selected' : ''}`} />
+          <AnimatePresence>
+            {chatType === 'direct' && (
+              <motion.div
+                className='selected'
+                variants={variants}
+                initial='closed'
+                animate='open'
+                exit='closed'
+              />
+            )}
+          </AnimatePresence>
           <div>DM</div>
           <div className={`${dmUnread && chatType !== 'direct' ? 'unread' : ''}`} />
         </div>
         <div className='chat-components-header-tabs-item' onClick={handleQAClick}>
-          <div className={`${chatType === 'qa' ? 'selected' : ''}`} />
+          <AnimatePresence>
+            {chatType === 'qa' && (
+              <motion.div
+                className='selected'
+                variants={variants}
+                initial='closed'
+                animate='open'
+                exit='closed'
+              />
+            )}
+          </AnimatePresence>
           <div>Q&A</div>
           <div className={`${qaUnread && chatType !== 'qa' ? 'unread' : ''}`} />
         </div>
