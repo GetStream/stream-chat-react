f0bd7eafa516e66a26fba1f1bdfff2b359e0cf0d
Anna Baldwin <anna.baldwin21@gmail.com>
2022-01-04 17:08:30 -0700

    fix tests

--- src/components/Gallery/__tests__/Gallery.test.js
+++ src/components/Gallery/__tests__/Gallery.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { cleanup, render, waitFor } from '@testing-library/react';
+import { cleanup, fireEvent, render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import { getTestClientWithUser } from '../../../mock-builders';
@@ -64,6 +64,17 @@ describe('Gallery', () => {
   it('should render component with 6 images', () => {
     const tree = renderer.create(<Gallery images={mockGalleryAssets} />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should open modal on image click', async () => {
+    jest.spyOn(console, 'warn').mockImplementation(() => null);
+
+    const { getByTestId, getByTitle } = render(<Gallery images={mockGalleryAssets.slice(0, 1)} />);
+    fireEvent.click(getByTestId('gallery-image'));
+
+    await waitFor(() => {
+      expect(getByTitle('Close')).toBeInTheDocument();
+    });
   });
 
   it('should display correct image count', async () => {
