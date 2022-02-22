3f97c98d286678e026e276127a7ec88e37b30674
Zachery Converse <zacheryconverse@gmail.com>
2022-02-22 11:18:42 -0700

    Fixes issue 1376 (#1380)
    fix: check for message actions

--- src/components/Message/MessageOptions.tsx
+++ src/components/Message/MessageOptions.tsx
@@ -75,7 +75,8 @@ const UnMemoizedMessageOptions = <
   const handleOpenThread = propHandleOpenThread || contextHandleOpenThread;
 
   const messageActions = getMessageActions();
-  const showActionsBox = showMessageActionsBox(messageActions) || !!customMessageActions;
+  const showActionsBox =
+    showMessageActionsBox(messageActions, threadList) || !!customMessageActions;
 
   const shouldShowReactions = messageActions.indexOf(MESSAGE_ACTIONS.react) > -1;
   const shouldShowReplies =
