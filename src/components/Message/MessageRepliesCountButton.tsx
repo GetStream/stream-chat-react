7535d4d958affda09ad86a8a5db8d949176af8e3
Zachery Converse <zacheryconverse@gmail.com>
2022-03-07 13:26:19 -0700

    update i18 variables

--- src/components/Message/MessageRepliesCountButton.tsx
+++ src/components/Message/MessageRepliesCountButton.tsx
@@ -33,7 +33,7 @@ const UnMemoizedMessageRepliesCountButton: React.FC<MessageRepliesCountButtonPro
     if (labelPlural) {
       pluralReplyText = `${reply_count} ${labelPlural}`;
     } else {
-      pluralReplyText = t('replyCount_other', { reply_count });
+      pluralReplyText = t('replyCount_other', { replyCount: reply_count });
     }
   }
 
