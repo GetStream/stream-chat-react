975d30ec11039c917dd85f8fbb202ba36c54a4e6
Petyo Ivanov <underlog@gmail.com>
2022-04-04 17:35:42 +0300

    chore: try fixing the e2e

--- e2e/autocomplete-mention.test.ts
+++ e2e/autocomplete-mention.test.ts
@@ -3,11 +3,11 @@
 import { expect, Page, test } from '@playwright/test';
 
 test.describe('autocomplete a mention', () => {
-  test('message list should clear', async ({ baseURL, page }) => {
+  test('should fill in textarea with username', async ({ baseURL, page }) => {
     await page.goto(`${baseURL}/?story=hello--basic-setup`);
     await page.waitForSelector('[data-storyloaded]');
     await page.fill('data-testid=message-input', '@');
-    await page.click('text=test-user-2');
+    await page.click('button >> text=test-user-2');
     await expect(page.locator('data-testid=message-input')).toContainText('@test-user-2');
   });
 });
