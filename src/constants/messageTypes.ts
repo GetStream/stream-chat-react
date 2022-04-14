98cd4a9d00174be219f63e95cd0896d27b00a197
MartinCupela <martin.cupela@gmail.com>
2022-04-14 15:01:16 +0200

    refactor: apply const assertion on CUSTOM_MESSAGE_TYPE

--- src/constants/messageTypes.ts
+++ src/constants/messageTypes.ts
@@ -1,6 +1,4 @@
-import type { CustomMessageType } from '../types/types';
-
-export const CUSTOM_MESSAGE_TYPE: Record<'date' | 'intro', CustomMessageType> = {
+export const CUSTOM_MESSAGE_TYPE = {
   date: 'message.date',
   intro: 'channel.intro',
-};
+} as const;
