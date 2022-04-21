0f0b59421b3c26961a5a30fb767d89ea8ecc20d4
MartinCupela <martin.cupela@gmail.com>
2022-04-21 23:28:28 +0200

    fix: set isMounted ref value on every mount

--- src/components/Channel/hooks/useIsMounted.ts
+++ src/components/Channel/hooks/useIsMounted.ts
@@ -3,12 +3,12 @@ import { useEffect, useRef } from 'react';
 export const useIsMounted = () => {
   const isMounted = useRef(true);
 
-  useEffect(
-    () => () => {
+  useEffect(() => {
+    isMounted.current = true;
+    return () => {
       isMounted.current = false;
-    },
-    [],
-  );
+    };
+  }, []);
 
   return isMounted;
 };
