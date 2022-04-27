4e97490b0f99ad265cc9c01c05a94df2f56f5083
Anton Arnautov <arnautov.anton@gmail.com>
2022-04-28 01:17:43 +0200

    test: update playwright.config.ts, adjust number of workers and timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,8 +3,8 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 // https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
   testDir: './e2e',
+  timeout: 15 * 1000,
   use: {
-    headless: true,
     viewport: { height: 920, width: 1280 },
   },
   webServer: {
@@ -13,6 +13,7 @@ const config: PlaywrightTestConfig = {
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
+  workers: 1,
 };
 
 export default config;
