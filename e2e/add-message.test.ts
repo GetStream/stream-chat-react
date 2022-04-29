3d3fca98cc992250ca5542c0c2c19c6821bba6bc
Anton Arnautov <arnautov.anton@gmail.com>
2022-04-29 15:08:26 +0200

    test: fix add-message.test.ts, make test suites independent from each other

--- e2e/add-message.test.ts
+++ e2e/add-message.test.ts
@@ -3,7 +3,8 @@
 import { expect, Page, test } from '@playwright/test';
 
 function getPreview(page: Page) {
-  return page.locator('*data-testid=channel-preview-button >> text=add-message');
+  // with id:$eq filter there should be only one (add-message)
+  return page.locator('data-testid=channel-preview-button');
 }
 
 test.describe('add a message', () => {
@@ -11,26 +12,28 @@ test.describe('add a message', () => {
     await Promise.all([
       page.waitForSelector('data-testid=add-message'),
       page.goto(`${baseURL}/?story=connected-user--user1`),
+      getPreview(page).click(),
+    ]);
+  });
+
+  test('message list and preview button should be clear', async ({ page }) => {
+    await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/truncate') && r.ok()),
+      page.click('data-testid=truncate'),
     ]);
 
-    // Select correct channel
-    const preview = getPreview(page);
-    await preview.click();
-  });
-
-  test('message list should clear', async ({ page }) => {
-    await page.click('data-testid=truncate');
     const list = page.locator('.str-chat__list');
     await expect(list).toBeEmpty();
-  });
 
-  test('channel list preview should be cleared', async ({ page }) => {
-    const preview = getPreview(page);
-    await expect(preview).toContainText('Nothing yet...');
+    await expect(getPreview(page)).toContainText('Nothing yet...');
   });
 
   test('message list should update for current user', async ({ page }) => {
-    await page.click('data-testid=add-message');
+    await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/message') && r.ok()),
+      page.click('data-testid=add-message'),
+    ]);
+
     const list = page.locator('.str-chat__list');
     await expect(list).not.toBeEmpty();
     const newMessage = page.locator('.str-chat__message').first();
@@ -38,8 +41,7 @@ test.describe('add a message', () => {
   });
 
   test('channel list should update for current user', async ({ page }) => {
-    const preview = getPreview(page);
-    await expect(preview).toContainText('Hello world!');
+    await expect(getPreview(page)).toContainText('Hello world!');
   });
 });
 
@@ -47,19 +49,36 @@ test.describe('receive a message', () => {
   test.beforeEach(async ({ baseURL, page }) => {
     await Promise.all([
       page.waitForSelector('data-testid=add-message'),
+      page.goto(`${baseURL}/?story=connected-user--user1`),
+      getPreview(page).click(),
+    ]);
+
+    await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/message') && r.ok()),
+      page.click('data-testid=add-message'),
+    ]);
+
+    await Promise.all([
+      page.waitForSelector('data-testid=channel-preview-button'),
       page.goto(`${baseURL}/?story=connected-user--user2`),
     ]);
   });
 
   test('channel list should update for channel members and show unread', async ({ page }) => {
     const preview = getPreview(page);
+
     await expect(preview).toHaveClass(/str-chat__channel-preview-messenger--unread/);
     await expect(preview).toContainText('Hello world!');
   });
 
   test('message list should update for different users on the channel', async ({ page }) => {
     const preview = getPreview(page);
-    await preview.click();
+
+    await Promise.all([
+      page.waitForResponse((r) => r.url().includes('/read') && r.ok()),
+      preview.click(),
+    ]);
+
     await expect(preview).not.toHaveClass(/str-chat__channel-preview-messenger--unread/);
 
     const list = page.locator('.str-chat__list');
