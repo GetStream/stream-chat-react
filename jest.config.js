71e92b5f677835cd101faef4eb038921dd0ef2db
Anton Arnautov <arnautov.anton@gmail.com>
2022-05-02 22:45:57 +0200

    Revert "test(configuration): add maxWorkers limit"
    This reverts commit e0aee1a8e50bedad46dd428dae63a6adb7829e3e.

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
   },
   globalSetup: './jest-global-setup.js',
   maxConcurrency: 15,
-  maxWorkers: 1,
   moduleNameMapper: {
     '\\.(css|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/assetsTransformer.js',
