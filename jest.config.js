f1a351ad8b955615fb770e422a0dd7b4f6b8a888
Anton Arnautov <arnautov.anton@gmail.com>
2022-05-02 12:40:37 +0200

    Revert "test(configuration): replace maxConcurrency with maxWorkers, add --silent for CI"
    This reverts commit a4e3b9b370afd41eb57876072089e67c5fadd087.

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     ICAL: {},
   },
   globalSetup: './jest-global-setup.js',
-  maxWorkers: 5,
+  maxConcurrency: 15,
   moduleNameMapper: {
     '\\.(css|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/assetsTransformer.js',
