8ebf9a2309818e97d5d6b2f3e837fa2937acf99f
MartinCupela <martin.cupela@gmail.com>
2022-05-13 05:03:52 +0200

    test(e2e): use unique text for reply

--- e2e/navigate-long-message-lists.test.ts
+++ e2e/navigate-long-message-lists.test.ts
@@ -1,10 +1,10 @@
+import { v4 as uuid } from 'uuid';
+
 import { expect } from '@playwright/test';
 import selectors from './user/selectors';
 import { test } from './user/test';
 
 const CHANNEL_NAME = 'navigate-long-message-lists' as const;
-const ADDED_REPLY = 'Added text' as const;
-
 
 test.describe('thread autoscroll', () => {
   test.afterEach(async ({controller}) => {
@@ -12,15 +12,16 @@ test.describe('thread autoscroll', () => {
   });
 
   test('only if I send a message and thread bottom is not visible', async ({controller, user, page}) => {
+    const addedReply = uuid();
     await controller.openStory('navigate-long-message-lists--user1', selectors.channelPreviewButton)
     await user.clicks.ChannelPreview.text(CHANNEL_NAME);
     await user.clicks.Thread.open('replies');
     await user.sees.Thread.contains.nthMessage('Message 149');
     await user.sees.Thread.start();
     await user.sees.Thread.contains.nthMessage('Message 0');
-    await user.sees.Thread.not.contains.nthMessage(ADDED_REPLY);
-    await user.submits.MessageInput.reply(ADDED_REPLY);
-    await user.sees.Thread.contains.nthMessage(ADDED_REPLY);
+    await user.sees.Thread.not.contains.nthMessage(addedReply);
+    await user.submits.MessageInput.reply(addedReply);
+    await user.sees.Thread.contains.nthMessage(addedReply);
     const threadMessageList = await page.evaluate(() => {
       return document.querySelector('.str-chat__thread-list');
     });
