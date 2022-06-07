badc567243bc8c2f95596030f8186fe4ab95b4d8
Anton Arnautov <arnautov.anton@gmail.com>
2022-06-07 15:57:03 +0200

    test: fix Thread linting issues

--- e2e/user/components/Thread/Thread.ts
+++ e2e/user/components/Thread/Thread.ts
@@ -2,69 +2,65 @@ import type { Page } from '@playwright/test';
 import selectors from '../../selectors';
 import { expect } from '@playwright/test';
 import { getMessage } from '../Message/MessageSimple';
-// import { getMessage, getMessageSelector } from '../Message/MessageSimple';
-// import { isVisible } from '../../utls';
 
-
-export default (page: Page) => {
-  return {
-    get: (prependSelectors?: string) => page.locator(`${prependSelectors || ''} ${selectors.threadMessageList}`),
-    click: {
-      async close() {
-        await page.click(selectors.buttonCloseThread);
-      },
-      async open(text: string) {
-        await page.locator(selectors.messageRepliesButton, { hasText: text }).click();
+export default (page: Page) => ({
+  click: {
+    close() {
+      return page.click(selectors.buttonCloseThread);
+    },
+    open(text: string) {
+      return page.locator(selectors.messageRepliesButton, { hasText: text }).click();
+    },
+  },
+  get: (prependSelectors?: string) =>
+    page.locator(`${prependSelectors || ''} ${selectors.threadMessageList}`),
+  see: {
+    empty() {
+      const replies = page.locator(`${selectors.threadReplyList}`);
+      return expect(replies).toBeEmpty();
+    },
+    inViewport: {
+      async nthMessage(text: string, nth?: number) {
+        const isThread = true;
+        await expect(getMessage(page, text, nth, isThread)).toBeVisible();
+        // todo: playwright's toBeVisible() does not differ, whether the element is in viewport or not
+        // => needed custom function to determine
+        // await page.waitForSelector(getMessageSelector(text, nth, isThread))
+        // const threadMsgListBox = await page.locator(selectors.threadMessageList).boundingBox();
+        // const msgBox = await getMessage(page, text, nth, isThread).boundingBox();
+        // expect(isVisible(msgBox, threadMsgListBox)).toBeTruthy();
       },
     },
-    see: {
-      inViewport: {
-        async nthMessage(text: string, nth?: number) {
+    not: {
+      empty() {
+        const replies = page.locator(selectors.threadReplyListWithReplies);
+        return expect(replies).not.toBeEmpty();
+      },
+      inDOM: {
+        nthMessage(text: string, nth?: number) {
           const isThread = true;
-          await expect(getMessage(page, text, nth, isThread)).toBeVisible();
+          const msgLocator = getMessage(page, text, nth, isThread);
+          return expect(msgLocator).toHaveCount(0);
+        },
+      },
+      inViewport: {
+        nthMessage(text: string, nth?: number) {
+          const isThread = true;
+          const msgLocator = getMessage(page, text, nth, isThread);
+          return expect(msgLocator).not.toBeVisible();
+
           // todo: playwright's toBeVisible() does not differ, whether the element is in viewport or not
           // => needed custom function to determine
-          // await page.waitForSelector(getMessageSelector(text, nth, isThread))
+
+          // await expect(msgLocator).toBeVisible();
           // const threadMsgListBox = await page.locator(selectors.threadMessageList).boundingBox();
-          // const msgBox = await getMessage(page, text, nth, isThread).boundingBox();
-          // expect(isVisible(msgBox, threadMsgListBox)).toBeTruthy();
-        },
-      },
-      async empty() {
-        const replies = page.locator(`${selectors.threadReplyList}`);
-        await expect(replies).toBeEmpty();
-      },
-      async start() {
-        await expect(page.locator(selectors.threadStart)).toBeVisible();
-      },
-      not: {
-        async empty() {
-          const replies = page.locator(selectors.threadReplyListWithReplies);
-          await expect(replies).not.toBeEmpty();
-        },
-        inDOM: {
-          async nthMessage(text: string, nth?: number) {
-            const isThread = true;
-            const msgLocator = getMessage(page, text, nth, isThread);
-            await expect(msgLocator).toHaveCount(0);
-          }
-        },
-        inViewport: {
-          async nthMessage(text: string, nth?: number) {
-            const isThread = true;
-            const msgLocator = getMessage(page, text, nth, isThread);
-            await expect(msgLocator).not.toBeVisible();
-
-            // todo: playwright's toBeVisible() does not differ, whether the element is in viewport or not
-            // => needed custom function to determine
-
-            // await expect(msgLocator).toBeVisible();
-            // const threadMsgListBox = await page.locator(selectors.threadMessageList).boundingBox();
-            // const msgBox = await msgLocator.boundingBox();
-            // expect(isVisible(msgBox, threadMsgListBox)).toBeFalsy();
-          },
+          // const msgBox = await msgLocator.boundingBox();
+          // expect(isVisible(msgBox, threadMsgListBox)).toBeFalsy();
         },
       },
     },
-  };
-}
+    start() {
+      return expect(page.locator(selectors.threadStart)).toBeVisible();
+    },
+  },
+});
