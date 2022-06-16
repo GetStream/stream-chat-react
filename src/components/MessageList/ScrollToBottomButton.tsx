e871b41148a54aeb7b75ea618b00a692d320c953
MartinCupela <martin.cupela@gmail.com>
2022-06-16 09:09:04 +0200

    feat: add new ScrollToBottomButton component

---
+++ src/components/MessageList/ScrollToBottomButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import type { MessageNotificationProps } from './MessageNotification';
+import { ArrowDown } from './icons';
+
+const UnMemoizedScrollToBottomButton = (props: Omit<MessageNotificationProps, 'children'>) => {
+  const { className = '', onClick, showNotification = true } = props;
+
+  if (!showNotification) return null;
+
+  return (
+    <div className='str-chat__jump-to-latest-message'>
+      <button
+        aria-live='polite'
+        className={`
+        str-chat__message-notification-right
+        str-chat__circle-fab
+        ${className}
+      `}
+        data-testid='message-notification'
+        onClick={onClick}
+      >
+        <ArrowDown />
+      </button>
+    </div>
+  );
+};
+
+export const ScrollToBottomButton = React.memo(
+  UnMemoizedScrollToBottomButton,
+) as typeof UnMemoizedScrollToBottomButton;
