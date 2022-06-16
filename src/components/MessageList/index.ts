5394d4065e0381a6d62bf2e4eef887da025d5fdd
MartinCupela <martin.cupela@gmail.com>
2022-06-16 09:45:45 +0200

    fix: export ScrollToBottomButton & ThreadHeader components from index files

--- src/components/MessageList/index.ts
+++ src/components/MessageList/index.ts
@@ -2,6 +2,7 @@ export * from './ConnectionStatus'; // TODO: export this under its own folder
 export * from './GiphyPreviewMessage';
 export * from './MessageList';
 export * from './MessageNotification';
+export * from './ScrollToBottomButton';
 export * from './VirtualizedMessageList';
 export * from './hooks';
 export * from './utils';
--- src/components/Thread/index.ts
+++ src/components/Thread/index.ts
@@ -1,1 +1,2 @@
 export * from './Thread';
+export * from './ThreadHeader';
