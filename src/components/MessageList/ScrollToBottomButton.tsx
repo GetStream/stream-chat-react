ef45de5163e1267b8d176b3083a8995194268b4f
MartinCupela <martin.cupela@gmail.com>
2022-06-23 14:20:41 +0200

    fix: remove custom class name prop forwarding in ScrollToBottomButton

--- src/components/MessageList/ScrollToBottomButton.tsx
+++ src/components/MessageList/ScrollToBottomButton.tsx
@@ -3,7 +3,7 @@ import type { MessageNotificationProps } from './MessageNotification';
 import { ArrowDown } from './icons';
 
 const UnMemoizedScrollToBottomButton = (props: Omit<MessageNotificationProps, 'children'>) => {
-  const { className = '', onClick, showNotification = true } = props;
+  const { onClick, showNotification = true } = props;
 
   if (!showNotification) return null;
 
@@ -15,7 +15,6 @@ const UnMemoizedScrollToBottomButton = (props: Omit<MessageNotificationProps, 'c
         str-chat__message-notification-right
         str-chat__message-notification-scroll-to-latest
         str-chat__circle-fab
-        ${className}
       `}
         data-testid='message-notification'
         onClick={onClick}
