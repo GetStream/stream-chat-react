52acd80b6b7ccd939aae357b0019f1b99975f585
MartinCupela <martin.cupela@gmail.com>
2022-07-05 13:56:21 +0200

    feat: add FileAttachment component for theming v2

--- src/components/Attachment/__tests__/File.test.js
+++ src/components/Attachment/__tests__/File.test.js
@@ -2,11 +2,16 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import renderer from 'react-test-renderer';
 import { FileAttachment } from '../FileAttachment';
+import { ChatContext } from '../../../context';
 
-const getComponent = (attachment) => <FileAttachment attachment={attachment} />;
+const getComponent = ({ attachment, chatContext }) => (
+  <ChatContext.Provider value={chatContext}>
+    <FileAttachment attachment={attachment} />
+  </ChatContext.Provider>
+);
 
 describe('File', () => {
-  it('should render File component', () => {
+  it('should render File component in V1', () => {
     const file = {
       asset_url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
       file_size: 1337,
@@ -15,7 +20,24 @@ describe('File', () => {
       title: 'Nice file',
       type: 'file',
     };
-    const tree = renderer.create(getComponent(file)).toJSON();
+    const tree = renderer
+      .create(getComponent({ attachment: file, chatContext: { themeVersion: '1' } }))
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render File component in V2', () => {
+    const file = {
+      asset_url: 'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+      file_size: 1337,
+      mime_type: 'application/pdf',
+      text: 'My file',
+      title: 'Nice file',
+      type: 'file',
+    };
+    const tree = renderer
+      .create(getComponent({ attachment: file, chatContext: { themeVersion: '2' } }))
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
