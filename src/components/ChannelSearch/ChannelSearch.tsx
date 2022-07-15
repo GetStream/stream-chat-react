a69fa115b44b27638ad01a5f04d6fbd7b8197a85
MartinCupela <martin.cupela@gmail.com>
2022-07-15 21:44:38 +0200

    feat: use SearchBar and useChannelSearch hook in ChannelSearch

--- src/components/ChannelSearch/ChannelSearch.tsx
+++ src/components/ChannelSearch/ChannelSearch.tsx
@@ -1,42 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
-import throttle from 'lodash.throttle';
+import React from 'react';
 
-import {
-  ChannelSearchFunctionParams,
-  SearchInput as DefaultSearchInput,
-  SearchInputProps,
-} from './SearchInput';
+import { SearchBar as DefaultSearchBar, SearchBarProps } from './SearchBar';
 import { DropdownContainerProps, SearchResultItemProps, SearchResults } from './SearchResults';
+import { useChannelSearch } from './hooks/useChannelSearch';
 
-import { ChannelOrUserResponse, isChannel } from './utils';
-
-import { useChatContext } from '../../context/ChatContext';
-
-import type {
-  ChannelFilters,
-  ChannelOptions,
-  ChannelSort,
-  UserFilters,
-  UserOptions,
-  UserSort,
-} from 'stream-chat';
-
+import type { ChannelOrUserResponse } from './utils';
 import type { DefaultStreamChatGenerics } from '../../types/types';
-
-export type SearchQueryParams<
-  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
-> = {
-  channelFilters?: {
-    filters?: ChannelFilters<StreamChatGenerics>;
-    options?: ChannelOptions;
-    sort?: ChannelSort<StreamChatGenerics>;
-  };
-  userFilters?: {
-    filters?: UserFilters<StreamChatGenerics>;
-    options?: UserOptions;
-    sort?: UserSort<StreamChatGenerics>;
-  };
-};
+import type { ChannelSearchFunctionParams, SearchQueryParams } from './hooks/useChannelSearch';
 
 export type ChannelSearchProps<
   StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
@@ -51,6 +21,8 @@ export type ChannelSearchProps<
   placeholder?: string;
   /** Display search results as an absolutely positioned popup, defaults to false and shows inline */
   popupResults?: boolean;
+  /** Custom UI component to display the search text input */
+  SearchBar?: React.ComponentType<SearchBarProps<StreamChatGenerics>>;
   /** Custom UI component to display empty search results */
   SearchEmpty?: React.ComponentType;
   /** Boolean to search for channels as well as users in the server query, default is false and just searches for users */
@@ -60,8 +32,6 @@ export type ChannelSearchProps<
     params: ChannelSearchFunctionParams<StreamChatGenerics>,
     event: React.BaseSyntheticEvent,
   ) => Promise<void> | void;
-  /** Custom UI component to display the search text input */
-  SearchInput?: React.ComponentType<SearchInputProps<StreamChatGenerics>>;
   /** Custom UI component to display the search loading state */
   SearchLoading?: React.ComponentType;
   /** Object containing filters/sort/options overrides for user search */
@@ -86,123 +56,29 @@ const UnMemoizedChannelSearch = <
     SearchEmpty,
     searchForChannels = false,
     searchFunction,
-    SearchInput = DefaultSearchInput,
+    SearchBar = DefaultSearchBar,
     SearchLoading,
     searchQueryParams,
     SearchResultItem,
     SearchResultsHeader,
   } = props;
 
-  const { client, setActiveChannel } = useChatContext<StreamChatGenerics>('ChannelSearch');
-
-  const [query, setQuery] = useState('');
-  const [results, setResults] = useState<Array<ChannelOrUserResponse<StreamChatGenerics>>>([]);
-  const [resultsOpen, setResultsOpen] = useState(false);
-  const [searching, setSearching] = useState(false);
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  const clearState = () => {
-    setQuery('');
-    setResults([]);
-    setResultsOpen(false);
-    setSearching(false);
-  };
-
-  useEffect(() => {
-    const clickListener = (event: MouseEvent) => {
-      if (resultsOpen && event.target instanceof HTMLElement) {
-        const isInputClick = inputRef.current?.contains(event.target);
-        if (!isInputClick) {
-          clearState();
-        }
-      }
-    };
-
-    document.addEventListener('click', clickListener);
-    return () => document.removeEventListener('click', clickListener);
-  }, [resultsOpen]);
-
-  const selectResult = async (result: ChannelOrUserResponse<StreamChatGenerics>) => {
-    if (!client.userID) return;
-
-    if (isChannel(result)) {
-      setActiveChannel(result);
-    } else {
-      const newChannel = client.channel(channelType, { members: [client.userID, result.id] });
-      await newChannel.watch();
-
-      setActiveChannel(newChannel);
-    }
-    clearState();
-  };
-
-  const getChannels = async (text: string) => {
-    if (!text || searching) return;
-    setSearching(true);
-
-    try {
-      const userResponse = await client.queryUsers(
-        // @ts-expect-error
-        {
-          $or: [{ id: { $autocomplete: text } }, { name: { $autocomplete: text } }],
-          id: { $ne: client.userID },
-          ...searchQueryParams?.userFilters?.filters,
-        },
-        { id: 1, ...searchQueryParams?.userFilters?.sort },
-        { limit: 8, ...searchQueryParams?.userFilters?.options },
-      );
-
-      if (searchForChannels) {
-        const channelResponse = client.queryChannels(
-          // @ts-expect-error
-          {
-            name: { $autocomplete: text },
-            ...searchQueryParams?.channelFilters?.filters,
-          },
-          searchQueryParams?.channelFilters?.sort || {},
-          { limit: 5, ...searchQueryParams?.channelFilters?.options },
-        );
-
-        const [channels, { users }] = await Promise.all([channelResponse, userResponse]);
-
-        setResults([...channels, ...users]);
-        setResultsOpen(true);
-        setSearching(false);
-        return;
-      }
-
-      const { users } = await Promise.resolve(userResponse);
-
-      setResults(users);
-      setResultsOpen(true);
-    } catch (error) {
-      clearState();
-      console.error(error);
-    }
-
-    setSearching(false);
-  };
-
-  const getChannelsThrottled = throttle(getChannels, 200);
-
-  const onSearch = (event: React.BaseSyntheticEvent) => {
-    event.preventDefault();
-    setQuery(event.target.value);
-    getChannelsThrottled(event.target.value);
-  };
-
-  const channelSearchParams = {
-    setQuery,
-    setResults,
-    setResultsOpen,
-    setSearching,
-  };
+  const {
+    channelSearchParams,
+    clearState,
+    inputRef,
+    onSearch,
+    query,
+    results,
+    searching,
+    selectResult,
+  } = useChannelSearch<StreamChatGenerics>({ channelType, searchForChannels, searchQueryParams });
 
   return (
     <div className='str-chat__channel-search' data-testid='channel-search'>
-      <SearchInput
+      <SearchBar
         channelSearchParams={channelSearchParams}
+        clearState={clearState}
         inputRef={inputRef}
         onSearch={onSearch}
         placeholder={placeholder}
