2624dc7f4c7ae3df56a23fb9ddf2513d2245e61a
MartinCupela <martin.cupela@gmail.com>
2022-07-25 21:30:27 +0200

    refactor(SearchInput): separate type for props provided by search controller

--- src/components/ChannelSearch/SearchInput.tsx
+++ src/components/ChannelSearch/SearchInput.tsx
@@ -2,36 +2,33 @@ import React from 'react';
 
 import { useTranslationContext } from '../../context/TranslationContext';
 
-import type { ChannelSearchProps } from './ChannelSearch';
-import type { SearchControllerState } from './hooks/useChannelSearch';
-import type { DefaultStreamChatGenerics } from '../../types/types';
+export type SearchInputController = {
+  /** Clears the channel search state */
+  clearState: () => void;
+  inputRef: React.RefObject<HTMLInputElement>;
+  /** Search input change handler */
+  onSearch: (event: React.BaseSyntheticEvent) => void;
+  /** Current search string */
+  query: string;
+};
 
-export type SearchInputProps<
-  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
-> = Pick<
-  SearchControllerState<StreamChatGenerics>,
-  'channelSearchParams' | 'clearState' | 'inputRef' | 'onSearch' | 'query'
-> &
-  Pick<ChannelSearchProps<StreamChatGenerics>, 'searchFunction'> & {
-    /** Custom placeholder text to be displayed in the search input */
-    placeholder?: string;
-  };
+export type AdditionalSearchInputProps = {
+  /** Custom placeholder text to be displayed in the search input */
+  placeholder?: string;
+};
 
-export const SearchInput = <
-  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
->(
-  props: SearchInputProps<StreamChatGenerics>,
-) => {
-  const { channelSearchParams, inputRef, onSearch, placeholder, query, searchFunction } = props;
+export type SearchInputProps = AdditionalSearchInputProps & SearchInputController;
+
+export const SearchInput = (props: SearchInputProps) => {
+  const { inputRef, onSearch, placeholder, query } = props;
 
   const { t } = useTranslationContext('SearchInput');
 
   return (
     <input
       className='str-chat__channel-search-input'
-      onChange={(event: React.BaseSyntheticEvent) =>
-        searchFunction ? searchFunction(channelSearchParams, event) : onSearch(event)
-      }
+      data-testid='search-input'
+      onChange={onSearch}
       placeholder={placeholder ?? t('Search')}
       ref={inputRef}
       type='text'
