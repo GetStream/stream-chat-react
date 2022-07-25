ca38f57561e0d758160984ce4ebaed2b3c777430
MartinCupela <martin.cupela@gmail.com>
2022-07-26 00:21:22 +0200

    refactor: make ChannelSearch a composite of SearchInput and SearchResults

--- src/components/ChannelSearch/ChannelSearch.tsx
+++ src/components/ChannelSearch/ChannelSearch.tsx
@@ -1,46 +1,26 @@
 import React from 'react';
 
-import { SearchBar as DefaultSearchBar, SearchBarProps } from './SearchBar';
-import { DropdownContainerProps, SearchResultItemProps, SearchResults } from './SearchResults';
-import { useChannelSearch } from './hooks/useChannelSearch';
+import {
+  AdditionalSearchInputProps,
+  SearchInput as DefaultSearchInput,
+  SearchInputProps,
+} from './SearchInput';
+import { AdditionalSearchResultsProps, SearchResults } from './SearchResults';
 
-import type { ChannelOrUserResponse } from './utils';
 import type { DefaultStreamChatGenerics } from '../../types/types';
-import type { ChannelSearchFunctionParams, SearchQueryParams } from './hooks/useChannelSearch';
+import type { SearchController } from './hooks/useChannelSearch';
+
+export type AdditionalChannelSearchProps<
+  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
+> = AdditionalSearchInputProps &
+  AdditionalSearchResultsProps<StreamChatGenerics> & {
+    /** Custom UI component to display the search text input */
+    SearchInput?: React.ComponentType<SearchInputProps>;
+  };
 
 export type ChannelSearchProps<
   StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
-> = {
-  /** The type of channel to create on user result select, defaults to `messaging` */
-  channelType?: string;
-  /** Custom UI component to display all of the search results, defaults to accepts same props as: [DefaultDropdownContainer](https://github.com/GetStream/stream-chat-react/blob/master/src/components/ChannelSearch/SearchResults.tsx)  */
-  DropdownContainer?: React.ComponentType<DropdownContainerProps<StreamChatGenerics>>;
-  /** Custom handler function to run on search result item selection */
-  onSelectResult?: (result: ChannelOrUserResponse<StreamChatGenerics>) => Promise<void> | void;
-  /** Custom placeholder text to be displayed in the search input */
-  placeholder?: string;
-  /** Display search results as an absolutely positioned popup, defaults to false and shows inline */
-  popupResults?: boolean;
-  /** Custom UI component to display the search text input */
-  SearchBar?: React.ComponentType<SearchBarProps<StreamChatGenerics>>;
-  /** Custom UI component to display empty search results */
-  SearchEmpty?: React.ComponentType;
-  /** Boolean to search for channels as well as users in the server query, default is false and just searches for users */
-  searchForChannels?: boolean;
-  /** Custom search function to override default */
-  searchFunction?: (
-    params: ChannelSearchFunctionParams<StreamChatGenerics>,
-    event: React.BaseSyntheticEvent,
-  ) => Promise<void> | void;
-  /** Custom UI component to display the search loading state */
-  SearchLoading?: React.ComponentType;
-  /** Object containing filters/sort/options overrides for user search */
-  searchQueryParams?: SearchQueryParams<StreamChatGenerics>;
-  /** Custom UI component to display a search result list item, defaults to and accepts same props as: [DefaultSearchResultItem](https://github.com/GetStream/stream-chat-react/blob/master/src/components/ChannelSearch/SearchResults.tsx) */
-  SearchResultItem?: React.ComponentType<SearchResultItemProps<StreamChatGenerics>>;
-  /** Custom UI component to display the search results header */
-  SearchResultsHeader?: React.ComponentType;
-};
+> = AdditionalChannelSearchProps<StreamChatGenerics> & SearchController<StreamChatGenerics>;
 
 const UnMemoizedChannelSearch = <
   StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
@@ -48,46 +28,34 @@ const UnMemoizedChannelSearch = <
   props: ChannelSearchProps<StreamChatGenerics>,
 ) => {
   const {
-    channelType = 'messaging',
-    DropdownContainer,
-    onSelectResult,
-    placeholder,
-    popupResults = false,
-    SearchEmpty,
-    searchForChannels = false,
-    searchFunction,
-    SearchBar = DefaultSearchBar,
-    SearchLoading,
-    searchQueryParams,
-    SearchResultItem,
-    SearchResultsHeader,
-  } = props;
-
-  const {
-    channelSearchParams,
     clearState,
+    SearchResultsList,
     inputRef,
     onSearch,
+    placeholder,
+    popupResults = false,
     query,
     results,
+    SearchEmpty,
     searching,
+    SearchInput = DefaultSearchInput,
+    SearchLoading,
+    SearchResultItem,
+    SearchResultsHeader,
     selectResult,
-  } = useChannelSearch<StreamChatGenerics>({ channelType, searchForChannels, searchQueryParams });
+  } = props;
 
   return (
     <div className='str-chat__channel-search' data-testid='channel-search'>
-      <SearchBar
-        channelSearchParams={channelSearchParams}
+      <SearchInput
         clearState={clearState}
         inputRef={inputRef}
         onSearch={onSearch}
         placeholder={placeholder}
         query={query}
-        searchFunction={searchFunction}
       />
       {query && (
         <SearchResults
-          DropdownContainer={DropdownContainer}
           popupResults={popupResults}
           results={results}
           SearchEmpty={SearchEmpty}
@@ -95,7 +63,8 @@ const UnMemoizedChannelSearch = <
           SearchLoading={SearchLoading}
           SearchResultItem={SearchResultItem}
           SearchResultsHeader={SearchResultsHeader}
-          selectResult={onSelectResult || selectResult}
+          SearchResultsList={SearchResultsList}
+          selectResult={selectResult}
         />
       )}
     </div>
