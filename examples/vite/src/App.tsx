a6baff811f3314a7322f74c06396b2fea4a33b93
Anton Arnautov <arnautov.anton@gmail.com>
2022-07-27 22:42:16 +0200

    chore(examples/vite): update Vite example

--- examples/vite/src/App.tsx
+++ examples/vite/src/App.tsx
@@ -10,13 +10,17 @@ import {
   Thread,
   Window,
 } from 'stream-chat-react';
-import '@stream-io/stream-chat-css/dist/css/index.css';
+import '@stream-io/stream-chat-css/dist/v2/css/index.css';
+
+const params = (new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, property) => searchParams.get(property as string),
+}) as unknown) as Record<string, string | null>;
 
 const apiKey = import.meta.env.VITE_STREAM_KEY as string;
-const userId = import.meta.env.VITE_USER_ID as string;
-const userToken = import.meta.env.VITE_USER_TOKEN as string;
+const userId = params.uid ?? (import.meta.env.VITE_USER_ID as string);
+const userToken = params.ut ?? (import.meta.env.VITE_USER_TOKEN as string);
 
-const filters: ChannelFilters = { type: 'messaging', members: {$in: [userId]}  };
+const filters: ChannelFilters = { members: { $in: [userId] }, type: 'messaging' };
 const options: ChannelOptions = { limit: 10, presence: true, state: true };
 const sort: ChannelSort = { last_message_at: -1, updated_at: -1 };
 
