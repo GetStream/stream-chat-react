7744c6384102caa958b64f0bb8c9c2684f9aba87
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2022-08-02 11:48:52 +0200

    chore: add test which overrides the mention component

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import renderer from 'react-test-renderer';
 import { renderText } from '../utils';
 
@@ -33,6 +34,22 @@ describe(`renderText`, () => {
     const Markdown = renderText(
       '@username@email.com @username@email.com username@email.com @username@email.com',
       [{ id: 'id-username@email.com', name: 'username@email.com' }],
+    );
+    const tree = renderer.create(Markdown).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders custom mention', () => {
+    const Markdown = renderText(
+      '@username@email.com @username@email.com username@email.com @username@email.com',
+      [{ id: 'id-username@email.com', name: 'username@email.com' }],
+      {
+        customMarkDownRenderers: {
+          mention: function MyMention(props) {
+            return <span className='my-mention'>{props.children}</span>;
+          },
+        },
+      },
     );
     const tree = renderer.create(Markdown).toJSON();
     expect(tree).toMatchSnapshot();
