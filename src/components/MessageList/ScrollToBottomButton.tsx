d83000fcd95b2a41dedfd2813e8b833857143d48
MartinCupela <martin.cupela@gmail.com>
2022-08-10 17:19:10 +0200

    feat: show ScrollToBottomButton on scroll above the threshold & show unread counter

--- src/components/MessageList/ScrollToBottomButton.tsx
+++ src/components/MessageList/ScrollToBottomButton.tsx
@@ -1,11 +1,66 @@
-import React from 'react';
-import type { MessageNotificationProps } from './MessageNotification';
+import React, { useEffect, useState } from 'react';
+import clsx from 'clsx';
+
 import { ArrowDown } from './icons';
 
-const UnMemoizedScrollToBottomButton = (props: Omit<MessageNotificationProps, 'children'>) => {
-  const { onClick, showNotification = true } = props;
+import { useChannelStateContext, useChatContext } from '../../context';
 
-  if (!showNotification) return null;
+import type { Event } from 'stream-chat';
+import type { MessageNotificationProps } from './MessageNotification';
+
+const UnMemoizedScrollToBottomButton = (
+  props: Pick<MessageNotificationProps, 'isMessageListScrolledToBottom' | 'onClick' | 'threadList'>,
+) => {
+  const { isMessageListScrolledToBottom, onClick, threadList } = props;
+
+  const { channel: activeChannel, client } = useChatContext();
+  const { thread } = useChannelStateContext();
+  const [countUnread, setCountUnread] = useState(activeChannel?.countUnread() || 0);
+  const [replyCount, setReplyCount] = useState(thread?.reply_count || 0);
+  const observedEvent = threadList ? 'message.updated' : 'message.new';
+
+  useEffect(() => {
+    const handleEvent = (event: Event) => {
+      const newMessageInAnotherChannel = event.cid !== activeChannel?.cid;
+      const newMessageIsMine = event.user?.id === client.user?.id;
+
+      const isThreadOpen = !!thread;
+      const newMessageIsReply = !!event.message?.parent_id;
+      const dontIncreaseMainListCounterOnNewReply =
+        isThreadOpen && !threadList && newMessageIsReply;
+
+      if (
+        isMessageListScrolledToBottom ||
+        newMessageInAnotherChannel ||
+        newMessageIsMine ||
+        dontIncreaseMainListCounterOnNewReply
+      ) {
+        return;
+      }
+
+      if (event.type === 'message.new') {
+        // cannot rely on channel.countUnread because active channel is automatically marked read
+        setCountUnread((prev) => prev + 1);
+      } else if (event.message?.id === thread?.id) {
+        const newReplyCount = event.message?.reply_count || 0;
+        setCountUnread(() => newReplyCount - replyCount);
+      }
+    };
+    client.on(observedEvent, handleEvent);
+
+    return () => {
+      client.off(observedEvent, handleEvent);
+    };
+  }, [activeChannel, isMessageListScrolledToBottom, replyCount, thread]);
+
+  useEffect(() => {
+    if (isMessageListScrolledToBottom) {
+      setCountUnread(0);
+      setReplyCount(thread?.reply_count || 0);
+    }
+  }, [isMessageListScrolledToBottom, thread]);
+
+  if (isMessageListScrolledToBottom) return null;
 
   return (
     <div className='str-chat__jump-to-latest-message'>
@@ -20,6 +75,18 @@ const UnMemoizedScrollToBottomButton = (props: Omit<MessageNotificationProps, 'c
         onClick={onClick}
       >
         <ArrowDown />
+        {countUnread > 0 && (
+          <div
+            className={clsx(
+              'str-chat__message-notification',
+              'str-chat__message-notification-scroll-to-latest-unread-count',
+              'str-chat__jump-to-latest-unread-count',
+            )}
+            data-testid={'unread-message-notification-counter'}
+          >
+            {countUnread}
+          </div>
+        )}
       </button>
     </div>
   );
