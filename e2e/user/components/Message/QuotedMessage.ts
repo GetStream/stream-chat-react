2ae074bff48580d107c4e58e1cd72ce05b4821fe
MartinCupela <martin.cupela@gmail.com>
2022-08-16 16:40:37 +0200

    fix(e2e): fix failing tests

--- e2e/user/components/Message/QuotedMessage.ts
+++ e2e/user/components/Message/QuotedMessage.ts
@@ -10,7 +10,9 @@ export default (page: Page) => ({
   click: {
     //"nth-match" engine expects a one-based index as the last argument
     nth(text: string, index = 1) {
-      return page.click(`${selectors.quotedMessage} :nth-match(:text("${text}"),${index})`);
+      return page.click(`${selectors.quotedMessage} :nth-match(:text("${text}"),${index})`, {
+        force: true, // onClickCapture registered on the quoted message intercepts pointer events
+      });
     },
   },
   get: (text: string) => getQuotedMessage(page, text),
