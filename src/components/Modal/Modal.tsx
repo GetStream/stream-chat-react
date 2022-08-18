7acd4afe6b4955453b6fdf1e9b237b7c25208e62
MartinCupela <martin.cupela@gmail.com>
2022-08-18 10:03:06 +0200

    feat(Modal): allow for addition of custom class & allow to control close button display

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import React, { PropsWithChildren, useEffect, useRef } from 'react';
 
 import { CloseIconRound } from './icons';
@@ -7,13 +8,23 @@ import { useChatContext, useTranslationContext } from '../../context';
 export type ModalProps = {
   /** If true, modal is opened or visible. */
   open: boolean;
+  /** Custom classes for modal elements */
+  className?: { content?: string; overlay?: string };
+  /** If true, the close button will not be rendered */
+  hideCloseButton?: boolean;
   /** Callback handler for closing of modal. */
   onClose?: (
     event: React.KeyboardEvent | React.MouseEvent<HTMLButtonElement | HTMLDivElement>,
   ) => void;
 };
 
-export const Modal = ({ children, onClose, open }: PropsWithChildren<ModalProps>) => {
+export const Modal = ({
+  children,
+  className,
+  hideCloseButton,
+  onClose,
+  open,
+}: PropsWithChildren<ModalProps>) => {
   const { t } = useTranslationContext('Modal');
   const { themeVersion } = useChatContext('Modal');
 
@@ -22,9 +33,9 @@ export const Modal = ({ children, onClose, open }: PropsWithChildren<ModalProps>
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement | HTMLDivElement>) => {
     const target = event.target as HTMLButtonElement | HTMLDivElement;
-    if (!innerRef.current || !closeRef.current) return;
+    if (!innerRef.current) return;
 
-    if (!innerRef.current.contains(target) || closeRef.current.contains(target)) onClose?.(event);
+    if (!innerRef.current.contains(target) || closeRef?.current?.contains(target)) onClose?.(event);
   };
 
   useEffect(() => {
@@ -41,23 +52,31 @@ export const Modal = ({ children, onClose, open }: PropsWithChildren<ModalProps>
   if (!open) return null;
 
   return (
-    <div className='str-chat__modal str-chat__modal--open' onClick={handleClick}>
-      <button className='str-chat__modal__close-button' ref={closeRef} title={t<string>('Close')}>
-        {themeVersion === '2' && <CloseIconRound />}
-
-        {themeVersion === '1' && (
-          <>
-            {t<string>('Close')}
-            <svg height='10' width='10' xmlns='http://www.w3.org/2000/svg'>
-              <path
-                d='M9.916 1.027L8.973.084 5 4.058 1.027.084l-.943.943L4.058 5 .084 8.973l.943.943L5 5.942l3.973 3.974.943-.943L5.942 5z'
-                fillRule='evenodd'
-              />
-            </svg>
-          </>
-        )}
-      </button>
-      <div className='str-chat__modal__inner str-chat-react__modal__inner' ref={innerRef}>
+    <div
+      className={clsx('str-chat__modal', 'str-chat__modal--open', className?.overlay)}
+      data-testid='modal-overlay'
+      onClick={handleClick}
+    >
+      {!hideCloseButton && (
+        <button className='str-chat__modal__close-button' ref={closeRef} title={t<string>('Close')}>
+          {themeVersion === '2' && <CloseIconRound />}
+          {themeVersion === '1' && (
+            <>
+              {t<string>('Close')}
+              <svg height='10' width='10' xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  d='M9.916 1.027L8.973.084 5 4.058 1.027.084l-.943.943L4.058 5 .084 8.973l.943.943L5 5.942l3.973 3.974.943-.943L5.942 5z'
+                  fillRule='evenodd'
+                />
+              </svg>
+            </>
+          )}
+        </button>
+      )}
+      <div
+        className={clsx('str-chat__modal__inner str-chat-react__modal__inner', className?.content)}
+        ref={innerRef}
+      >
         {children}
       </div>
     </div>
