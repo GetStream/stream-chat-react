cb65b3d24ce2261763003b59aeb0a4ea4da4be03
Anton Arnautov <arnautov.anton@gmail.com>
2022-08-25 17:15:11 +0200

    test: update Next and Remix (.sh) paths

--- test-react-frameworks/next/next.sh
+++ test-react-frameworks/next/next.sh
@@ -7,10 +7,8 @@ yarn create next-app --typescript test-next-app
 # Apply a hello-world chat page to the project. 
 cp ./next-page.tsx test-next-app/pages/index.tsx
 
-cd test-next-app
-
 # install the lib...
-yarn add --dev ../stream-chat-react.tgz
+yarn --cwd ./test-next-app add --dev ../../stream-chat-react.tgz
 
 # and build - $? should be 0
-yarn build
+yarn --cwd ./test-next-app build
--- test-react-frameworks/remix/remix.sh
+++ test-react-frameworks/remix/remix.sh
@@ -7,10 +7,8 @@ yes | yarn create remix --template remix-run/indie-stack test-remix-app
 # Apply a hello-world chat page to the project. 
 cp ./remix-index-route.tsx test-remix-app/app/routes/index.tsx
 
-cd test-remix-app
-
 # install the lib...
-yarn add --dev ../stream-chat-react.tgz
+yarn --cwd ./test-remix-app add --dev ../../stream-chat-react.tgz
 
 # and build - $? should be 0
-yarn build
+yarn --cwd ./test-remix-app build
