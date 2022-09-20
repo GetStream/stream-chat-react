7ac8df79b593ce002296c9cede4912e9b19f76a1
MartinCupela <martin.cupela@gmail.com>
2022-09-20 16:04:20 +0200

    docs: add missing doc strings to LoadMorePaginatorProps and TypingIndicatorProps

--- src/components/LoadMore/LoadMorePaginator.tsx
+++ src/components/LoadMore/LoadMorePaginator.tsx
@@ -5,10 +5,13 @@ import { LoadMoreButton as DefaultLoadMoreButton, LoadMoreButtonProps } from './
 export type LoadMorePaginatorProps = {
   /** callback to load the next page */
   loadNextPage: () => void;
+  /** Boolean for if there is a next page to load */
   hasNextPage?: boolean;
+  /** A UI button component that handles pagination logic */
   LoadMoreButton?: React.ComponentType<LoadMoreButtonProps>;
-  /** indicates if there there's currently any refreshing taking place */
+  /** indicates if there's currently any refreshing taking place */
   refreshing?: boolean;
+  /** indicates if the `LoadMoreButton` should be displayed at the top of the list of channels instead of the bottom of the list (the default) */
   reverse?: boolean;
 };
 
