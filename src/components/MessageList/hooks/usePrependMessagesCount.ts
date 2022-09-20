122a53891465f98c8b6dbfb1041a323b6edc88cc
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2022-09-20 11:31:47 +0200

    fix: edge case of prepend count not being reset when jumping (#1765)
    * fix: edge case of prepend count not being reset when jumping
    * fix: reset previously prepended items value
    
    Co-authored-by: Petyo Ivanov <underlog@gmail.com>
    (cherry picked from commit 18ba8f83261ec8157651721404906e0f00c92fd4)

--- src/components/MessageList/hooks/usePrependMessagesCount.ts
+++ src/components/MessageList/hooks/usePrependMessagesCount.ts
@@ -8,15 +8,17 @@ export function usePrependedMessagesCount<
   StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
 >(messages: StreamMessage<StreamChatGenerics>[], hasDateSeparator: boolean) {
   const firstRealMessageIndex = hasDateSeparator ? 1 : 0;
-  const currentFirstMessageId = messages?.[firstRealMessageIndex]?.id;
-  const firstMessageId = useRef(currentFirstMessageId);
-  const earliestMessageId = useRef(currentFirstMessageId);
+  const firstMessageId = useRef<string>();
+  const earliestMessageId = useRef<string>();
   const previousNumItemsPrepended = useRef(0);
 
   const numItemsPrepended = useMemo(() => {
     if (!messages || !messages.length) {
+      previousNumItemsPrepended.current = 0;
       return 0;
     }
+
+    const currentFirstMessageId = messages?.[firstRealMessageIndex]?.id;
     // if no new messages were prepended, return early (same amount as before)
     if (currentFirstMessageId === earliestMessageId.current) {
       return previousNumItemsPrepended.current;
@@ -35,9 +37,9 @@ export function usePrependedMessagesCount<
       }
     }
 
-    // if no match has found, we have jumped
+    // if no match has found, we have jumped - reset the prepend item count.
     firstMessageId.current = currentFirstMessageId;
-
+    previousNumItemsPrepended.current = 0;
     return 0;
     // TODO: there's a bug here, the messages prop is the same array instance (something mutates it)
     // that's why the second dependency is necessary
