cc1736980d5a98f836191460e4b50b0d29c3e8dc
Zita Szupera <szuperaz@gmail.com>
2022-10-03 14:41:06 +0200

    chore: watch scroll position in attachment sizing e2e tests

--- e2e/attachment-sizing.test.ts
+++ e2e/attachment-sizing.test.ts
@@ -5,11 +5,14 @@ import selectors from './user/selectors';
 import { test } from './user/test';
 
 import ChannelPreview from './user/components/ChannelPreview';
+import MessageList from './user/components/MessageList/MessageList';
 
 dotenv.config();
 dotenv.config({ path: `.env.local` });
 
 const CHANNEL_NAME = 'attachment-sizing' as const;
+const user1Id = process.env.E2E_TEST_USER_1;
+const USER1_CHAT_VIEW_CLASSNAME = `.${user1Id}`;
 
 test.describe('add height to video and image attachments', () => {
   test.beforeEach(async ({ controller, user }) => {
@@ -20,7 +23,7 @@ test.describe('add height to video and image attachments', () => {
     await user.clicks(ChannelPreview).text(CHANNEL_NAME);
   });
 
-  test('should add height for video attachments', async ({ page }) => {
+  test('should add height for video attachments', async ({ page, user }) => {
     const videoElementsLocator = page.locator('[data-testid="video-wrapper"]');
     const result = await videoElementsLocator.evaluateAll<boolean>((videoElements => videoElements.length > 0 && videoElements.every(element =>
       getComputedStyle(element).height.includes('px')
@@ -28,19 +31,28 @@ test.describe('add height to video and image attachments', () => {
 
 
     expect(result).toBeTruthy();
+    await user
+      .sees(MessageList)
+      .isScrolledToBottom(`${USER1_CHAT_VIEW_CLASSNAME} ${selectors.messageList}`);
   });
 
-  test('should add height for single image attachments', async ({page}) => {
+  test('should add height for single image attachments', async ({page, user}) => {
     const imageElementsLocator = page.locator('[data-testid="image-test"]');
     const result = await imageElementsLocator.evaluateAll((imageElements => imageElements.length > 0 && imageElements.every(element => getComputedStyle(element).height.includes('px'))));
 
     expect(result).toBe(true);
+    await user
+      .sees(MessageList)
+      .isScrolledToBottom(`${USER1_CHAT_VIEW_CLASSNAME} ${selectors.messageList}`);
   });
 
-  test('should add height for gallery image attachments', async ({page}) => {
+  test('should add height for gallery image attachments', async ({page, user}) => {
     const imageElementsLocator = page.locator('[data-testid="gallery-image-last"],[data-testid="gallery-image"]');
     const result = await imageElementsLocator.evaluateAll((imageElements => imageElements.length > 0 && imageElements.every(element => getComputedStyle(element).height.includes('px'))));
 
     expect(result).toBe(true);
+    await user
+      .sees(MessageList)
+      .isScrolledToBottom(`${USER1_CHAT_VIEW_CLASSNAME} ${selectors.messageList}`);
   });
 });
