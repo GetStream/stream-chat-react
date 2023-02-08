689514c2f9bcf36a2af8d9950518c0f3d4441d28
MartinCupela <32706194+MartinCupela@users.noreply.github.com>
2023-02-08 13:13:49 +0100

    fix: reflect thread prop in Window component (#1919)

--- src/components/Window/__tests__/Window.test.js
+++ src/components/Window/__tests__/Window.test.js
@@ -15,6 +15,7 @@ const renderComponent = ({ channelStateContextMock, children, props }) =>
   );
 
 const thread = generateMessage();
+const HIDE_CLASS_NAME = 'str-chat__main-panel--hideOnThread';
 
 describe('Window', () => {
   it('should render its children if hideOnThread is false and thread is truthy', () => {
@@ -40,4 +41,30 @@ describe('Window', () => {
 
     expect(getByText('bla')).toBeInTheDocument();
   });
+  it.each([
+    ['not add', 'truthy', 'falsy', 'falsy', true, undefined, undefined],
+    ['add', 'truthy', 'truthy', 'falsy', true, thread, undefined],
+    ['add', 'truthy', 'falsy', 'truthy', true, undefined, thread],
+    ['add', 'truthy', 'truthy', 'truthy', true, thread, thread],
+    ['not add', 'falsy', 'falsy', 'falsy', false, undefined, undefined],
+    ['not add', 'falsy', 'truthy', 'falsy', false, thread, undefined],
+    ['not add', 'falsy', 'falsy', 'truthy', false, undefined, thread],
+    ['not add', 'falsy', 'truthy', 'truthy', false, thread, thread],
+  ])(
+    'should %s class str-chat__main-panel--hideOnThread if hideOnThread is %s, prop thread is %s, context thread is %s',
+    (expectation, _, __, ___, hideOnThread, propThread, contextThread) => {
+      const { container } = renderComponent({
+        channelStateContextMock: {
+          thread: contextThread,
+        },
+        children: [<div key='bla'>bla</div>],
+        props: { hideOnThread, thread: propThread },
+      });
+
+      if (expectation === 'add')
+        expect(container.querySelector(`.${HIDE_CLASS_NAME}`)).toBeInTheDocument();
+      if (expectation === 'not add')
+        expect(container.querySelector(`.${HIDE_CLASS_NAME}`)).not.toBeInTheDocument();
+    },
+  );
 });
