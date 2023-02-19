cf1b55e033580dce01b7d9027ee72a2636931c01
Anton Arnautov <arnautov.anton@gmail.com>
2023-02-20 00:51:30 +0100

    feat(Reactions): introduce SpriteImage, StreamEmoji and defaultReactionOptions

---
+++ src/components/Reactions/SpriteImage.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+
+import { getImageDimensions } from './utils/utils';
+
+export type SpriteImageProps = {
+  columns: number;
+  position: [number, number];
+  rows: number;
+  spriteUrl: string;
+  fallback?: React.ReactNode;
+  height?: number;
+  width?: number;
+};
+
+export const SpriteImage = ({
+  columns,
+  fallback,
+  height,
+  position,
+  rows,
+  spriteUrl,
+  width,
+}: SpriteImageProps) => {
+  const [[spriteWidth, spriteHeight], setSpriteDimensions] = useState([0, 0]);
+
+  useEffect(() => {
+    getImageDimensions(spriteUrl).then(setSpriteDimensions);
+  }, [spriteUrl]);
+
+  const [x, y] = position;
+  const spriteItemWidth = spriteWidth / columns;
+  const spriteItemHeight = spriteHeight / rows;
+
+  let resizeRatio = 1;
+
+  if (!width && height) resizeRatio = height / spriteItemHeight;
+  if (width && !height) resizeRatio = width / spriteItemWidth;
+
+  if (resizeRatio === Infinity) resizeRatio = 1;
+
+  if (!spriteHeight || !spriteWidth) return <>{fallback}</>;
+
+  return (
+    <div
+      style={{
+        backgroundImage: `url('${spriteUrl}')`,
+        backgroundPosition: `${x * (100 / (columns - 1))}% ${y * (100 / (rows - 1))}%`,
+        backgroundSize: `${columns * 100}% ${rows * 100}%`,
+        height: height ?? spriteItemHeight * resizeRatio,
+        width: width ?? spriteItemWidth * resizeRatio,
+      }}
+    />
+  );
+};
