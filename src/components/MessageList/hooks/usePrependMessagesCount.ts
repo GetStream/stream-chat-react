f1bf6fa8b21523e968b8eb9bb30ce5beb6eca9da
MartinCupela <32706194+MartinCupela@users.noreply.github.com>
2023-03-16 13:41:04 +0100

    fix: do not increase count of prepended VirtualizedMessageList messages of status "sending" or "failed" (#1972)
    * fix: do not increase count of prepended VirtualizedMessageList messages of status "sending"
    
    * fix: do not increase count of prepended VirtualizedMessageList messages of status "failed"
    
    * fix: keep messages without status in the prepend count
    
    * test: ignore messages of status "sending" & "failed" from prepend count

--- src/components/MessageList/hooks/usePrependMessagesCount.ts
+++ src/components/MessageList/hooks/usePrependMessagesCount.ts
@@ -4,6 +4,8 @@ import type { StreamMessage } from '../../../context/ChannelStateContext';
 
 import type { DefaultStreamChatGenerics } from '../../../types/types';
 
+const STATUSES_EXCLUDED_FROM_PREPEND = ['sending', 'failed'];
+
 export function usePrependedMessagesCount<
   StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
 >(messages: StreamMessage<StreamChatGenerics>[], hasDateSeparator: boolean) {
@@ -30,14 +32,33 @@ export function usePrependedMessagesCount<
     earliestMessageId.current = currentFirstMessageId;
     // if new messages were prepended, find out how many
     // start with this number because there cannot be fewer prepended items than before
+    let adjustPrependedMessageCount = 0;
     for (let i = previousNumItemsPrepended.current; i < messages.length; i += 1) {
+      // Optimistic UI update, when sending messages, can lead to a situation, when
+      // the order of the messages changes for a moment. This can happen, when a user
+      // sends multiple messages withing few milliseconds. E.g. we send a message A
+      // then message B. At first we have message array with both messages of status "sending"
+      // then response for message A is received with a new - later - created_at timestamp
+      // this leads to rearrangement of 1.B ("sending"), 2.A ("received"). Still firstMessageId.current
+      // points to message A, but now this message has index 1 => previousNumItemsPrepended.current === 1
+      // That in turn leads to incorrect index calculation in VirtualizedMessageList trying to access a message
+      // at non-existent index. Therefore, we ignore messages of status "sending" / "failed" in order they are
+      // not considered as prepended messages.
+      if (
+        messages[i]?.status &&
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        STATUSES_EXCLUDED_FROM_PREPEND.includes(messages[i].status!) &&
+        messages[i].id !== firstMessageId.current
+      ) {
+        adjustPrependedMessageCount++;
+      }
       if (messages[i].id === firstMessageId.current) {
-        previousNumItemsPrepended.current = i;
-        return i;
+        previousNumItemsPrepended.current = i - adjustPrependedMessageCount;
+        return previousNumItemsPrepended.current;
       }
     }
 
-    // if no match has found, we have jumped - reset the prepend item count.
+    // if no match has found, we have jumped - reset the prepended item count.
     firstMessageId.current = currentFirstMessageId;
     previousNumItemsPrepended.current = 0;
     return 0;
