1ef81ea602c9b1e817f3bbe2c2dcf5b12e838fee
Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>
2023-08-31 12:56:36 +0200

    fix: export ChannelSearchFunctionParams & ChannelSearchParams types (#2084)

--- src/components/ChannelSearch/index.ts
+++ src/components/ChannelSearch/index.ts
@@ -3,3 +3,4 @@ export * from './SearchBar';
 export * from './SearchInput';
 export * from './SearchResults';
 export * from './utils';
+export type { ChannelSearchFunctionParams, ChannelSearchParams } from './hooks/useChannelSearch';
