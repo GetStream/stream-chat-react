30f40e1000b9dee5c95768afcf07a1ec92b82efa
Anton Arnautov <arnautov.anton@gmail.com>
2023-10-25 01:06:30 +0200

    Add "exports" field to the package.json, fix UMD build, add Emojis CJS

---
+++ src/index_UMD.ts
@@ -0,0 +1,8 @@
+// entry file for Rollup for the UMD build \w Emojis
+export * from './components';
+export * from './context';
+export * from './i18n';
+// todo: distribute utils into separate files
+export * from './utils';
+export { getChannel } from './utils/getChannel';
+export * from './components/Emojis';
