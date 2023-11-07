5c514d1c076e147c8461e367cbd19ddc318f4367
Anton Arnautov <arnautov.anton@gmail.com>
2023-11-07 22:21:35 +0100

    Adjust documentation

--- docusaurus/docs/React/_docusaurus-components/GHComponentLink.jsx
+++ docusaurus/docs/React/_docusaurus-components/GHComponentLink.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const GHComponentLink = ({ text, path, branch = 'master' }) => {
+const GHComponentLink = ({ text, as: As = React.Fragment, path, branch = 'master' }) => {
   return (
     <a
       target='_blank'
       href={`https://github.com/GetStream/stream-chat-react/blob/${branch}/src/components${path}`}
     >
-      {text}
+      <As>{text}</As>
     </a>
   );
 };
