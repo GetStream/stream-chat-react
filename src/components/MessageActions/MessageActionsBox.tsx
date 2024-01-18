bcb25a118e63ab650bc3758d6af530228ad7930a
Matvei Andrienko <m.y.andrienko@outlook.com>
2024-01-18 15:07:37 +0100

    fix: get rid of positioning wrapper for message actions box

--- src/components/MessageActions/MessageActionsBox.tsx
+++ src/components/MessageActions/MessageActionsBox.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ComponentProps } from 'react';
 import clsx from 'clsx';
 
 import { MESSAGE_ACTIONS } from '../Message/utils';
@@ -29,123 +29,125 @@ export type MessageActionsBoxProps<
   isUserMuted: () => boolean;
   mine: boolean;
   open: boolean;
-};
+} & ComponentProps<'div'>;
 
-const UnMemoizedMessageActionsBox = <
-  StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics
->(
-  props: MessageActionsBoxProps<StreamChatGenerics>,
-) => {
-  const {
-    getMessageActions,
-    handleDelete,
-    handleEdit,
-    handleFlag,
-    handleMute,
-    handlePin,
-    isUserMuted,
-    open = false,
-  } = props;
+const UnMemoizedMessageActionsBox = React.forwardRef(
+  <StreamChatGenerics extends DefaultStreamChatGenerics = DefaultStreamChatGenerics>(
+    props: MessageActionsBoxProps<StreamChatGenerics>,
+    ref: React.ForwardedRef<HTMLDivElement | null>,
+  ) => {
+    const {
+      getMessageActions,
+      handleDelete,
+      handleEdit,
+      handleFlag,
+      handleMute,
+      handlePin,
+      isUserMuted,
+      open = false,
+      ...restDivProps
+    } = props;
 
-  const {
-    CustomMessageActionsList = DefaultCustomMessageActionsList,
-  } = useComponentContext<StreamChatGenerics>('MessageActionsBox');
-  const { setQuotedMessage } = useChannelActionContext<StreamChatGenerics>('MessageActionsBox');
-  const { customMessageActions, message } = useMessageContext<StreamChatGenerics>(
-    'MessageActionsBox',
-  );
+    const {
+      CustomMessageActionsList = DefaultCustomMessageActionsList,
+    } = useComponentContext<StreamChatGenerics>('MessageActionsBox');
+    const { setQuotedMessage } = useChannelActionContext<StreamChatGenerics>('MessageActionsBox');
+    const { customMessageActions, message } = useMessageContext<StreamChatGenerics>(
+      'MessageActionsBox',
+    );
 
-  const { t } = useTranslationContext('MessageActionsBox');
+    const { t } = useTranslationContext('MessageActionsBox');
 
-  const messageActions = getMessageActions();
+    const messageActions = getMessageActions();
 
-  const handleQuote = () => {
-    setQuotedMessage(message);
+    const handleQuote = () => {
+      setQuotedMessage(message);
 
-    const elements = message.parent_id
-      ? document.querySelectorAll('.str-chat__thread .str-chat__textarea__textarea')
-      : document.getElementsByClassName('str-chat__textarea__textarea');
-    const textarea = elements.item(0);
+      const elements = message.parent_id
+        ? document.querySelectorAll('.str-chat__thread .str-chat__textarea__textarea')
+        : document.getElementsByClassName('str-chat__textarea__textarea');
+      const textarea = elements.item(0);
 
-    if (textarea instanceof HTMLTextAreaElement) {
-      textarea.focus();
-    }
-  };
+      if (textarea instanceof HTMLTextAreaElement) {
+        textarea.focus();
+      }
+    };
 
-  const rootClassName = clsx('str-chat__message-actions-box', {
-    'str-chat__message-actions-box--open': open,
-  });
-  const buttonClassName =
-    'str-chat__message-actions-list-item str-chat__message-actions-list-item-button';
+    const rootClassName = clsx('str-chat__message-actions-box', {
+      'str-chat__message-actions-box--open': open,
+    });
+    const buttonClassName =
+      'str-chat__message-actions-list-item str-chat__message-actions-list-item-button';
 
-  return (
-    <div className={rootClassName} data-testid='message-actions-box'>
-      <div aria-label='Message Options' className='str-chat__message-actions-list' role='listbox'>
-        <CustomMessageActionsList customMessageActions={customMessageActions} message={message} />
-        {messageActions.indexOf(MESSAGE_ACTIONS.quote) > -1 && (
-          <button
-            aria-selected='false'
-            className={buttonClassName}
-            onClick={handleQuote}
-            role='option'
-          >
-            {t<string>('Reply')}
-          </button>
-        )}
-        {messageActions.indexOf(MESSAGE_ACTIONS.pin) > -1 && !message.parent_id && (
-          <button
-            aria-selected='false'
-            className={buttonClassName}
-            onClick={handlePin}
-            role='option'
-          >
-            {!message.pinned ? t<string>('Pin') : t<string>('Unpin')}
-          </button>
-        )}
-        {messageActions.indexOf(MESSAGE_ACTIONS.flag) > -1 && (
-          <button
-            aria-selected='false'
-            className={buttonClassName}
-            onClick={handleFlag}
-            role='option'
-          >
-            {t<string>('Flag')}
-          </button>
-        )}
-        {messageActions.indexOf(MESSAGE_ACTIONS.mute) > -1 && (
-          <button
-            aria-selected='false'
-            className={buttonClassName}
-            onClick={handleMute}
-            role='option'
-          >
-            {isUserMuted() ? t<string>('Unmute') : t<string>('Mute')}
-          </button>
-        )}
-        {messageActions.indexOf(MESSAGE_ACTIONS.edit) > -1 && (
-          <button
-            aria-selected='false'
-            className={buttonClassName}
-            onClick={handleEdit}
-            role='option'
-          >
-            {t<string>('Edit Message')}
-          </button>
-        )}
-        {messageActions.indexOf(MESSAGE_ACTIONS.delete) > -1 && (
-          <button
-            aria-selected='false'
-            className={buttonClassName}
-            onClick={handleDelete}
-            role='option'
-          >
-            {t<string>('Delete')}
-          </button>
-        )}
+    return (
+      <div {...restDivProps} className={rootClassName} data-testid='message-actions-box' ref={ref}>
+        <div aria-label='Message Options' className='str-chat__message-actions-list' role='listbox'>
+          <CustomMessageActionsList customMessageActions={customMessageActions} message={message} />
+          {messageActions.indexOf(MESSAGE_ACTIONS.quote) > -1 && (
+            <button
+              aria-selected='false'
+              className={buttonClassName}
+              onClick={handleQuote}
+              role='option'
+            >
+              {t<string>('Reply')}
+            </button>
+          )}
+          {messageActions.indexOf(MESSAGE_ACTIONS.pin) > -1 && !message.parent_id && (
+            <button
+              aria-selected='false'
+              className={buttonClassName}
+              onClick={handlePin}
+              role='option'
+            >
+              {!message.pinned ? t<string>('Pin') : t<string>('Unpin')}
+            </button>
+          )}
+          {messageActions.indexOf(MESSAGE_ACTIONS.flag) > -1 && (
+            <button
+              aria-selected='false'
+              className={buttonClassName}
+              onClick={handleFlag}
+              role='option'
+            >
+              {t<string>('Flag')}
+            </button>
+          )}
+          {messageActions.indexOf(MESSAGE_ACTIONS.mute) > -1 && (
+            <button
+              aria-selected='false'
+              className={buttonClassName}
+              onClick={handleMute}
+              role='option'
+            >
+              {isUserMuted() ? t<string>('Unmute') : t<string>('Mute')}
+            </button>
+          )}
+          {messageActions.indexOf(MESSAGE_ACTIONS.edit) > -1 && (
+            <button
+              aria-selected='false'
+              className={buttonClassName}
+              onClick={handleEdit}
+              role='option'
+            >
+              {t<string>('Edit Message')}
+            </button>
+          )}
+          {messageActions.indexOf(MESSAGE_ACTIONS.delete) > -1 && (
+            <button
+              aria-selected='false'
+              className={buttonClassName}
+              onClick={handleDelete}
+              role='option'
+            >
+              {t<string>('Delete')}
+            </button>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  },
+);
 
 /**
  * A popup box that displays the available actions on a message, such as edit, delete, pin, etc.
