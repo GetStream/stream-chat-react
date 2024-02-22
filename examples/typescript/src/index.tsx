87f78ba9e35d1f75096402552e8a0305307a349d
Matvei Andrienko <m.y.andrienko@outlook.com>
2024-02-22 17:50:57 +0100

    fix: remove debug stuff

--- examples/typescript/src/index.tsx
+++ examples/typescript/src/index.tsx
@@ -7,7 +7,11 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
