f4f7391a5966eccfc6ac37ff6f59ee876ddcc12b
martincupela <martin.cupela@gmail.com>
2024-03-04 18:02:51 +0100

    feat: use Largest-Triangle-Three-Buckets (LTTB) algorithm for voiceRecording waveform downsampling

---
+++ src/components/Attachment/__tests__/WaveProgressBar.test.js
@@ -0,0 +1,73 @@
+import { downSample, upSample } from '../components';
+
+describe('WaveProgressBar', () => {
+  describe('amplitude sampling', () => {
+    afterEach(jest.restoreAllMocks);
+    const originalSample = Array.from({ length: 10 }, (_, i) => i);
+
+    describe('upSample', () => {
+      afterEach(jest.restoreAllMocks);
+      it('should return original values if target size is smaller than the original sample size', () => {
+        jest.spyOn(console, 'warn').mockImplementation();
+        expect(upSample(originalSample, 5)).toHaveLength(originalSample.length);
+      });
+
+      it('should return original values if the original sample size is empty', () => {
+        jest.spyOn(console, 'warn').mockImplementation();
+        expect(upSample([], 5)).toHaveLength(0);
+      });
+
+      it('should return original values if the original sample size equals the target', () => {
+        jest.spyOn(console, 'warn').mockImplementation();
+        expect(upSample(originalSample, originalSample.length)).toHaveLength(originalSample.length);
+      });
+
+      it('should fill each bucket to reach the target sample size', () => {
+        expect(JSON.stringify(upSample(originalSample, 17))).toBe(
+          JSON.stringify([0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 9]),
+        );
+      });
+    });
+
+    describe('downSample', () => {
+      it('should return original values if target size is greater than the original sample size', () => {
+        jest.spyOn(console, 'warn').mockImplementation();
+        expect(downSample(originalSample, 20)).toHaveLength(originalSample.length);
+      });
+
+      it('should return original values if the original sample size is empty', () => {
+        jest.spyOn(console, 'warn').mockImplementation();
+        expect(downSample([], 5)).toHaveLength(0);
+      });
+
+      it('should return original values if the original sample size equals the target', () => {
+        jest.spyOn(console, 'warn').mockImplementation();
+        expect(downSample(originalSample, originalSample.length)).toHaveLength(
+          originalSample.length,
+        );
+      });
+
+      it('should return a mean of original values if the target output size is 1', () => {
+        expect(JSON.stringify(downSample([10, 2, 6, 10, 3, 4, 8, 0], 1))).toBe(
+          JSON.stringify([5.375]),
+        );
+      });
+
+      it('should fill each bucket to reach the target sample size', () => {
+        expect(
+          JSON.stringify(
+            downSample(
+              [10, 2, 6, 10, 3, 4, 8, 1, 10, 0, 6, 10, 3, 4, 8, 1, 2, 6, 10, 3, 8, 10, 0],
+              7,
+            ),
+          ),
+        ).toBe(JSON.stringify([10, 2, 10, 0, 8, 10, 0]));
+        expect(JSON.stringify(downSample([10, 2, 6, 10, 3, 4, 8, 0], 7))).toBe(
+          JSON.stringify([10, 2, 6, 10, 3, 8, 0]),
+        );
+        expect(JSON.stringify(downSample([10, 2], 2))).toBe(JSON.stringify([10, 2]));
+        expect(JSON.stringify(downSample([10, 2, 10], 2))).toBe(JSON.stringify([10, 10]));
+      });
+    });
+  });
+});
--- src/components/Attachment/components/WaveProgressBar.tsx
+++ src/components/Attachment/components/WaveProgressBar.tsx
@@ -16,7 +16,7 @@ export const WaveProgressBar = ({
   progress = 0,
   waveformData,
 }: WaveProgressBarProps) => {
-  const resampledwaveformData = useMemo(
+  const resampledWaveformData = useMemo(
     () =>
       waveformData.length === amplitudesCount
         ? waveformData
@@ -35,11 +35,11 @@ export const WaveProgressBar = ({
       onClick={onClick}
       role='progressbar'
     >
-      {resampledwaveformData.map((amplitude, i) => (
+      {resampledWaveformData.map((amplitude, i) => (
         <div
           className={clsx('str-chat__wave-progress-bar__amplitude-bar', {
             ['str-chat__wave-progress-bar__amplitude-bar--active']:
-              progress > (i / resampledwaveformData.length) * 100,
+              progress > (i / resampledWaveformData.length) * 100,
           })}
           data-testid='amplitude-bar'
           key={`amplitude-${i}`}
@@ -63,64 +63,104 @@ export const WaveProgressBar = ({
   );
 };
 
-export const downSample = (values: number[], targetSize: number) => {
-  if (!targetSize) {
-    console.warn('Unknown target size of the down-sampled amplitude array.');
-    return values;
-  }
-  if (targetSize > values.length) {
-    console.warn('Requested to down-sample the waveformData that is smaller than the target size');
-    return [];
-  }
-
-  if (targetSize === values.length) return values;
-
-  const bucketSize = values.length / targetSize;
-  const lastBucketSize = Math.round(values.length % bucketSize);
-  const result = [];
-  let countValuesReduced = 0;
-
-  while (countValuesReduced <= values.length - bucketSize) {
-    const bucketItems = values.slice(
-      Math.round(countValuesReduced),
-      Math.round(countValuesReduced + bucketSize),
-    );
-    result.push(mean(bucketItems));
-    countValuesReduced += bucketSize;
+/**
+ * The downSample function uses the Largest-Triangle-Three-Buckets (LTTB) algorithm.
+ * See the thesis Downsampling Time Series for Visual Representation by Sveinn Steinarsson for more (https://skemman.is/bitstream/1946/15343/3/SS_MSthesis.pdf)
+ * @param data
+ * @param targetOutputSize
+ */
+export function downSample(data: number[], targetOutputSize: number): number[] {
+  if (data.length <= targetOutputSize || targetOutputSize === 0) {
+    return data;
   }
 
-  if (lastBucketSize > 0) {
-    const bucketItems = values.slice(-lastBucketSize);
-    result.push(mean(bucketItems));
+  if (targetOutputSize === 1) return [mean(data)];
+
+  const result: number[] = [];
+  // bucket size adjusted due to the fact that the first and the last item in the original data array is kept in target output
+  const bucketSize = (data.length - 2) / (targetOutputSize - 2);
+  let lastSelectedPointIndex = 0;
+  result.push(data[lastSelectedPointIndex]); // Always add the first point
+  let maxAreaPoint, maxArea, triangleArea;
+
+  for (let bucketIndex = 1; bucketIndex < targetOutputSize - 1; bucketIndex++) {
+    const previousBucketRefPoint = data[lastSelectedPointIndex];
+    const nextBucketMean = getNextBucketMean(data, bucketIndex, bucketSize);
+
+    const currentBucketStartIndex = Math.floor((bucketIndex - 1) * bucketSize) + 1;
+    const nextBucketStartIndex = Math.floor(bucketIndex * bucketSize) + 1;
+    const countUnitsBetweenAtoC = 1 + nextBucketStartIndex - currentBucketStartIndex;
+
+    maxArea = triangleArea = -1;
+
+    for (
+      let currentPointIndex = currentBucketStartIndex;
+      currentPointIndex < nextBucketStartIndex;
+      currentPointIndex++
+    ) {
+      const countUnitsBetweenAtoB = Math.abs(currentPointIndex - currentBucketStartIndex) + 1;
+      const countUnitsBetweenBtoC = countUnitsBetweenAtoC - countUnitsBetweenAtoB;
+      const currentPointValue = data[currentPointIndex];
+
+      triangleArea = triangleAreaHeron(
+        triangleBase(Math.abs(previousBucketRefPoint - currentPointValue), countUnitsBetweenAtoB),
+        triangleBase(Math.abs(currentPointValue - nextBucketMean), countUnitsBetweenBtoC),
+        triangleBase(Math.abs(previousBucketRefPoint - nextBucketMean), countUnitsBetweenAtoC),
+      );
+
+      if (triangleArea > maxArea) {
+        maxArea = triangleArea;
+        maxAreaPoint = data[currentPointIndex];
+        lastSelectedPointIndex = currentPointIndex;
+      }
+    }
+
+    if (typeof maxAreaPoint !== 'undefined') result.push(maxAreaPoint);
   }
+
+  result.push(data[data.length - 1]); // Always add the last point
 
   return result;
+}
+
+const triangleAreaHeron = (a: number, b: number, c: number) => {
+  const s = (a + b + c) / 2;
+  return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+};
+
+const triangleBase = (a: number, b: number) => Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+const mean = (values: number[]) => values.reduce((acc, value) => acc + value, 0) / values.length;
+
+const getNextBucketMean = (data: number[], currentBucketIndex: number, bucketSize: number) => {
+  const nextBucketStartIndex = Math.floor(currentBucketIndex * bucketSize) + 1;
+  let nextNextBucketStartIndex = Math.floor((currentBucketIndex + 1) * bucketSize) + 1;
+  nextNextBucketStartIndex =
+    nextNextBucketStartIndex < data.length ? nextNextBucketStartIndex : data.length;
+
+  return mean(data.slice(nextBucketStartIndex, nextNextBucketStartIndex));
 };
 
 export const upSample = (values: number[], targetSize: number) => {
   if (!values.length) {
-    console.log('Cannot extend empty array of amplitudes.');
-    return [];
+    console.warn('Cannot extend empty array of amplitudes.');
+    return values;
   }
 
   if (values.length > targetSize) {
     console.warn('Requested to extend the waveformData that is longer than the target list size');
-    return [];
+    return values;
   }
 
   if (targetSize === values.length) return values;
 
   // eslint-disable-next-line prefer-const
   let [bucketSize, remainder] = divMode(targetSize, values.length);
-  let result: number[] = [];
+  const result: number[] = [];
 
   for (let i = 0; i < values.length; i++) {
     const extra = remainder && remainder-- ? 1 : 0;
-    result = [...result, ...Array(bucketSize + extra).fill(values[i])];
+    result.push(...Array(bucketSize + extra).fill(values[i]));
   }
   return result;
 };
-
-const sum = (values: number[]) => values.reduce((acc, value) => acc + value, 0);
-
-const mean = (values: number[]) => sum(values) / values.length;
