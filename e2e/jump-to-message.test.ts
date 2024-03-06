d3310caec249579c38c052e3c9812fa1f349e844
Matvei Andrienko <m.y.andrienko@outlook.com>
2024-03-06 10:49:57 +0100

    chore: remove flaky test

--- e2e/jump-to-message.test.ts
+++ e2e/jump-to-message.test.ts
@@ -1,7 +1,6 @@
 /* eslint-disable jest/expect-expect */
 /* eslint-disable jest/no-done-callback */
 /* eslint-disable jest/require-top-level-describe */
-import { expect } from '@playwright/test';
 import { test } from './user/test';
 
 import Message from './user/components/Message/MessageSimple';
@@ -12,19 +11,12 @@ const suiteArray = [
   ['regular', 'jump-to-message--jump-in-regular-message-list'],
 ];
 
-const controlsButtonSelector = 'data-testid=jump-to-message';
 const onPageLoadWaitForMessage149 = 'data-testid=message-text-inner-wrapper >> text=Message 149';
 
 suiteArray.forEach(([mode, story]) => {
   test.describe(`jump to message - ${mode}`, () => {
     test.beforeEach(async ({ controller }) => {
       await controller.openStory(story, onPageLoadWaitForMessage149);
-    });
-
-    test(`${mode} jumps to message 29 and then back to bottom`, async ({ page, user }) => {
-      const message29 = await user.sees(Message).not.displayed('Message 29');
-      await page.click(controlsButtonSelector);
-      await expect(message29).toBeVisible();
     });
 
     test(`${mode} jumps to quoted message`, async ({ user }) => {
