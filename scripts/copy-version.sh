269bc6b128b2d24292d1ce1ffd77d0bec7967a47
Matvei Andrienko <m.y.andrienko@outlook.com>
2024-03-14 15:55:59 +0100

    chore: copy-version should emit prettified code

--- scripts/copy-version.sh
+++ scripts/copy-version.sh
@@ -2,6 +2,5 @@
 
 echo -e "\033[34mℹ\033[0m Copying Version to \033[34msrc/version.ts\033[0m"
 PACKAGE_VERSION=$(node -pe 'require(`./package.json`).version')
-PACKAGE_STRING=\"'$PACKAGE_VERSION'\"
-echo 'export const version = '$PACKAGE_STRING';' > ./src/version.ts
+echo "export const version = '"$PACKAGE_VERSION"';" > ./src/version.ts
 echo -e "\033[32m✓\033[0m Done Copying Version"
