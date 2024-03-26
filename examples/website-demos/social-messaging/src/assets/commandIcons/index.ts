f1e7f1383bd5301fd866b9b78fe98d36cd89f59b
Matvei Andrienko <m.y.andrienko@outlook.com>
2024-03-26 15:45:47 +0100

    chore: remove outdated demos

--- examples/website-demos/social-messaging/src/assets/commandIcons/index.ts
+++
@@ -1,6 +0,0 @@
-export { Ban } from './Ban';
-export { Flag } from './Flag';
-export { Mute } from './Mute';
-export { Giphy } from './Giphy';
-export { Unban } from './Unban';
-export { Unmute } from './Unmute';
