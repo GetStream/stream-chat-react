7e98a85efe60f84241aae316bc16453aca9c2ba9
Matvei Andrienko <m.y.andrienko@outlook.com>
2024-04-08 14:51:32 +0200

    fix: file size formatting (#2357)
    Quick fix for some stuff that got broken after removing `pretty-bytes`
    in #2301:
    1. Sizes are now 1024-based (as opposed for 1000-based) to align with
    backend.
    2. Add support for zero-precision,

--- src/components/MessageInput/hooks/utils.ts
+++ src/components/MessageInput/hooks/utils.ts
@@ -212,5 +212,7 @@ export function prettifyFileSize(bytes: number, precision = 3) {
   const units = ['B', 'kB', 'MB', 'GB'];
   const exponent = Math.min(Math.floor(Math.log(bytes) / Math.log(1024)), units.length - 1);
   const mantissa = bytes / 1024 ** exponent;
-  return `${mantissa.toPrecision(precision)} ${units[exponent]}`;
+  const formattedMantissa =
+    precision === 0 ? Math.round(mantissa).toString() : mantissa.toPrecision(precision);
+  return `${formattedMantissa} ${units[exponent]}`;
 }
