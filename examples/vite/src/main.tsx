6514002cf52c55f63f9f6bebe6be48ba3825b5fd
Anton Arnautov <arnautov.anton@gmail.com>
2024-04-30 11:57:39 +0200

    Refresh Vite example

--- examples/vite/src/main.tsx
+++ examples/vite/src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.scss'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
-);
+)
