9efcaeb8bf3d555ce63ebce58f934fce391208ba
Matvei Andrienko <m.y.andrienko@outlook.com>
2024-06-11 12:21:31 +0200

    fix: do not copy css v1

--- scripts/copy-css.sh
+++ scripts/copy-css.sh
@@ -3,7 +3,5 @@
 mkdir -p dist/assets dist/css/v2 dist/scss/v2
 
 cp -r node_modules/@stream-io/stream-chat-css/dist/assets/* dist/assets
-cp -r node_modules/@stream-io/stream-chat-css/dist/css/*.css dist/css
-cp -r node_modules/@stream-io/stream-chat-css/dist/scss/* dist/scss
 cp -r node_modules/@stream-io/stream-chat-css/dist/v2/css/*.css dist/css/v2
 cp -r node_modules/@stream-io/stream-chat-css/dist/v2/scss/* dist/scss/v2
