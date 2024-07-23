4bc2d3591900963290d87408279dc2b516206715
Matvei Andrienko <m.y.andrienko@outlook.com>
2024-07-23 13:22:48 +0200

    fix: change platform for CJS bundle from node to browser (#2454)
    That should fix the problem with node-specific versions of packages
    included in the CJS bundle.

--- scripts/validate-cjs-bundle.cjs
+++ scripts/validate-cjs-bundle.cjs
@@ -1,3 +1,19 @@
 // As the community transitions to ESM, we can easily break our CJS bundle.
 // This smoke test can help to detect this early.
+
+// First, set up minimal browser-like environment:
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM('', {
+  url: 'https://localhost',
+});
+
+global.window = dom.window;
+
+for (const key of Object.keys(window)) {
+  if (global[key] === undefined) {
+    global[key] = window[key];
+  }
+}
+
+// Then, try importing our CJS bundle:
 require('../dist/index.cjs.js');
