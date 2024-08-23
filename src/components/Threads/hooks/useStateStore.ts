ca5a9358f275f1f4841b4bce7f13fe3449fc7f8d
Anton Arnautov <arnautov.anton@gmail.com>
2024-08-23 16:06:42 +0200

    Make useStateStore accept undefined state

--- src/components/Threads/hooks/useStateStore.ts
+++ src/components/Threads/hooks/useStateStore.ts
@@ -2,17 +2,30 @@ import { useEffect, useState } from 'react';
 
 import type { StateStore } from 'stream-chat';
 
-export const useStateStore = <T extends Record<string, unknown>, O extends readonly unknown[]>(
+export function useStateStore<T extends Record<string, unknown>, O extends readonly unknown[]>(
   store: StateStore<T>,
   selector: (v: T) => O,
-) => {
-  const [state, setState] = useState<O>(selector(store.getLatestValue()));
+): O;
+export function useStateStore<T extends Record<string, unknown>, O extends readonly unknown[]>(
+  store: StateStore<T> | undefined,
+  selector: (v: T) => O,
+): O | undefined;
+export function useStateStore<T extends Record<string, unknown>, O extends readonly unknown[]>(
+  store: StateStore<T> | undefined,
+  selector: (v: T) => O,
+) {
+  const [state, setState] = useState<O | undefined>(() => {
+    if (!store) return undefined;
+    return selector(store.getLatestValue());
+  });
 
   useEffect(() => {
+    if (!store) return;
+
     const unsubscribe = store.subscribeWithSelector(selector, setState);
 
     return unsubscribe;
   }, [store, selector]);
 
   return state;
-};
+}
