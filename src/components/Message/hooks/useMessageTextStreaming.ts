4479ef2422fd36fcf5870865f4453bcde025bf1b
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2024-12-03 18:59:33 +0100

    fix: remove optimistic code block capture as it is already there

--- src/components/Message/hooks/useMessageTextStreaming.ts
+++ src/components/Message/hooks/useMessageTextStreaming.ts
@@ -40,9 +40,7 @@ export const useMessageTextStreaming = <
       const newCursorValue = textCursor.current + renderingLetterCount;
       const newText = text.substring(0, newCursorValue);
       textCursor.current += newText.length - textCursor.current;
-      const codeBlockCounts = (newText.match(/```/g) || []).length;
-      const shouldOptimisticallyCloseCodeBlock = codeBlockCounts > 0 && codeBlockCounts % 2 > 0;
-      setStreamedMessageText(shouldOptimisticallyCloseCodeBlock ? newText + '```' : newText);
+      setStreamedMessageText(newText);
     }, streamingLetterIntervalMs);
 
     return () => {
