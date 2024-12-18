0fb87031168dfcef28fab587554d0cc8b31f2d2f
Matvei Andrienko <m.y.andrienko@outlook.com>
2024-12-18 19:18:31 +0100

    fix: use strict mode for demo app

--- examples/vite/src/main.tsx
+++ examples/vite/src/main.tsx
@@ -1,5 +1,10 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
