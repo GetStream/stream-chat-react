4fe18c8f1a9a09fe8aa9a19d95504d6fc8a36be7
Anton Arnautov <arnautov.anton@gmail.com>
2025-02-05 17:47:41 +0100

    Remove generics from examples

--- examples/capacitor/src/App.tsx
+++ examples/capacitor/src/App.tsx
@@ -17,29 +17,11 @@ const apiKey = process.env.REACT_APP_STREAM_KEY as string;
 const userId = process.env.REACT_APP_USER_ID as string;
 const userToken = process.env.REACT_APP_USER_TOKEN as string;
 
-const filters: ChannelFilters = { type: 'messaging', members: {$in: [userId]}  };
+const filters: ChannelFilters = { type: 'messaging', members: { $in: [userId] } };
 const options: ChannelOptions = { state: true, presence: true, limit: 10 };
 const sort: ChannelSort = { last_message_at: -1, updated_at: -1 };
 
-type LocalAttachmentType = Record<string, unknown>;
-type LocalChannelType = Record<string, unknown>;
-type LocalCommandType = string;
-type LocalEventType = Record<string, unknown>;
-type LocalMessageType = Record<string, unknown>;
-type LocalReactionType = Record<string, unknown>;
-type LocalUserType = Record<string, unknown>;
-
-type StreamChatGenerics = {
-  attachmentType: LocalAttachmentType;
-  channelType: LocalChannelType;
-  commandType: LocalCommandType;
-  eventType: LocalEventType;
-  messageType: LocalMessageType;
-  reactionType: LocalReactionType;
-  userType: LocalUserType;
-};
-
-const chatClient = StreamChat.getInstance<StreamChatGenerics>(apiKey);
+const chatClient = StreamChat.getInstance(apiKey);
 
 if (process.env.REACT_APP_CHAT_SERVER_ENDPOINT) {
   chatClient.setBaseURL(process.env.REACT_APP_CHAT_SERVER_ENDPOINT);
