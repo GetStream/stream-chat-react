b2e8c9a730102d5ea7642ce0cab2886b34d27f2e
Anton Arnautov <arnautov.anton@gmail.com>
2025-03-05 19:52:06 +0100

    Fix test and minor inconsistencies

--- scripts/get-package-version.mjs
+++ scripts/get-package-version.mjs
@@ -1,7 +1,7 @@
 import { execSync } from 'node:child_process';
 import packageJson from '../package.json' with { type: 'json' };
 
-// get the latest version so that "__STREAM_CHAT_REACT_VERSION__" can be replaced with it in the source code (used for reporting purposes), see bundle-cjs.mjs/bundle-esm.mjs for source
+// get the latest version so that "process.env.STREAM_CHAT_REACT_VERSION" can be replaced with it in the source code (used for reporting purposes), see bundle-cjs.mjs/bundle-esm.mjs for source
 export default function getPackageVersion() {
   // "build" script ("prepack" hook) gets invoked when semantic-release runs "npm publish", at that point package.json#version already contains updated next version which we can use
   let version = packageJson.version;
