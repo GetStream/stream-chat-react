9f6c7bf89ac9bd78281f73d0c5696df6d581d503
martincupela <martin.cupela@gmail.com>
2025-03-28 16:55:24 +0100

    feat: use MessageComposer's PollComposer to handle poll creation

--- src/components/Poll/PollCreationDialog/OptionFieldSet.tsx
+++ src/components/Poll/PollCreationDialog/OptionFieldSet.tsx
@@ -1,49 +1,31 @@
 import clsx from 'clsx';
-import { MAX_POLL_OPTIONS } from '../constants';
-import { nanoid } from 'nanoid';
 import React, { useCallback } from 'react';
 import { FieldError } from '../../Form/FieldError';
 import { DragAndDropContainer } from '../../DragAndDrop/DragAndDropContainer';
 import { useTranslationContext } from '../../../context';
-import type { OptionErrors, PollFormState, PollOptionFormData } from './types';
+import { useMessageComposer } from '../../MessageInput/hooks/messageComposer/useMessageComposer';
+import { useStateStore } from '../../../store';
+import type { PollComposerState } from 'stream-chat';
 
-const VALIDATION_ERRORS = { 'Option already exists': true } as const as Record<
-  string,
-  boolean
->;
+const pollComposerStateSelector = (state: PollComposerState) => ({
+  errors: state.errors.options,
+  options: state.data.options,
+});
 
-export type OptionFieldSetProps = {
-  errors: OptionErrors;
-  options: PollFormState['options'];
-  setErrors: (fn: (prev: OptionErrors) => OptionErrors) => void;
-  setState: (fn: (prev: PollFormState) => PollFormState) => void;
-};
-
-export const OptionFieldSet = ({
-  errors,
-  options,
-  setErrors,
-  setState,
-}: OptionFieldSetProps) => {
+export const OptionFieldSet = () => {
+  const { pollComposer } = useMessageComposer();
+  const { errors, options } = useStateStore(
+    pollComposer.state,
+    pollComposerStateSelector,
+  );
   const { t } = useTranslationContext('OptionFieldSet');
-
-  const findOptionDuplicate = (sourceOption: PollOptionFormData) => {
-    const isDuplicateFilter = (option: PollOptionFormData) =>
-      !!sourceOption.text.trim() && // do not include empty options into consideration
-      option.id !== sourceOption.id &&
-      option.text.trim() === sourceOption.text.trim();
-
-    return options.find(isDuplicateFilter);
-  };
 
   const onSetNewOrder = useCallback(
     (newOrder: number[]) => {
-      setState((prev) => ({
-        ...prev,
-        options: newOrder.map((index) => prev.options[index]),
-      }));
+      const prevOptions = pollComposer.options;
+      pollComposer.updateFields({ options: newOrder.map((index) => prevOptions[index]) });
     },
-    [setState],
+    [pollComposer],
   );
 
   const draggable = options.length > 1;
@@ -56,83 +38,47 @@ export const OptionFieldSet = ({
         draggable={draggable}
         onSetNewOrder={onSetNewOrder}
       >
-        {options.map((option, i) => (
-          <div
-            className={clsx('str-chat__form__input-field', {
-              'str-chat__form__input-field--draggable': draggable,
-              'str-chat__form__input-field--has-error': errors[option.id],
-            })}
-            key={`new-poll-option-${i}`}
-          >
-            <div className='str-chat__form__input-field__value'>
-              <FieldError
-                className='str-chat__form__input-field__error'
-                data-testid={'poll-option-input-field-error'}
-                text={errors[option.id]}
-              />
-              <input
-                id={option.id}
-                onBlur={(e) => {
-                  if (findOptionDuplicate({ id: e.target.id, text: e.target.value })) {
-                    setErrors((prev) => ({
-                      ...prev,
-                      [e.target.id]: t<string>('Option already exists'),
-                    }));
-                  }
-                }}
-                onChange={(e) => {
-                  setState((prev) => {
-                    const shouldAddEmptyOption =
-                      prev.options.length < MAX_POLL_OPTIONS &&
-                      (!prev.options ||
-                        (prev.options.slice(i + 1).length === 0 && !!e.target.value));
-                    const shouldRemoveOption =
-                      prev.options &&
-                      prev.options.slice(i + 1).length > 0 &&
-                      !e.target.value;
-
-                    const optionListHead = prev.options ? prev.options.slice(0, i) : [];
-                    const optionListTail = shouldAddEmptyOption
-                      ? [{ id: nanoid(), text: '' }]
-                      : (prev.options || []).slice(i + 1);
-
-                    if (
-                      (errors[option.id] && !e.target.value) ||
-                      (VALIDATION_ERRORS[errors[option.id]] &&
-                        !findOptionDuplicate({ id: e.target.id, text: e.target.value }))
-                    ) {
-                      setErrors((prev) => {
-                        delete prev[option.id];
-                        return prev;
-                      });
+        {options.map((option, i) => {
+          const error = errors?.[option.id];
+          return (
+            <div
+              className={clsx('str-chat__form__input-field', {
+                'str-chat__form__input-field--draggable': draggable,
+                'str-chat__form__input-field--has-error': error,
+              })}
+              key={`new-poll-option-${i}`}
+            >
+              <div className='str-chat__form__input-field__value'>
+                <FieldError
+                  className='str-chat__form__input-field__error'
+                  data-testid={'poll-option-input-field-error'}
+                  text={error && t(error)}
+                />
+                <input
+                  id={option.id}
+                  onBlur={() => {
+                    pollComposer.handleFieldBlur('options');
+                  }}
+                  onChange={(e) => {
+                    pollComposer.updateFields({
+                      options: { index: i, text: e.target.value },
+                    });
+                  }}
+                  onKeyUp={(event) => {
+                    if (event.key === 'Enter') {
+                      const nextInputId = options[i + 1].id;
+                      document.getElementById(nextInputId)?.focus();
                     }
-
-                    return {
-                      ...prev,
-                      options: [
-                        ...optionListHead,
-                        ...(shouldRemoveOption
-                          ? []
-                          : [{ ...option, text: e.target.value }]),
-                        ...optionListTail,
-                      ],
-                    };
-                  });
-                }}
-                onKeyUp={(event) => {
-                  if (event.key === 'Enter') {
-                    const nextInputId = options[i + 1].id;
-                    document.getElementById(nextInputId)?.focus();
-                  }
-                }}
-                placeholder={t<string>('Add an option')}
-                type='text'
-                value={option.text}
-              />
+                  }}
+                  placeholder={t<string>('Add an option')}
+                  type='text'
+                  value={option.text}
+                />
+              </div>
+              {draggable && <div className='str-chat__drag-handle' />}
             </div>
-            {draggable && <div className='str-chat__drag-handle' />}
-          </div>
-        ))}
+          );
+        })}
       </DragAndDropContainer>
     </fieldset>
   );
--- src/components/Poll/PollCreationDialog/PollCreationDialog.tsx
+++ src/components/Poll/PollCreationDialog/PollCreationDialog.tsx
@@ -1,193 +1,81 @@
-import clsx from 'clsx';
-import { nanoid } from 'nanoid';
-import React, { useState } from 'react';
-import { FieldError } from '../../Form/FieldError';
+import React, { useCallback } from 'react';
+import type { PollComposerState } from 'stream-chat';
+import { VotingVisibility } from 'stream-chat';
+import { MultipleAnswersField } from './MultipleAnswersField';
+import { NameField } from './NameField';
 import { OptionFieldSet } from './OptionFieldSet';
 import { PollCreationDialogControls } from './PollCreationDialogControls';
-import { VALID_MAX_VOTES_VALUE_REGEX } from '../constants';
 import { ModalHeader } from '../../Modal/ModalHeader';
 import { SimpleSwitchField } from '../../Form/SwitchField';
-import { useChatContext, useTranslationContext } from '../../../context';
-
-import type { VotingVisibility } from 'stream-chat';
-import type { OptionErrors, PollFormState } from './types';
+import { useMessageComposer } from '../../MessageInput/hooks/messageComposer/useMessageComposer';
+import { useTranslationContext } from '../../../context';
+import { useStateStore } from '../../../store';
 
 export type PollCreationDialogProps = {
   close: () => void;
 };
 
-export const PollCreationDialog = ({ close }: PollCreationDialogProps) => {
-  const { client } = useChatContext();
-  const { t } = useTranslationContext();
+const pollComposerStateSelector = (state: PollComposerState) => ({
+  allow_answers: state.data.allow_answers,
+  allow_user_suggested_options: state.data.allow_user_suggested_options,
+  voting_visibility: state.data.voting_visibility,
+});
 
-  const [nameError, setNameError] = useState<string>();
-  const [optionsErrors, setOptionsErrors] = useState<OptionErrors>({});
-  const [multipleAnswerCountError, setMultipleAnswerCountError] = useState<string>();
-  const [state, setState] = useState<PollFormState>(
-    () =>
-      ({
-        allow_answers: false,
-        allow_user_suggested_options: false,
-        description: '',
-        enforce_unique_vote: true,
-        id: nanoid(),
-        max_votes_allowed: '',
-        name: '',
-        options: [{ id: nanoid(), text: '' }],
-        user_id: client.user?.id,
-        voting_visibility: 'public',
-      }) as PollFormState,
-  );
+export const PollCreationDialog = ({ close }: PollCreationDialogProps) => {
+  const { t } = useTranslationContext();
+  const { pollComposer } = useMessageComposer();
+  const { allow_answers, allow_user_suggested_options, voting_visibility } =
+    useStateStore(pollComposer.state, pollComposerStateSelector);
+
+  const onClose = useCallback(() => {
+    pollComposer.initState();
+    close();
+  }, [pollComposer, close]);
 
   return (
     <div
       className='str-chat__dialog str-chat__poll-creation-dialog'
       data-testid='poll-creation-dialog'
     >
-      <ModalHeader close={close} title={t<string>('Create poll')} />
+      <ModalHeader close={onClose} title={t<string>('Create poll')} />
       <div className='str-chat__dialog__body'>
         <form autoComplete='off'>
-          <div
-            className={clsx(
-              'str-chat__form__field str-chat__form__input-field str-chat__form__input-field--with-label',
-              {
-                'str-chat__form__input-field--has-error': nameError,
-              },
-            )}
-          >
-            <label className='str-chat__form__field-label' htmlFor='name'>
-              {t<string>('Question')}
-            </label>
-            <div className={clsx('str-chat__form__input-field__value')}>
-              <FieldError
-                className='str-chat__form__input-field__error'
-                data-testid={'poll-name-input-field-error'}
-                text={nameError}
-              />
-              <input
-                id='name'
-                onBlur={(e) => {
-                  if (!e.target.value) {
-                    setNameError('The field is required');
-                  }
-                }}
-                onChange={(e) => {
-                  setState((prev) => ({ ...prev, name: e.target.value }));
-                  if (nameError && e.target.value) {
-                    setNameError(undefined);
-                  }
-                }}
-                placeholder={t<string>('Ask a question')}
-                type='text'
-                value={state.name}
-              />
-            </div>
-          </div>
-          <OptionFieldSet
-            errors={optionsErrors}
-            options={state.options}
-            setErrors={setOptionsErrors}
-            setState={setState}
-          />
-          <div
-            className={clsx('str-chat__form__expandable-field', {
-              'str-chat__form__expandable-field--expanded': !state.enforce_unique_vote,
-            })}
-          >
-            <SimpleSwitchField
-              checked={!state.enforce_unique_vote}
-              id='enforce_unique_vote'
-              labelText={t<string>('Multiple answers')}
-              onChange={(e) => {
-                setState((prev) => ({
-                  ...prev,
-                  enforce_unique_vote: !e.target.checked,
-                  max_votes_allowed: '',
-                }));
-                setMultipleAnswerCountError(undefined);
-              }}
-            />
-            {!state.enforce_unique_vote && (
-              <div
-                className={clsx('str-chat__form__input-field', {
-                  'str-chat__form__input-field--has-error': multipleAnswerCountError,
-                })}
-              >
-                <div className={clsx('str-chat__form__input-field__value')}>
-                  <FieldError
-                    className='str-chat__form__input-field__error'
-                    data-testid={'poll-max-votes-allowed-input-field-error'}
-                    text={multipleAnswerCountError}
-                  />
-                  <input
-                    id='max_votes_allowed'
-                    onChange={(e) => {
-                      const isValidValue =
-                        !e.target.value ||
-                        e.target.value.match(VALID_MAX_VOTES_VALUE_REGEX);
-                      if (!isValidValue) {
-                        setMultipleAnswerCountError(
-                          t<string>('Type a number from 2 to 10'),
-                        );
-                      } else if (multipleAnswerCountError) {
-                        setMultipleAnswerCountError(undefined);
-                      }
-                      setState((prev) => ({
-                        ...prev,
-                        max_votes_allowed: e.target.value,
-                      }));
-                    }}
-                    placeholder={t<string>('Maximum number of votes (from 2 to 10)')}
-                    type='number'
-                    value={state.max_votes_allowed}
-                  />
-                </div>
-              </div>
-            )}
-          </div>
+          <NameField />
+          <OptionFieldSet />
+          <MultipleAnswersField />
           <SimpleSwitchField
-            checked={state.voting_visibility === 'anonymous'}
+            checked={voting_visibility === 'anonymous'}
             id='voting_visibility'
             labelText={t<string>('Anonymous poll')}
             onChange={(e) =>
-              setState((prev) => ({
-                ...prev,
-                voting_visibility: (e.target.checked
-                  ? 'anonymous'
-                  : 'public') as VotingVisibility,
-              }))
+              pollComposer.updateFields({
+                voting_visibility: e.target.checked
+                  ? VotingVisibility.anonymous
+                  : VotingVisibility.public,
+              })
             }
           />
           <SimpleSwitchField
-            checked={state.allow_user_suggested_options}
+            checked={allow_user_suggested_options}
             id='allow_user_suggested_options'
             labelText={t<string>('Allow option suggestion')}
             onChange={(e) =>
-              setState((prev) => ({
-                ...prev,
+              pollComposer.updateFields({
                 allow_user_suggested_options: e.target.checked,
-              }))
+              })
             }
           />
           <SimpleSwitchField
-            checked={state.allow_answers}
+            checked={allow_answers}
             id='allow_answers'
             labelText={t<string>('Allow comments')}
             onChange={(e) =>
-              setState((prev) => ({ ...prev, allow_answers: e.target.checked }))
+              pollComposer.updateFields({ allow_answers: e.target.checked })
             }
           />
         </form>
       </div>
-      <PollCreationDialogControls
-        close={close}
-        errors={[
-          ...(nameError ?? []),
-          ...(multipleAnswerCountError ?? []),
-          ...Object.keys(optionsErrors),
-        ]}
-        state={state}
-      />
+      <PollCreationDialogControls close={close} />
     </div>
   );
 };
