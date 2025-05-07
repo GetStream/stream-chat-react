9c9d05eb3d3e02c3f722699035fa45050d08c7d5
martincupela <martin.cupela@gmail.com>
2025-05-07 12:24:50 +0200

    chore: pass env variables to examples app vite

--- examples/vite/vite.config.ts
+++ examples/vite/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  const rootDir = process.cwd();
+
+  // Load shared .env file
+  const env = loadEnv('', rootDir, '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env, // need `process.env` access
+    },
+  };
 });
