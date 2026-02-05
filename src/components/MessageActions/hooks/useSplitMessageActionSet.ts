e8bcb55fe594db45d375a5d12f9e348fd2abb7bb
MartinCupela <32706194+MartinCupela@users.noreply.github.com>
2026-02-05 17:17:13 +0100

    refactor: streamline useMemo
    Co-authored-by: Anton Arnautov <43254280+arnautov-anton@users.noreply.github.com>

--- src/components/MessageActions/hooks/useSplitMessageActionSet.ts
+++ src/components/MessageActions/hooks/useSplitMessageActionSet.ts
@@ -19,5 +19,14 @@ export const useSplitMessageActionSet = (messageActionSet: MessageActionSetItem[
       if (action.placement === 'dropdown') dropdownActionSet.push(action);
     }
 
-    return { dropdownActionSet, quickActionSet };
+  useMemo(() => {
+    const quickActionSet: QuickMessageActionSetItem[] = [];
+    const dropdownActionSet: DropdownMessageActionSetItem[] = [];
+
+for (const action of messageActionSet) {
+if (action.placement === 'quick') quickActionSet.push(action);
+if (action.placement === 'dropdown') dropdownActionSet.push(action);
+}
+
+return { dropdownActionSet, quickActionSet } as const;
   }, [messageActionSet]);
