aeb0798e482370c12f2ba20a509915ff29a8c478
Anton Arnautov <arnautov.anton@gmail.com>
2026-03-06 17:04:52 +0100

    Remove unused file

--- src/components/ChannelPreview/hooks/index.ts
+++ src/components/ChannelPreview/hooks/index.ts
@@ -1,9 +1,2 @@
 export { useChannelPreviewInfo } from './useChannelPreviewInfo';
-export { useLatestMessagePreview } from './useLatestMessagePreview';
-export type {
-  ChannelPreviewDeliveryStatus,
-  ChannelPreviewMessageType,
-  LatestMessagePreviewData,
-  UseLatestMessagePreviewParams,
-} from './useLatestMessagePreview';
 export { MessageDeliveryStatus } from './useMessageDeliveryStatus';
