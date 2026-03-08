e5d2874d9aa5021f75a55442de4f2256029acd6f
martincupela <martin.cupela@gmail.com>
2026-03-08 16:22:49 +0100

    feat: redesign PollAnswerList

--- src/components/Poll/PollActions/PollAnswerList.tsx
+++ src/components/Poll/PollActions/PollAnswerList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Prompt } from '../../Dialog';
+import { Viewer } from '../../Dialog';
 import { PollVote } from '../PollVote';
 import { usePollAnswerPagination } from '../hooks';
 import { InfiniteScrollPaginator } from '../../InfiniteScrollPaginator/InfiniteScrollPaginator';
@@ -34,11 +34,11 @@ export const PollAnswerList = ({
   const { answers, error, hasNextPage, loading, loadMore } = usePollAnswerPagination();
 
   return (
-    <Prompt.Root className='str-chat__modal__poll-answer-list'>
-      <Prompt.Header close={close} title={t('Poll comments')} />
-      <Prompt.Body className='str-chat__modal__poll-answer-list__body'>
-        <InfiniteScrollPaginator loadNextOnScrollToBottom={loadMore} threshold={40}>
-          <div className='str-chat__poll-answer-list'>
+    <Viewer.Root className='str-chat__modal__poll-answer-list'>
+      <Viewer.Header close={close} title={t('Poll comments')} />
+      <Viewer.Body className='str-chat__modal__poll-answer-list__body'>
+        <div className='str-chat__poll-answer-list'>
+          <InfiniteScrollPaginator loadNextOnScrollToBottom={loadMore} threshold={40}>
             {answers.map((answer) => (
               <div className='str-chat__poll-answer' key={`comment-${answer.id}`}>
                 {answer.answer_text && (
@@ -47,27 +47,28 @@ export const PollAnswerList = ({
                 <PollVote key={`poll-vote-${answer.id}`} vote={answer} />
               </div>
             ))}
-          </div>
-          {hasNextPage && (
-            <div className='str-chat__loading-indicator-placeholder'>
-              {loading && <LoadingIndicator />}
-            </div>
-          )}
-        </InfiniteScrollPaginator>
+
+            {hasNextPage && (
+              <div className='str-chat__loading-indicator-placeholder'>
+                {loading && <LoadingIndicator />}
+              </div>
+            )}
+          </InfiniteScrollPaginator>
+        </div>
         {error?.message && <div>{error?.message}</div>}
-      </Prompt.Body>
-      <Prompt.Footer>
+      </Viewer.Body>
+      <Viewer.Footer>
         {answers.length > 0 && !is_closed && (
-          <Prompt.FooterControls>
-            <Prompt.FooterControlsButtonSecondary
+          <Viewer.FooterControls>
+            <Viewer.FooterControlsButtonSecondary
               className='str-chat__poll-action'
               onClick={onUpdateOwnAnswerClick}
             >
               {ownAnswer ? t('Update your comment') : t('Add a comment')}
-            </Prompt.FooterControlsButtonSecondary>
-          </Prompt.FooterControls>
+            </Viewer.FooterControlsButtonSecondary>
+          </Viewer.FooterControls>
         )}
-      </Prompt.Footer>
-    </Prompt.Root>
+      </Viewer.Footer>
+    </Viewer.Root>
   );
 };
