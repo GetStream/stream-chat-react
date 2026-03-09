d2848b747119bc91e4c6a16b500598e555ac9d0b
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2026-03-09 13:18:00 +0100

    feat: redesign channel list loading skeletons

--- src/components/Loading/LoadingChannels.tsx
+++ src/components/Loading/LoadingChannels.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 
 const LoadingItems = () => (
-  <div className='str-chat__loading-channels-item str-chat__channel-preview-loading'>
+  <div className='str-chat__channel-preview-loading'>
     <div className='str-chat__loading-channels-avatar' />
-    <div className='str-chat__loading-channels-meta str-chat__channel-preview-end-loading'>
+    <div className='str-chat__channel-preview-end-loading'>
       <div className='str-chat__loading-channels-username' />
       <div className='str-chat__loading-channels-status' />
     </div>
   </div>
 );
 
-const UnMemoizedLoadingChannels = () => (
+export const LoadingChannels = () => (
   <div className='str-chat__loading-channels'>
+    <LoadingItems />
+    <LoadingItems />
     <LoadingItems />
     <LoadingItems />
     <LoadingItems />
   </div>
 );
-
-/**
- * Loading indicator for the ChannelList
- */
-export const LoadingChannels = React.memo(UnMemoizedLoadingChannels);
