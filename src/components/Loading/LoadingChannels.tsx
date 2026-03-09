ddea05fb9b99d40924ea63c93af42b126bdafa6a
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2026-03-09 13:32:03 +0100

    fix: reduce channel list loading layout shift

--- src/components/Loading/LoadingChannels.tsx
+++ src/components/Loading/LoadingChannels.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const LoadingItems = () => (
-  <div className='str-chat__channel-preview-loading'>
-    <div className='str-chat__loading-channels-avatar' />
-    <div className='str-chat__channel-preview-end-loading'>
-      <div className='str-chat__loading-channels-username' />
-      <div className='str-chat__loading-channels-status' />
+  <div className='str-chat__channel-preview-container'>
+    <div
+      aria-hidden='true'
+      className='str-chat__channel-preview str-chat__channel-preview--loading'
+    >
+      <div className='str-chat__loading-channels-avatar' />
+      <div className='str-chat__channel-preview-data str-chat__channel-preview-data--loading'>
+        <div className='str-chat__loading-channels-username' />
+        <div className='str-chat__loading-channels-status' />
+      </div>
     </div>
   </div>
 );
