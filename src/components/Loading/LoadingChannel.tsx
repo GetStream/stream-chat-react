790df172ab949d5ec4b4fc3bcd73b60871fe25f6
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2026-03-09 15:13:13 +0100

    feat: redesign the LoadingChannel component (#2991)
    ### 🎯 Goal
    
    See:
    https://linear.app/stream/issue/REACT-826/redesign-loading-skeletons
    and https://github.com/GetStream/stream-chat-react/pull/2988

--- src/components/Loading/LoadingChannel.tsx
+++ src/components/Loading/LoadingChannel.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 
-const LoadingMessage = () => (
-  <div className='str-chat__loading-channel-message'>
-    <div className='str-chat__loading-channel-message-avatar'></div>
-    <div className='str-chat__loading-channel-message-end'>
-      <div className='str-chat__loading-channel-message-sender'></div>
-      <div className='str-chat__loading-channel-message-last-row'>
-        <div className='str-chat__loading-channel-message-text'></div>
+type LoadingMessageProps = {
+  bubbleSize: 'sm' | 'md' | 'lg';
+  outgoing?: boolean;
+};
+
+const LoadingMessage = ({ bubbleSize, outgoing = false }: LoadingMessageProps) => (
+  <div
+    className={`str-chat__loading-channel-message ${
+      outgoing
+        ? 'str-chat__loading-channel-message--outgoing'
+        : 'str-chat__loading-channel-message--incoming'
+    }`}
+  >
+    {!outgoing ? <div className='str-chat__loading-channel-message-avatar' /> : null}
+    <div className='str-chat__loading-channel-message-content'>
+      <div
+        className={`str-chat__loading-channel-message-bubble str-chat__loading-channel-message-bubble--${bubbleSize}`}
+      ></div>
+      <div className='str-chat__loading-channel-message-metadata'>
+        <div className='str-chat__loading-channel-message-sender'></div>
         <div className='str-chat__loading-channel-message-date'></div>
       </div>
     </div>
@@ -14,29 +27,34 @@ const LoadingMessage = () => (
 );
 
 const LoadingMessageInput = () => (
-  <div className='str-chat__loading-channel-message-input-row'>
-    <div className='str-chat__loading-channel-message-input'></div>
-    <div className='str-chat__loading-channel-message-send'></div>
+  <div className='str-chat__message-composer-container str-chat__message-composer-container--loading'>
+    <div className='str-chat__message-composer'>
+      <div className='str-chat__loading-channel-message-input-button'></div>
+      <div className='str-chat__loading-channel-message-input-pill'></div>
+    </div>
   </div>
 );
 
 const LoadingChannelHeader = () => (
-  <div className='str-chat__loading-channel-header'>
-    <div className='str-chat__loading-channel-header-avatar'></div>
-    <div className='str-chat__loading-channel-header-end'>
+  <div className='str-chat__channel-header str-chat__channel-header--loading'>
+    <div className='str-chat__channel-header__data str-chat__channel-header__data--loading'>
       <div className='str-chat__loading-channel-header-name'></div>
-      <div className='str-chat__loading-channel-header-info'></div>
     </div>
+    <div className='str-chat__loading-channel-header-avatar'></div>
   </div>
 );
 
 export const LoadingChannel = () => (
   <div className='str-chat__loading-channel'>
     <LoadingChannelHeader />
-    <div className='str-chat__loading-channel-message-list'>
-      {Array.from(Array(3)).map((_, i) => (
-        <LoadingMessage key={`loading-message-${i}`} />
-      ))}
+    <div className='str-chat__message-list str-chat__message-list--loading'>
+      <div className='str-chat__message-list-scroll'>
+        <div className='str-chat__loading-channel-message-list'>
+          <LoadingMessage bubbleSize='lg' />
+          <LoadingMessage bubbleSize='sm' outgoing />
+          <LoadingMessage bubbleSize='md' />
+        </div>
+      </div>
     </div>
     <LoadingMessageInput />
   </div>
