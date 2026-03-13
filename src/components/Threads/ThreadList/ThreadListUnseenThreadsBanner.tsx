2d50a852a74f165d03d94785dd4b210296202551
Anton Arnautov <arnautov.anton@gmail.com>
2026-03-13 16:34:58 +0100

    Adjust ThreadLIstUnseenThreadsBanner and add item highlighting

--- src/components/Threads/ThreadList/ThreadListUnseenThreadsBanner.tsx
+++ src/components/Threads/ThreadList/ThreadListUnseenThreadsBanner.tsx
@@ -1,31 +1,49 @@
 import React from 'react';
+import clsx from 'clsx';
 
 import type { ThreadManagerState } from 'stream-chat';
 
-import { IconArrowRotateClockwise } from '../../Icons';
-import { useChatContext } from '../../../context';
+import { IconArrowRotateRightLeftRepeatRefresh } from '../../Icons';
+import { useChatContext, useTranslationContext } from '../../../context';
 import { useStateStore } from '../../../store';
+import { LoadingIndicator } from '../../Loading';
 
 const selector = (nextValue: ThreadManagerState) => ({
+  isLoading: nextValue.pagination.isLoading,
   unseenThreadIds: nextValue.unseenThreadIds,
 });
 
 export const ThreadListUnseenThreadsBanner = () => {
   const { client } = useChatContext();
-  const { unseenThreadIds } = useStateStore(client.threads.state, selector);
+  const { t } = useTranslationContext();
+  const { isLoading, unseenThreadIds } = useStateStore(client.threads.state, selector);
 
   if (!unseenThreadIds.length) return null;
 
   return (
-    <div className='str-chat__unseen-threads-banner'>
-      {/* TODO: translate */}
-      {unseenThreadIds.length} unread threads
-      <button
-        className='str-chat__unseen-threads-banner__button'
-        onClick={() => client.threads.reload()}
-      >
-        <IconArrowRotateClockwise />
-      </button>
-    </div>
+    <button
+      className={clsx('str-chat__unseen-threads-banner', {
+        'str-chat__unseen-threads-banner--loading': isLoading,
+      })}
+      disabled={isLoading}
+      onClick={() => client.threads.reload()}
+    >
+      {!isLoading && (
+        <>
+          <IconArrowRotateRightLeftRepeatRefresh />
+          <span>
+            {t('ThreadListUnseenThreadsBanner/unreadThreads', {
+              count: unseenThreadIds.length,
+            })}
+          </span>
+        </>
+      )}
+      {isLoading && (
+        <>
+          <LoadingIndicator />
+          <span>{t('ThreadListUnseenThreadsBanner/loading')}</span>
+        </>
+      )}
+    </button>
   );
 };
