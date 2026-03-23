36303305b423e36ba16cd834ce1c2caf7d7329eb
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2026-03-23 23:40:56 +0100

    chore: remove outdated example apps

--- examples/nextjs/pages/_app.jsx
+++
@@ -1,7 +0,0 @@
-// pages/_app.js
-import '@stream-io/stream-chat-css/dist/css/index.css';
-import './App.css';
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
