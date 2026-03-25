eda084445e23e5c83cbf1bab54cc77581806ae20
MartinCupela <32706194+MartinCupela@users.noreply.github.com>
2026-03-25 11:33:22 +0100

    fix: calculate message read status for the first message in a channel (#3055)

--- src/utils/findReverse.ts
+++ src/utils/findReverse.ts
@@ -3,7 +3,7 @@ export const findReverse = <T>(
   items: T[],
   matches: (items: T) => boolean,
 ): T | undefined => {
-  for (let i = items.length - 1; i > 0; i -= 1) {
+  for (let i = items.length - 1; i >= 0; i -= 1) {
     if (matches(items[i])) {
       return items[i];
     }
