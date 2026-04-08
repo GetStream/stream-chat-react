0bc0dbd3e44efc66f274581b0040f9958d117a6c
Oliver Lazoroski <oliver.lazoroski@gmail.com>
2026-04-08 11:48:35 +0200

    chore: remove missing import

--- src/components/ChannelList/hooks/index.ts
+++ src/components/ChannelList/hooks/index.ts
@@ -1,5 +1,4 @@
 export * from './useConnectionRecoveredListener';
-export * from './useMobileNavigation';
 export * from './usePaginatedChannels';
 export * from './useChannelMembershipState';
 export * from './useChannelMembersState';
