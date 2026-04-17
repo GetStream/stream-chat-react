c4ab89b7815cd14093f61ea8754d382367228d0f
Zita Szupera <szuperaz@gmail.com>
2026-04-17 14:03:57 +0200

    fix: recognize async moderated shadow blocked messages as blocked

--- src/components/Message/utils.tsx
+++ src/components/Message/utils.tsx
@@ -399,11 +399,12 @@ export const isMessageBounced = (
     message.moderation?.action === 'bounce');
 
 export const isMessageBlocked = (
-  message: Pick<LocalMessage, 'type' | 'moderation' | 'moderation_details'>,
+  message: Pick<LocalMessage, 'type' | 'moderation' | 'moderation_details' | 'shadowed'>,
 ) =>
-  message.type === 'error' &&
-  (message.moderation_details?.action === 'MESSAGE_RESPONSE_ACTION_REMOVE' ||
-    message.moderation?.action === 'remove');
+  message.shadowed ||
+  (message.type === 'error' &&
+    (message.moderation_details?.action === 'MESSAGE_RESPONSE_ACTION_REMOVE' ||
+      message.moderation?.action === 'remove'));
 
 export const isMessageDeleted = (message: LocalMessage): boolean =>
   Boolean(message.deleted_at || message.type === 'deleted' || message.deleted_for_me);
