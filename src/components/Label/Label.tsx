8172027aeb11caba3d8e9ff2c34175cde730e410
martincupela <martin.cupela@gmail.com>
2026-06-16 14:06:38 +0200

    fix(ChannelDetail): untrack Label

--- src/components/Label/Label.tsx
+++
@@ -1,8 +0,0 @@
-import clsx from 'clsx';
-import type { ComponentProps } from 'react';
-
-export type LabelProps = Omit<ComponentProps<'div'>, 'role'>;
-
-export const Label = ({ className, ...props }: LabelProps) => (
-  <div {...props} className={clsx('str-chat__label', className)} role='label' />
-);
